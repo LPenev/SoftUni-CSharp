9fcb05fbe9a7948be53ecdb611091b294a7817fd
LPenev <lppenev@gmail.com>
2023-06-03 15:09:40 +0300

    01. Hello SoftUni Completed

---
+++ 01. Basic/01. CSharp-First-Steps-in-Coding-Lab/01. Hello SoftUni/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01._Hello_SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Show Message
+            Console.WriteLine("Hello SoftUni")
+        }
+    }
+}
