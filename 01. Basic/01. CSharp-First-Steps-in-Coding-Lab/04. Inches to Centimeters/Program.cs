5cbdc2c4dbe8fa9c92cbef633a8b5875a64b8ea3
LPenev <lppenev@gmail.com>
2023-06-04 18:49:16 +0200

    Task 04 completed

---
+++ 01. Basic/01. CSharp-First-Steps-in-Coding-Lab/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _04._Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from console
+            double inch = double.Parse(Console.ReadLine());
+            // Show result
+            Console.WriteLine(inch * 2.54);
+        }
+    }
+}
