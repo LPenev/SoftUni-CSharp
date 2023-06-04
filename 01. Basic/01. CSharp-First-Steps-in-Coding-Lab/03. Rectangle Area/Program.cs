13ce7bc79456b8a5ae61c0795191b821e3a73bad
LPenev <lppenev@gmail.com>
2023-06-04 12:13:58 +0300

    03. Rectangle Area Completed

---
+++ 01. Basic/01. CSharp-First-Steps-in-Coding-Lab/03. Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03._Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int a, b, area;
+            // Input a
+            a = int.Parse(Console.ReadLine());
+            // Input b
+            b = int.Parse(Console.ReadLine());
+            // Calculate a*b
+            area = a * b;
+            // Show Result
+            Console.WriteLine(area);
+        }
+    }
+}
