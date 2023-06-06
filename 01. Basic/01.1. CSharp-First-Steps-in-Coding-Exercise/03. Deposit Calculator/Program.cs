8532a85abca30293fe4c446e3527caea18dcbb19
LPenev <lppenev@gmail.com>
2023-06-06 12:01:04 +0300

    03. Deposit Calculator Completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from Console
+            double depositSum = double.Parse(Console.ReadLine());
+            int depositCount = int.Parse(Console.ReadLine());
+            double yearInterest = double.Parse(Console.ReadLine());
+            // Calc
+            yearInterest = (depositSum * yearInterest / 100) / 12;
+            depositSum = depositSum + yearInterest * depositCount;
+            // Show result
+            Console.WriteLine(depositSum);
+        }
+    }
+}
