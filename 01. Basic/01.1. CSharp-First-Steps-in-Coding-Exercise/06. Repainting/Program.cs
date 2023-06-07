1d2402ad5a7bedf5db31b593a8a297fd2ca60e3f
LPenev <lppenev@gmail.com>
2023-06-07 11:12:26 +0300

    06. Repainting Completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/06. Repainting/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _06._Repainting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from Console
+            int nailon = int.Parse(Console.ReadLine());
+            int boq = int.Parse(Console.ReadLine());
+            int razreditel = int.Parse(Console.ReadLine());
+            int chasove = int.Parse(Console.ReadLine());
+
+            // Calc
+            // nailon
+            double sum = (nailon + 2) * 1.5;
+            // boq
+            sum = sum + (boq * 1.1) * 14.5;
+            // razreditel
+            sum = sum + razreditel * 5;
+            // torbichki
+            sum += 0.4;
+            // majstori
+            sum = sum + sum * 0.3 * chasove;
+
+            // Show result
+            Console.WriteLine(sum);
+        }
+    }
+}
