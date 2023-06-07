aae968c324ca1dcbd51ea96405acdb91e3e687ee
LPenev <lppenev@gmail.com>
2023-06-07 18:51:20 +0200

    Task 07 completed

---
+++ 01. Basic/01. CSharp-First-Steps-in-Coding-Lab/07. Projects Creation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07._Projects_Creation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                string archName = Console.ReadLine();
+                int numProjects = int.Parse(Console.ReadLine());
+                int hours = numProjects * 3;
+                // Show result
+                Console.WriteLine($"The architect {archName} will need {hours} hours to complete {numProjects} project/s.");
+            }
+    }
+}
