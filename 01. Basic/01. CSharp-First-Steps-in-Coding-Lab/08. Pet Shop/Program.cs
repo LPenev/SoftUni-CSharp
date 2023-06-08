035f193d7486cadbb6a407cd356cb3a6b61f701f
LPenev <lppenev@gmail.com>
2023-06-08 18:51:56 +0200

    Task 08 completed

---
+++ 01. Basic/01. CSharp-First-Steps-in-Coding-Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _08._Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable
+            int cats, dogs;
+            double sum;
+            // Read from Console
+            dogs = int.Parse(Console.ReadLine());
+            cats = int.Parse(Console.ReadLine());
+            // Calc
+            sum = cats * 4 + dogs * 2.50;
+            // Show result
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
