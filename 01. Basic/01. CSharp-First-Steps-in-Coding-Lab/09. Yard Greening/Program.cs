e73074bc0ffe352790125c3fbcc01446b4ff13d7
LPenev <lppenev@gmail.com>
2023-06-09 18:52:47 +0200

    Task 09 completed

---
+++ 01. Basic/01. CSharp-First-Steps-in-Coding-Lab/09. Yard Greening/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _09._Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read console
+            double place = double.Parse(Console.ReadLine());
+            // calc place * price
+            double cena = place * 7.61;
+            // calc discount
+            double suma = cena * 0.18;
+            // calc final price with discount
+            cena -= suma;
+            // Show result
+            Console.WriteLine($"The final price is: {cena} lv.");
+            Console.WriteLine($"The discount is: {suma} lv.");
+        }
+    }
+}
