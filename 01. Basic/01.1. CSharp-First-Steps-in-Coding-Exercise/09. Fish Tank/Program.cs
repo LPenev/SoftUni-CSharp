86c1e9b8ccd7fd21fb0afe5fa1cfc29af6ed7bd6
LPenev <lppenev@gmail.com>
2023-06-09 10:46:27 +0300

    09. Fish Tank Completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/09. Fish Tank/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _09._Fish_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from Console
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double prozent = double.Parse(Console.ReadLine());
+
+            // Calc volume tank litri
+            double volumeTank = length * width * height;
+            volumeTank /= 1000;
+            // Calc litri
+            double litri = volumeTank * (1 - prozent / 100);
+
+            // Show result
+            Console.WriteLine(litri);
+        }
+    }
+}
