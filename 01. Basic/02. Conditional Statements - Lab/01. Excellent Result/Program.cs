3ecd65ff455517ac368b1d78ccfbb7f20fa13fce
LPenev <lppenev@gmail.com>
2023-06-10 14:21:52 +0300

    01. Excellent Result Completed

---
+++ 01. Basic/02. Conditional Statements - Lab/01. Excellent Result/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01._Excellent_Result
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.5)
+            {
+                System.Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
