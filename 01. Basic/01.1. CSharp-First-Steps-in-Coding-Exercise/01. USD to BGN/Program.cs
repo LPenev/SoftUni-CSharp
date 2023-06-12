51fc989890c941f2b00fbc718ef10250a2c9e5ff
LPenev <lppenev@gmail.com>
2023-06-12 10:01:21 +0200

    Task 01 completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/01. USD to BGN/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._USD_to_BGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            double usb, bgn;
+            string inputTxt;
+            // Show message
+            Console.Write("Input USD amount: ");
+            // Read Text from Conosle
+            inputTxt = Console.ReadLine();
+            // Convert inputed string to double
+            bgn = double.Parse(inputTxt);
+            // Convert usd to bgn
+            usb = bgn * 1.79549;
+            // Show message
+            Console.Write("Your amount in BGN is: ");
+            // Show Result
+            Console.WriteLine(usb);
+        }
+    }
+}
