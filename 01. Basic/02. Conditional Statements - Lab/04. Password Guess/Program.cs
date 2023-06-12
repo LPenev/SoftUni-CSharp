7b0ce29f2a66c2e5ef6681ca8dc8b7d1887225d8
LPenev <lppenev@gmail.com>
2023-06-12 12:11:13 +0300

    04. Password Guess Completed

---
+++ 01. Basic/02. Conditional Statements - Lab/04. Password Guess/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04._Password_Guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = Console.ReadLine();
+            if (pass == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
