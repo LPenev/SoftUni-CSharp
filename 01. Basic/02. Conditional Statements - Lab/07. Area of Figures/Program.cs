1c1d7964f01bc51431f909ae6e7d7401753418bd
LPenev <lppenev@gmail.com>
2023-06-12 12:14:28 +0300

    07. Area of Figures Comleted

---
+++ 01. Basic/02. Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _07._Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result, side, side1, side2;
+
+            string fig = Console.ReadLine();
+            if (fig == "square")
+            {
+                side = double.Parse(Console.ReadLine());
+                side *= side;
+                Console.WriteLine($"{side:f3}");
+            }
+            else if (fig == "rectangle")
+            {
+                side1 = double.Parse(Console.ReadLine());
+                side2 = double.Parse(Console.ReadLine());
+                result = side1 * side2;
+                Console.WriteLine($"{result:f3}");
+
+            }
+            else if (fig == "circle")
+            {
+                side1 = double.Parse(Console.ReadLine());
+                result = Math.PI * Math.Pow(side1, 2);
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (fig == "triangle")
+            {
+                side1 = double.Parse(Console.ReadLine());
+                side2 = double.Parse(Console.ReadLine());
+                result = (side1 * side2) / 2;
+                Console.WriteLine($"{result:f3}");
+            }
+        }
+    }
+}
