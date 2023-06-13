8ff01eeb4d3faa2f4ef13b4b92b87e5b1014ec36
LPenev <lppenev@gmail.com>
2023-06-13 10:02:54 +0200

    Task 02 completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/02. Radians to Degrees/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02._Radians_to_Degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get input from Console
+            double radians = double.Parse(Console.ReadLine());
+            // calc degrees
+            double degrees = radians * 180 / Math.PI;
+            // Show result
+            Console.WriteLine(degrees);
+        }
+    }
+}
