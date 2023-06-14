f4a645252568d5d5bd4691df7fd68ee851f006b4
LPenev <lppenev@gmail.com>
2023-06-14 11:49:26 +0300

    03. Time + 15 Minutes Completed

---
+++ 01. Basic/02.1. Conditional Statements - Exercise/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03._Time___15_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // input Hour and Minute
+            int hour = int.Parse(Console.ReadLine());
+            int minute = int.Parse(Console.ReadLine());
+            // Calculate time + 15 min
+            minute += 15;
+            // Check
+            if (minute > 59)
+            {
+                hour += 1;
+                minute = minute - 60;
+            }
+            if (hour > 23) hour = 0;
+            // Print result
+            Console.WriteLine($"{hour}:{minute:d2}");
+        }
+    }
+}
