512c1e954727424ad6acd6bef18281b78e7af292
LPenev <lppenev@gmail.com>
2023-06-15 10:06:00 +0200

    Task 04 completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _04._Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from Console
+            int countPages = int.Parse(Console.ReadLine());
+            int Pages = int.Parse(Console.ReadLine());
+            int countDays = int.Parse(Console.ReadLine());
+            // Calc
+            countPages /= Pages;
+            countDays = countPages / countDays;
+
+            // Show result
+            Console.WriteLine(countDays);
+        }
+    }
+}
