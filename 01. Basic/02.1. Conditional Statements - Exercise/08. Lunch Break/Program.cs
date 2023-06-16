a9709ea85538dc94e6d373f3d3f774fe5150473f
LPenev <lppenev@gmail.com>
2023-06-16 11:37:26 +0300

    08. Lunch Break Completed

---
+++ 01. Basic/02.1. Conditional Statements - Exercise/08. Lunch Break/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input variables
+            string nameSerial = Console.ReadLine();
+            int serialDuration = int.Parse(Console.ReadLine());
+            double restDuration = int.Parse(Console.ReadLine());
+
+            //Calc
+            double launchTime = restDuration / 8;
+            double restTime = restDuration / 4;
+            double freeTime = restDuration - launchTime - restTime;
+
+            // Show result
+            if (freeTime >= serialDuration)
+            {
+                restTime = Math.Ceiling(freeTime - serialDuration);
+                Console.WriteLine($"You have enough time to watch {nameSerial} and left with {restTime} minutes free time.");
+
+            }
+            else
+            {
+                restTime = Math.Ceiling(serialDuration - freeTime);
+                Console.WriteLine($"You don't have enough time to watch {nameSerial}, you need {restTime} more minutes.");
+            }
+        }
+    }
+}
