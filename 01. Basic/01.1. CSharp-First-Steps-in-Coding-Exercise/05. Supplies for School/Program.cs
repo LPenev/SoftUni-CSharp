cf26cec266768ce7d6f076269c9de899be7f1846
LPenev <lppenev@gmail.com>
2023-06-16 10:07:41 +0200

    Task 06 completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/05. Supplies for School/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _05._Supplies_for_School
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from Console
+            int broiHimikali = int.Parse(Console.ReadLine());
+            int broiMarkeri = int.Parse(Console.ReadLine());
+            int litriPreparat = int.Parse(Console.ReadLine());
+            int prozentNamalenie = int.Parse(Console.ReadLine());
+            // Calc
+            double sum = broiHimikali * 5.8 + broiMarkeri * 7.2 + litriPreparat * 1.2;
+            sum -= sum * prozentNamalenie / 100;
+
+            // Show result
+            Console.WriteLine(sum);
+        }
+    }
+}
