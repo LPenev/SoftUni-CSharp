66ce47b99b0a6c8365db3fd80e4d756d82b2bfaf
LPenev <lppenev@gmail.com>
2023-06-17 14:32:56 +0300

    03. Animal Type Completed

---
+++ 01. Basic/03. Conditional Statements Advanced - Lab/03. Animal Type/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _03._Animal_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read text from console
+            string animal = Console.ReadLine();
+            // switch menu
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
