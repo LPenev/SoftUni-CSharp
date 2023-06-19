ff3b712d2490172b8d69c9c21ca8c8077612489b
LPenev <lppenev@gmail.com>
2023-06-19 10:08:52 +0200

    Task 07 completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/07. Food Delivery/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _07._Food_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from Console
+            int chickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int veganMenu = int.Parse(Console.ReadLine());
+
+            // Calc
+            double sumMenu = chickenMenu * 10.35 + fishMenu * 12.40 + veganMenu * 8.15;
+            double sum = sumMenu * 1.2 + 2.5;
+            // Show result
+            Console.WriteLine(sum);
+        }
+    }
+}
