52a44a7cdb98028cc1cb42086e0b681683f42a76
LPenev <lppenev@gmail.com>
2023-06-19 12:05:50 +0300

    08. Cinema Ticket Completed

---
+++ 01. Basic/03. Conditional Statements Advanced - Lab/08. Cinema Ticket/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _08._Cinema_Ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            string day = Console.ReadLine();
+
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Friday":
+                    Console.WriteLine("12");
+                    break;
+                case "Wednesday":
+                case "Thursday":
+                    Console.WriteLine("14");
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    Console.WriteLine("16");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
