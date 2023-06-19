60d5d49de82d94d74eb7728625e3beafed35e6b1
LPenev <lppenev@gmail.com>
2023-06-19 12:05:01 +0300

    07. Working Hours Completed

---
+++ 01. Basic/03. Conditional Statements Advanced - Lab/07. Working Hours/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07._Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            // calc
+            if (day != "Sunday" && hour >= 10 && hour <= 18)
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
