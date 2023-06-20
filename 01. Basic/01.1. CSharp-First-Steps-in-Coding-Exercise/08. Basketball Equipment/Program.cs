61a0c56bd4ea7f26d398a8182a15810592036629
LPenev <lppenev@gmail.com>
2023-06-20 10:10:03 +0200

    Task 08 Completed

---
+++ 01. Basic/01.1. CSharp-First-Steps-in-Coding-Exercise/08. Basketball Equipment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from Console
+            int yearTax = int.Parse(Console.ReadLine());
+
+            // Calc
+            double kecove = yearTax - yearTax * 0.4;
+            double ekip = kecove - kecove * 0.2;
+            double topka = ekip / 4;
+            double accessoari = topka / 5;
+            double sum = kecove + ekip + topka + accessoari + yearTax;
+
+            // Show result
+            Console.WriteLine(sum);
+        }
+    }
+}
