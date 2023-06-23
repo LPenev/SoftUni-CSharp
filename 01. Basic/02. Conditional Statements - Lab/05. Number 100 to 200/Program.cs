38300a5c19c494b0295f304a569c31c29c9aae7f
LPenev <lppenev@gmail.com>
2023-06-23 14:00:05 +0200

    Task 05 completed

---
+++ 01. Basic/02. Conditional Statements - Lab/05. Number 100 to 200/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05._Number_100_to_200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numm = int.Parse(Console.ReadLine());
+            if (numm < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (numm >= 100 && numm <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
