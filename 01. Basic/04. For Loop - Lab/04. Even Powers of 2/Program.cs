444e2f71c81fe8171213e940081b462597332d28
LPenev <lppenev@gmail.com>
2023-06-24 11:33:15 +0300

    04. Even Powers of 2 Completed

---
+++ 01. Basic/04. For Loop - Lab/04. Even Powers of 2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04._Even_Powers_of_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+
+            for (int i = 0; i <= n; i += 2)
+            {
+                // print num
+                Console.WriteLine(num);
+                // 2 of even degree
+                num = num * 2 * 2;
+            }
+        }
+    }
+}
