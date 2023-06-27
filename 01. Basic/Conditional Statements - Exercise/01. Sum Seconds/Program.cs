34faf13eaabeeb98e1514565261df276e46733ce
LPenev <lppenev@gmail.com>
2023-06-27 15:04:32 +0200

    Task 01 completed

---
+++ 01. Basic/Conditional Statements - Exercise/01. Sum Seconds/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Sum_Seconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input times in Seconds
+            int time1 = int.Parse(Console.ReadLine());
+            int time2 = int.Parse(Console.ReadLine());
+            int time3 = int.Parse(Console.ReadLine());
+
+            // Calc
+            int sumTimes = time1 + time2 + time3;
+            int hours = sumTimes / 60;
+            int minutes = sumTimes % 60;
+            // Print result
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
