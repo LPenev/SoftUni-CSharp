d612f64bd1a16ef57f7dae2d14730b39db051729
LPenev <lppenev@gmail.com>
2023-06-27 15:05:14 +0200

    Task 02 completed

---
+++ 01. Basic/Conditional Statements - Exercise/02. Bonus Score/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02._Bonus_Score
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input bonus digit
+            int digit = int.Parse(Console.ReadLine());
+            double bonus = 0;
+            // Calculate Bonus
+            if (digit <= 100)
+            {
+                bonus = 5;
+            }
+            else if (digit < 1000)
+            {
+                bonus = digit * 0.2;
+            }
+            else
+            {
+                bonus = digit * 0.1;
+            }
+            // Calc extra bonus
+            if (digit % 2 == 0)
+            {
+                bonus += 1;
+            }
+            else if (digit % 10 == 5)
+            {
+                bonus += 2;
+            }
+
+            // Print result
+            Console.WriteLine(bonus);
+            Console.WriteLine(digit + bonus);
+        }
+    }
+}
