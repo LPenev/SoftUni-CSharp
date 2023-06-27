1287f93fbb07edaac5874413835ef9388d8687e0
LPenev <lppenev@gmail.com>
2023-06-27 11:54:11 +0300

    10. Odd Even Sum Completed

---
+++ 01. Basic/04. For Loop - Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _10._Odd_Even_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int n = int.Parse(Console.ReadLine());
+            int y = 0;
+            double sumOdd = 0;
+            double sumEven = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0) sumEven += int.Parse(Console.ReadLine());
+                else sumOdd += int.Parse(Console.ReadLine());
+            }
+            if (sumOdd == sumEven) Console.WriteLine("Yes\nSum = " + sumOdd);
+            else Console.WriteLine("No\nDiff = " + Math.Abs(sumEven - sumOdd));
+        }
+    }
+}
