5338626fbe61c0fbb7588529ccd66f406806ea9e
LPenev <lppenev@gmail.com>
2023-06-28 15:18:13 +0200

    Task 06 completed

---
+++ 01. Basic/03. Conditional Statements - Exercise/06. World Swimming Record/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06._World_Swimming_Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // input variables
+            double record = double.Parse(Console.ReadLine());
+            double lengthMetri = double.Parse(Console.ReadLine());
+            double swimming = double.Parse(Console.ReadLine());
+
+            // Calc times
+            double extraTime = Math.Floor(lengthMetri / 15) * 12.5;
+            double times = lengthMetri * swimming + extraTime;
+
+            // show result
+            if (record > times)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {times:f2} seconds.");
+            }
+            else
+            {
+                times -= record;
+                Console.WriteLine($"No, he failed! He was {times:f2} seconds slower.");
+            }
+        }
+    }
+}
