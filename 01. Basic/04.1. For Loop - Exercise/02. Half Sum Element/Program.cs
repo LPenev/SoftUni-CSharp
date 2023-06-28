bf8d01bf4b430109df11abe05409fddeac1ce57d
LPenev <lppenev@gmail.com>
2023-06-28 14:21:57 +0300

    02. Half Sum Element Completed

---
+++ 01. Basic/04.1. For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace halfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int numMax = int.MinValue;
+            int readNum = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                readNum = int.Parse(Console.ReadLine());
+                sum += readNum;
+                if(readNum > numMax) numMax = readNum;
+            }
+            sum = sum - numMax;
+            if (sum == numMax) Console.WriteLine("Yes\nSum = " + sum);
+            else Console.WriteLine("No\nDiff = " + Math.Abs(numMax - sum));
+        }
+    }
+}
