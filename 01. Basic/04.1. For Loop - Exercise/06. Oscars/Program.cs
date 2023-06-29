e73d720b63207e1a33aeeecb6ca4aa39be591c36
LPenev <lppenev@gmail.com>
2023-06-29 11:37:19 +0300

    06. Oscars Completed

---
+++ 01. Basic/04.1. For Loop - Exercise/06. Oscars/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            double points = 0;
+            // read from console
+            string nameActior = Console.ReadLine();
+            double pointAkademy = double.Parse(Console.ReadLine());
+            int countEvaluative = int.Parse(Console.ReadLine());
+            points += pointAkademy;
+            
+            for (int i = 0; i < countEvaluative && points < 1250.5; i++)
+            {
+                string nameEvaluative = Console.ReadLine();
+                double pointEvaluative = double.Parse(Console.ReadLine());
+                points = points + pointEvaluative * nameEvaluative.Length / 2;
+            }
+            // print result
+            if (points > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {nameActior} got a nominee for leading role with {points:f1}!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {nameActior} you need {1250.5-points:f1} more!");
+            }
+
+        }
+    }
+}
