66ce858e011b41bc01d92dc97bbd0222a39030e7
LPenev <lppenev@gmail.com>
2023-06-29 11:35:39 +0300

    04. Clever Lily Completed

---
+++ 01. Basic/04.1. For Loop - Exercise/04. Clever Lily/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int toysCounter = 0;
+            int sum = 0;
+            int birthdayEvenCounter = 0;
+            // read from console
+            int age = int.Parse(Console.ReadLine());
+            double priceWashingMashine = double.Parse(Console.ReadLine());
+            int pricePerToy = int.Parse(Console.ReadLine());
+
+            //
+            for( int i = 1; i <= age; i++ )
+            {
+                // if Age is even or odd
+                if( i%2 == 0)
+                {
+                    birthdayEvenCounter++;
+                    sum += birthdayEvenCounter * 10 - 1;
+                }
+                else
+                {
+                    toysCounter++;
+                }
+            }
+            // calc sum of toys
+            sum = sum + ( toysCounter * pricePerToy);
+
+            // print result
+            if(sum >= priceWashingMashine)
+            {
+                Console.WriteLine($"Yes! {sum-priceWashingMashine:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {priceWashingMashine-sum:f2}");
+            }
+
+        }
+    }
+}
