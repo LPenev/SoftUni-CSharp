1f8eeb81a05135ff43964512dd9019b17d7e35c9
LPenev <lppenev@gmail.com>
2023-07-01 11:47:57 +0300

    02. Password Completed

---
+++ 01. Basic/05. While Loop - Lab/02. Password/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            string userName = Console.ReadLine();
+            string password = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            // while loop
+            while (password != input)
+            {
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {userName}!");
+        }
+    }
+}
