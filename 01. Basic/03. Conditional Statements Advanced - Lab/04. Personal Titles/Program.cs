ccbcacfbb18edd8b1af1ef5f9f3aa8343d90ef1d
LPenev <lppenev@gmail.com>
2023-07-01 11:18:13 +0200

    Task 04 completed

---
+++ 01. Basic/03. Conditional Statements Advanced - Lab/04. Personal Titles/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _04._Personal_Titles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from console
+            double age = double.Parse(Console.ReadLine());
+            string gener = Console.ReadLine();
+
+            // calc
+            if (age >= 16)
+            {
+                if (gener == "m")
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else
+            {
+                if (gener == "m")
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
+    }
+}
