0afe3d9f1757cba135e4b6b18869f1c8b5fd3ba0
LPenev <lppenev@gmail.com>
2023-07-01 11:48:30 +0300

    03. Sum Numbers Completed

---
+++ 01. Basic/05. While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int startNumer = int.Parse(Console.ReadLine());
+            //int input = int.Parse(Console.ReadLine());
+            int sum = 0;
+            // while loop
+            while(startNumer > sum)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            // print result
+            Console.WriteLine(sum);
+        }
+    }
+}
