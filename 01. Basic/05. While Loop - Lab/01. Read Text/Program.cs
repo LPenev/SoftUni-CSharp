92b47fc01fb0aa0f35d197ede472ab470abd68b1
LPenev <lppenev@gmail.com>
2023-07-01 11:47:21 +0300

    01. Read Text Completed

---
+++ 01. Basic/05. While Loop - Lab/01. Read Text/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace readText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            string input;
+
+            // while loop
+            while (true)
+            {
+                // read from console
+                input = Console.ReadLine();
+                if(input == "Stop")
+                {
+                    break;
+                }
+                Console.WriteLine(input);
+            }
+
+        }
+    }
+}
