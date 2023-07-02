641f6cfec8afe6ec0ee0e2cb3919e6246d78154a
LPenev <lppenev@gmail.com>
2023-07-02 11:20:31 +0200

    Task 06 completed

---
+++ 01. Basic/03. Conditional Statements Advanced - Lab/06. Number in Range/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06._Number_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from console
+            int digit = int.Parse(Console.ReadLine());
+            // calc
+            if (digit <= 100 && digit >= -100 && digit != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
