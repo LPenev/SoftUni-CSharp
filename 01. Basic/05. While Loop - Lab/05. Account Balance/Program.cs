55de6b0c3a436aec144045f6b3d286283316483f
LPenev <lppenev@gmail.com>
2023-07-03 11:31:37 +0300

    05. Account Balance Completed

---
+++ 01. Basic/05. While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace accountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // valriables
+            double sum = 0;
+            double currentMoney = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "NoMoreMoney")
+            {
+                currentMoney = double.Parse(input);
+                if(currentMoney < 0 ) 
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                sum += currentMoney;
+                Console.WriteLine($"Increase: {currentMoney:f2}");
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
