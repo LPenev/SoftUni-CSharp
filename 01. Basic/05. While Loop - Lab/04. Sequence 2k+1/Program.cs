26b253e0a2d42bed3951343ac19e9f87ff29616f
LPenev <lppenev@gmail.com>
2023-07-03 11:29:03 +0300

    04. Sequence 2k+1 Completed

---
+++ 01. Basic/05. While Loop - Lab/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sequence2k
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int nextNumer = 1;
+            // read from console
+            int numer = int.Parse(Console.ReadLine());
+
+            while (numer >= nextNumer)
+            {
+                Console.WriteLine(nextNumer);
+                nextNumer = nextNumer * 2 + 1;
+            }
+
+        }
+    }
+}
