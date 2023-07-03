5a90bc5349a7c162770ae621fa5ff2011318ce9e
LPenev <lppenev@gmail.com>
2023-07-03 11:33:23 +0300

    07. Min Number Completed

---
+++ 01. Basic/05. While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace minNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int minNum = int.MaxValue;
+            int inputNum;
+            // read string from console
+            string input = Console.ReadLine();
+
+            // while loop bis Stop
+            while (input != "Stop")
+            {
+                inputNum = int.Parse(input);
+                if (minNum > inputNum)
+                {
+                    minNum = inputNum;
+                }
+                input = Console.ReadLine();
+            }
+            // print result
+            Console.WriteLine(minNum);
+        }
+    }
+}
