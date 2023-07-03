1cce711db1a7760d06b911c4ba99fccfe2df4a57
LPenev <lppenev@gmail.com>
2023-07-03 11:32:25 +0300

    06. Max Number Completed

---
+++ 01. Basic/05. While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace maxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int maxNumber = int.MinValue;
+            int inputNumber = 0;
+            // read from console
+            string input = Console.ReadLine();
+            while(input != "Stop")
+            {
+                inputNumber = int.Parse(input);
+                if(inputNumber > maxNumber)
+                {
+                    maxNumber = inputNumber;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
