f88a0ff08ce342d765c7ce44091bd78f7ffe70aa
LPenev <lppenev@gmail.com>
2023-07-03 23:32:31 +0200

    Task 09 completed

--- 01. Basic/03. Conditional Statements Advanced - Lab/09. Fruit or Vegetable/Program.cs
+++ 01. Basic/03. Conditional Statements Advanced - Lab/09. Fruit or Vegetable/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            if(input == "banana" || input == "apple" || input == "kiwi" 
+                || input == "cherry" || input == "lemon" || input == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (input == "tomato" || input == "cucumber" || input == "pepper" || input == "carrot" )
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
         }
     }
 }
