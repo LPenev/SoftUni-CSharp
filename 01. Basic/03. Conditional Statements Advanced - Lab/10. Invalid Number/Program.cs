b01a9316b1468b5ee879d39d0f61e0d37d122e4c
LPenev <lppenev@gmail.com>
2023-07-03 09:23:53 +0200

    Task 10 completed

---
+++ 01. Basic/03. Conditional Statements Advanced - Lab/10. Invalid Number/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _10._Invalid_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int digit = int.Parse(Console.ReadLine());
+            //
+            if (digit != 0 && digit < 100 || digit > 200)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
