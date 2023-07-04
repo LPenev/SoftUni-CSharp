d6893c66b7c35e6bd82bf046a092369c82f89ae7
LPenev <lppenev@gmail.com>
2023-07-04 12:19:19 +0300

    08. Graduation Completed

---
+++ 01. Basic/05. While Loop - Lab/08. Graduation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            bool badGrade = false;
+            double grade = 0;
+            double sumGrade = 0;
+            string name = Console.ReadLine();
+
+            for( i=0; i<12; i++ )
+            {
+                grade = double.Parse(Console.ReadLine());
+                sumGrade += grade;
+                if( grade <= 3 )
+                {
+                    if(badGrade)
+                    {
+                        break;
+                    }
+                    badGrade = true;
+                }
+            }
+
+            if (badGrade)
+            {
+                Console.WriteLine($"{name} has been excluded at {i} grade");
+            }
+            else
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {sumGrade / i:f2}");
+            }
+        }
+    }
+}
