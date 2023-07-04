c3ce8068db47d74f40913819c98e50d7df2136b4
LPenev <lppenev@gmail.com>
2023-07-04 12:20:31 +0300

    01. Old Books Completed

---
+++ 01. Basic/05.1. While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace oldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            string book = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            while ( book != input )
+            {
+                if( input == "No More Books")
+                {
+                    book = input;
+                    break;
+                }
+                input = Console.ReadLine();
+                i++;
+            }
+            if( book == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {i} books.");
+            }
+            else
+            {
+                Console.WriteLine($"You checked {i} books and found it.");
+            }
+
+        }
+    }
+}
