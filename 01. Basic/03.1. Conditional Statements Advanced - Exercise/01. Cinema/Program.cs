bbcd579cd5fec43b22ce2042b2a729b9dcdd07c6
LPenev <lppenev@gmail.com>
2023-07-05 10:39:34 +0200

    Task 01 completed

---
+++ 01. Basic/03.1. Conditional Statements Advanced - Exercise/01. Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01._Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from console
+            string type = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            double income = 0;
+
+            // calc
+            if (type == "Premiere")
+            {
+                income = c * r * 12;
+            }
+            else if (type == "Normal")
+            {
+                income = c * r * 7.50;
+            }
+            else if (type == "Discount")
+            {
+                income = c * r * 5;
+            }
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
