ebe66f3d275ff7378b6983e6db96c5a6c78aca26
LPenev <lppenev@gmail.com>
2023-07-06 15:00:04 +0300

    06. Cake Completed

---
+++ 01. Basic/05.1. While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables 
+            string input = "";
+            // read from console width and length of cake
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            // calc how much piece have
+            int pieceCake = width * length;
+
+            while ( pieceCake > 0)
+            {
+                input = Console.ReadLine();
+                if( input == "STOP" )
+                {
+                    Console.WriteLine($"{pieceCake} pieces are left.");
+                    break;
+                }
+                pieceCake -= int.Parse(input);
+            }
+            if ( input != "STOP" ) Console.WriteLine($"No more cake left! You need {Math.Abs(pieceCake)} pieces more.");
+        }
+    }
+}
