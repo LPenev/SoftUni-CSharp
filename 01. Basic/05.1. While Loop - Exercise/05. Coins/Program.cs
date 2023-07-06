580936b1f41339682b426b384af8116f53e066be
LPenev <lppenev@gmail.com>
2023-07-06 14:59:15 +0300

    05. Coins Completed

---
+++ 01. Basic/05.1. While Loop - Exercise/05. Coins/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coinsCounter = 0;
+            decimal money = decimal.Parse(Console.ReadLine());
+
+            while ( money > 0 )
+            {
+                if( money >= 2 )
+                {
+                    money -= 2;
+                }
+                else if( money >= 1 )
+                {
+                    money -= 1;
+                }
+                else if( money >= 0.5m )
+                {
+                    money -= 0.5m;
+                }
+                else if ( money >= 0.2m )
+                {
+                    money -= 0.2m;
+                }
+                else if ( money >= 0.1m )
+                {
+                    money -= 0.1m;
+                }
+                else if ( money >= 0.05m )
+                {
+                    money -= 0.05m;
+                }
+                else if ( money >= 0.02m )
+                {
+                    money -= 0.02m;
+                }
+                else if ( money >= 0.01m )
+                {
+                    money -= 0.01m;
+                }
+
+                coinsCounter++;
+            }
+            Console.WriteLine(coinsCounter);
+        }
+    }
+}
