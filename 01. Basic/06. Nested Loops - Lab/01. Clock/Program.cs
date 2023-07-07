02b1efbda832ed2b58bdb02917ff4c0fd73edfb0
LPenev <lppenev@gmail.com>
2023-07-07 16:53:32 +0300

    01. Clock Completed

---
+++ 01. Basic/06. Nested Loops - Lab/01. Clock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+
+            for (int h = 0; h < 24; h++)
+            {
+                for (int m = 0; m < 60; m++)
+                {
+                    Console.WriteLine($"{h}:{m}");
+                }
+            }
+
+        }
+    }
+}
