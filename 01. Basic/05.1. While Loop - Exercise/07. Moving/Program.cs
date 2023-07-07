7790358a482ee39d67b9da2044ec735d310fdcaf
LPenev <lppenev@gmail.com>
2023-07-07 10:57:55 +0300

    07. Moving Completed

---
+++ 01. Basic/05.1. While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Runtime.InteropServices;
+
+namespace moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            string input = "";
+            // read from console
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            // calc room volume
+            int volume = width * length * height;
+
+            while (volume > 0)
+            {
+                input = Console.ReadLine();
+
+                if (input == "Done")
+                {
+                    // print result
+                    Console.WriteLine($"{volume} Cubic meters left.");
+                    break;
+                }
+                volume -= int.Parse(input);
+            }
+            // print result 
+            if ( input != "Done" ) Console.WriteLine($"No more free space! You need {Math.Abs(volume)} Cubic meters more.");
+        }
+    }
+}
