9136994878cb473b177abbf3e2e375a7a65da90b
LPenev <lppenev@gmail.com>
2023-07-08 12:28:45 +0300

    02. Multiplication Table Completed

---
+++ 01. Basic/06. Nested Loops - Lab/02. Multiplication Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int d = 1; d <= 10; d++)
+            {
+                for(int m  = 1; m <= 10; m++)
+                {
+                    Console.WriteLine($"{d} * {m} = {d*m}");
+                }
+            }
+
+        }
+    }
+}
