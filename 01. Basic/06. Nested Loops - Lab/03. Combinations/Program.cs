73af78b8ecb17bd03bfd9a8fe99186dd62e7a0cc
LPenev <lppenev@gmail.com>
2023-07-09 11:47:52 +0300

    03. Combinations Completed

---
+++ 01. Basic/06. Nested Loops - Lab/03. Combinations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int counter =0;
+            // read from console
+            int input = int.Parse(Console.ReadLine());
+
+            for (int a = 0; a <= input; a++)
+            {
+                for( int b = 0; b <= input; b++)
+                {
+                    for (int c = 0; c <= input; c++)
+                    {
+                        if (a+b+c ==  input)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
