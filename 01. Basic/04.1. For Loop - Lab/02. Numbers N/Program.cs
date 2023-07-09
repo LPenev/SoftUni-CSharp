d4628497d2a321807869dd8841657e0027e43acb
LPenev <lppenev@gmail.com>
2023-07-09 13:58:17 +0200

    Task 02 completed

---
+++ 01. Basic/04.1. For Loop - Lab/02. Numbers N/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02._Numbers_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
