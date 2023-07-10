2c0bbfdc1ccbc0470e944a47d22281ed473f7dc6
LPenev <lppenev@gmail.com>
2023-07-10 14:01:04 +0200

    Task 05 completed

---
+++ 01. Basic/04.1. For Loop - Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _05._Character_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            string text = Console.ReadLine();
+            int lenght = text.Length;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+        }
+    }
+}
