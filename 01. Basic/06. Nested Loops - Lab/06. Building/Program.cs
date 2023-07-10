e93350b5b747c9f5010dd564641136a846475054
LPenev <lppenev@gmail.com>
2023-07-10 11:15:20 +0300

    06. Building Completed

---
+++ 01. Basic/06. Nested Loops - Lab/06. Building/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int numberOfFloors = int.Parse(Console.ReadLine());
+            int numberOfRooms = int.Parse(Console.ReadLine());
+
+            for(int floor = numberOfFloors; floor >= 1; floor--)
+            {
+                for(int room = 0; room < numberOfRooms ; room++)
+                {
+                    if ( numberOfFloors == floor )
+                    {
+                        Console.Write($"L{floor}{room} ");
+                    }
+                    else if(floor%2 == 0)
+                    {
+                        Console.Write($"O{floor}{room} ");
+                    }
+                    else
+                    {
+                        Console.Write($"A{floor}{room} ");
+                    }
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
