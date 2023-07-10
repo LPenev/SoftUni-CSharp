e430db0486e38def49779c121dd1c193fe81f7c6
LPenev <lppenev@gmail.com>
2023-07-10 11:14:36 +0300

    05. Travelling Completed

---
+++ 01. Basic/06. Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable
+            string input;
+            string destination = "";
+            decimal sum = 0;
+            decimal budget = 1;
+            bool isEnd = false;
+
+            while (!isEnd)
+            {
+                if(sum >= budget)
+                {
+                    Console.WriteLine($"Going to {destination}!");
+                    sum = 0;
+                }
+                // read from console destination und budget
+                destination = Console.ReadLine();
+                if( destination == "End")
+                {
+                    isEnd = true;
+                    break;
+                }
+
+                budget = decimal.Parse(Console.ReadLine());
+
+                while (sum < budget)
+                {
+                    input = Console.ReadLine();
+                    if (input == "End")
+                    {
+                        isEnd = true;
+                        break;
+                    }
+                    sum += decimal.Parse(input);
+                }
+            }
+        }
+    }
+}
