8c1dcc2218b01e52878ad710d30dc8b57ab87892
LPenev <lppenev@gmail.com>
2023-07-11 12:07:47 +0300

    02. Equal Sums Even Odd Position Completed

---
+++ 01. Basic/06.1 Nested Loops - Exercise/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace equalSumsEvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int sumOdd = 0, sumEven = 0;
+            // read from console
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            // for loop all number between first und second
+            for ( int i = first ; i <= second ; i++ )
+            {
+                string current = i.ToString();
+
+                // for loop all digits of current number
+                for ( int j = 0 ; j < current.Length ; j++)
+                {
+                    int digit = int.Parse( current[j].ToString() );
+
+                    // check current digit is Even or Odd
+                    if( j%2 == 0)
+                    {
+                        sumEven += digit;
+                    }
+                    else
+                    {
+                        sumOdd += digit;
+                    }
+                }
+
+                if ( sumEven == sumOdd)
+                {
+                    Console.Write(i + " ");
+                }
+
+                sumEven = 0;
+                sumOdd = 0;
+            }
+        }
+    }
+}
