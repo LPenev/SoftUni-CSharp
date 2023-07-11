766500d1d770081406205c8fdb14e02eadde3f0b
LPenev <lppenev@gmail.com>
2023-07-11 14:02:15 +0200

    Task 06 completed

---
+++ 01. Basic/04.1. For Loop - Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _06._Vowels_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            string text = Console.ReadLine();
+            char z;
+            int sum = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                z = text[i];
+                switch (z)
+                {
+                    case 'a':
+                        sum += 1;
+                        break;
+                    case 'e':
+                        sum += 2;
+                        break;
+                    case 'i':
+                        sum += 3;
+                        break;
+                    case 'o':
+                        sum += 4;
+                        break;
+                    case 'u':
+                        sum += 5;
+                        break;
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
