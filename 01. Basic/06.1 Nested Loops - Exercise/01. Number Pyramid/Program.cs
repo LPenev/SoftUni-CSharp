5c42aa4aef7c2e64132539b314e1c8b461367753
LPenev <lppenev@gmail.com>
2023-07-11 12:07:16 +0300

    01. Number Pyramid Completed

---
+++ 01. Basic/06.1 Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace numberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int digit = 0;
+            int numPyramid = int.Parse(Console.ReadLine());
+
+            while (numPyramid > digit)
+            {
+                counter++;
+                for(int i = 0; i < counter ; i++)
+                {
+                    if ( numPyramid == digit )
+                    {
+                        break;
+                    }
+
+                    digit++;
+                    Console.Write(digit + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
