aa4a9c664af8252a9fee06f572b9c1c0cd1534db
LPenev <lppenev@gmail.com>
2023-07-12 09:04:13 +0200

    Task 08 completed

---
+++ 01. Basic/04.1. For Loop - Lab/08. Number sequence/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _08._Number_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int n = int.Parse(Console.ReadLine());
+            int y, numMin = int.MaxValue, numMax = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                y = int.Parse(Console.ReadLine());
+                if (y < numMin) numMin = y;
+                if (y > numMax) numMax = y;
+            }
+            // print result
+            Console.WriteLine($"Max number: {numMax}");
+            Console.WriteLine($"Min number: {numMin}");
+        }
+    }
+}
