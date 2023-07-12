98b99866e628861854d87ee6ad832b46051282e0
LPenev <lppenev@gmail.com>
2023-07-12 09:03:24 +0200

    Task 07completed

---
+++ 01. Basic/04.1. For Loop - Lab/07. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _07._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from console
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int y = int.Parse(Console.ReadLine());
+                sum += y;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
