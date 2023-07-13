1c0c92d07d8c89bcde39832b3dd8b7f750b31791
LPenev <lppenev@gmail.com>
2023-07-13 11:15:48 +0300

    05. Special Numbers Completed

---
+++ 01. Basic/06.1 Nested Loops - Exercise/05. Special Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace specialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit = int.Parse(Console.ReadLine());
+            bool isSpecial = true;
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                int m = i;
+                for (int j = 1; j <= 4; j++)
+                {
+                    int k = m % 10;
+                    if(k < 1 || digit%k != 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                    m /= 10;
+                }
+
+                if(isSpecial) { Console.Write(i + " "); }
+                isSpecial = true;
+            }
+        }
+    }
+}
