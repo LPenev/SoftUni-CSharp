9acb4d81bbbb8fa0125ba9d45c7dce199c266199
LPenev <lppenev@gmail.com>
2023-07-13 11:15:25 +0300

    04. Train The Trainers Completed

---
+++ 01. Basic/06.1 Nested Loops - Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace trainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumGrade = 0;
+            double sumAllGrade = 0;
+            int counter = 0;
+            int numberOfJuri = int.Parse(Console.ReadLine());
+            string nameOfPresentation = Console.ReadLine();
+
+            while (nameOfPresentation != "Finish")
+            {
+                for(int i = 0; i < numberOfJuri; i++)
+                {
+                    sumGrade += double.Parse(Console.ReadLine());
+                    counter++;
+                }
+
+                Console.WriteLine($"{nameOfPresentation} - {sumGrade / numberOfJuri:f2}.");
+                sumAllGrade += sumGrade;
+                sumGrade = 0;
+
+                nameOfPresentation = Console.ReadLine();
+            }
+            double avGrade = sumAllGrade / counter;
+            Console.WriteLine($"Student's final assessment is {avGrade:f2}.");
+        }
+    }
+}
