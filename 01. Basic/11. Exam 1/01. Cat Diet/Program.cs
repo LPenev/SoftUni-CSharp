0e33fc2067fa49e3c2fa5b6541834cc63920e386
LPenev <lppenev@gmail.com>
2023-07-14 12:14:29 +0300

    01. Cat Diet Completed

---
+++ 01. Basic/11. Exam 1/01. Cat Diet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace catDiet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int percentageOfFat = int.Parse(Console.ReadLine());
+            int percentageOfProteins = int.Parse(Console.ReadLine());
+            int percentageOfCarbohydrates = int.Parse(Console.ReadLine());
+            int totalCalories = int.Parse(Console.ReadLine());
+            int percentageOfWaterContent = int.Parse(Console.ReadLine());
+
+            // calclations
+            double gramFat = totalCalories * ((double)percentageOfFat / 100) / 9;
+            double gramProteins = totalCalories * ((double)percentageOfProteins /100) /4;
+            double gramCarbohydrates = totalCalories * ((double)percentageOfCarbohydrates /100) /4;
+
+            double weightOfFood = gramFat + gramProteins + gramCarbohydrates;
+            double caloriesPerGramOfFood = totalCalories / weightOfFood;
+            double percentage = 100 - percentageOfWaterContent;
+            double calories = caloriesPerGramOfFood * (percentage / 100);
+
+            // print result
+            Console.WriteLine($"{calories:f4}");
+        }
+    }
+}
