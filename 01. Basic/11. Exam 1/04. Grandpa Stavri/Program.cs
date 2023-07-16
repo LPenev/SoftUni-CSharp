e9cf923d7eb8c8c72ecff417748601aa511eae10
LPenev <lppenev@gmail.com>
2023-07-16 12:00:01 +0300

    04. Grandpa Stavri Completed

---
+++ 01. Basic/11. Exam 1/04. Grandpa Stavri/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace grandpaStavri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            double totalAmountOfBrandy = 0;
+            double sumOfDegreesForLiterOfBrandy = 0;
+
+            // read from console
+            int numberDays = int.Parse(Console.ReadLine());
+            // calclations
+            for(int i = 0; i < numberDays; i++)
+            {
+                double amountOfBrandy = double.Parse(Console.ReadLine());
+                double degreeOfDrink = double.Parse(Console.ReadLine());
+                totalAmountOfBrandy += amountOfBrandy;
+                sumOfDegreesForLiterOfBrandy += amountOfBrandy * degreeOfDrink;
+            }
+            double avgOfDegreesOfBrandy = sumOfDegreesForLiterOfBrandy / totalAmountOfBrandy;
+
+            // print result
+            Console.WriteLine($"Liter: {totalAmountOfBrandy:f2}");
+            Console.WriteLine($"Degrees: {avgOfDegreesOfBrandy:f2}");
+            if(avgOfDegreesOfBrandy < 38)
+            {
+                Console.WriteLine("Not good, you should baking!");
+            }else if (avgOfDegreesOfBrandy <= 42)
+            {
+                Console.WriteLine("Super!");
+            }
+            else
+            {
+                Console.WriteLine("Dilution with distilled water!");
+            }
+        }
+    }
+}
