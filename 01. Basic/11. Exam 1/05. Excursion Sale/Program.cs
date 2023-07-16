82473e552d4ca951113fc8449dcbe89d11763e33
LPenev <lppenev@gmail.com>
2023-07-16 12:00:51 +0300

    05. Excursion Sale Completed

---
+++ 01. Basic/11. Exam 1/05. Excursion Sale/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace excursionSale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            double seaPrice = 680;
+            double mountainPrice = 499;
+            double sumProfit = 0;
+            bool isAllSold = false;
+
+            // read from console
+            int numberOfSeaExcursions = int.Parse(Console.ReadLine());
+            int numberOfMountainExcursions = int.Parse(Console.ReadLine());
+
+            // calclations
+            while (true)
+            {
+                string packageName = Console.ReadLine();
+                if (packageName == "Stop") break;
+                
+                if(packageName == "sea" && numberOfSeaExcursions !=0)
+                {
+                    numberOfSeaExcursions--;
+                    sumProfit += seaPrice;
+                }
+
+                if (packageName == "mountain" && numberOfMountainExcursions != 0)
+                {
+                    numberOfMountainExcursions--;
+                    sumProfit += mountainPrice;
+                }
+                if (numberOfMountainExcursions == 0 && numberOfSeaExcursions == 0)
+                {
+                    isAllSold = true;
+                    break;
+                }
+            }
+
+            // print result
+            if (isAllSold)
+            {
+                Console.WriteLine("Good job! Everything is sold.");
+            }
+            Console.WriteLine($"Profit: {sumProfit} leva.");
+        }
+    }
+}
