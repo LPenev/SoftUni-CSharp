0b27c10bfbf0683615c7456cf2e5243c3beceda9
LPenev <lppenev@gmail.com>
2023-07-17 15:08:48 +0300

    01. Oscars ceremony Completed

---
+++ 01. Basic/10.2. Programming Basics Online Example Exam 2/01. Oscars ceremony/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace _01._Oscars_ceremony
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal hallRent = decimal.Parse(Console.ReadLine());
+
+            decimal priceOfStatuettes = hallRent * 0.7m;
+            decimal priceOfCatering = priceOfStatuettes * 0.85m;
+            decimal priceOfSounding = priceOfCatering * 0.5m;
+
+            decimal totalPrice = hallRent + priceOfStatuettes + priceOfCatering + priceOfSounding;
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
