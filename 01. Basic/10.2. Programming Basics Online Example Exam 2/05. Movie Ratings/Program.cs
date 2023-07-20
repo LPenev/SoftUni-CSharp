6dd5bfd5b11f4dd4a329bb4d7f7ee6d83154551d
LPenev <lppenev@gmail.com>
2023-07-20 11:40:42 +0300

    05. Movie Ratings Completed

---
+++ 01. Basic/10.2. Programming Basics Online Example Exam 2/05. Movie Ratings/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _05._Movie_Ratings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfMovies = int.Parse(Console.ReadLine());
+
+            // init vars
+            string bestMovieName = string.Empty;
+            double bestMovieRating = double.MinValue;
+
+            string worstMovieName = string.Empty;
+            double worstMovieRating = double.MaxValue;
+
+            double sumOfRating = 0;
+
+            // calc
+            for (int i = 0; i < numberOfMovies; i++)
+            {
+                string nameOfMovie = Console.ReadLine().Trim();
+                double ratingOfMovie = double.Parse(Console.ReadLine());
+
+                if (ratingOfMovie > bestMovieRating)
+                {
+                    bestMovieRating = ratingOfMovie;
+                    bestMovieName = nameOfMovie;
+                }
+                else if(ratingOfMovie < worstMovieRating)
+                {
+                    worstMovieRating = ratingOfMovie;
+                    worstMovieName = nameOfMovie;
+                }
+
+                sumOfRating += ratingOfMovie;
+            }
+
+            double avgRating = sumOfRating / numberOfMovies;
+
+            // print result
+            Console.WriteLine($"{bestMovieName} is with highest rating: {bestMovieRating:f1}");
+            Console.WriteLine($"{worstMovieName} is with lowest rating: {worstMovieRating:f1}");
+            Console.WriteLine($"Average rating: {avgRating:f1}");
+        }
+    }
+}
