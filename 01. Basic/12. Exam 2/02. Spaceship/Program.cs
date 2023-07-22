d300bdff20f23b67ab3cb3305af84a6acedae0eb
LPenev <lppenev@gmail.com>
2023-07-22 14:41:48 +0300

    02. Spaceship Completed

---
+++ 01. Basic/12. Exam 2/02. Spaceship/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace spaceShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            double widthShip = double.Parse(Console.ReadLine());
+            double lengthShip = double.Parse(Console.ReadLine());
+            double heightShip = double.Parse(Console.ReadLine());
+            double averageHeightOfAstronauts = double.Parse(Console.ReadLine());
+
+            // calculations
+            double volumeOfShip = widthShip * lengthShip * heightShip;
+            double volumeOfAustronautsRoom = (averageHeightOfAstronauts + 0.40) * 2 * 2;
+            double numberOfAustonauts = Math.Floor(volumeOfShip / volumeOfAustronautsRoom);
+
+            // print result
+            if(numberOfAustonauts < 3 ) 
+            {
+                Console.WriteLine("The spacecraft is too small.");
+            }
+            else if( numberOfAustonauts <= 10 )
+            {
+                Console.WriteLine($"The spacecraft holds {numberOfAustonauts:f0} astronauts.");
+            }
+            else if( numberOfAustonauts > 10)
+            {
+                Console.WriteLine("The spacecraft is too big.");
+            }
+        }
+    }
+}
