2c2ea5db06b8fec30d724a1ebd5f726cbe4de564
LPenev <lppenev@gmail.com>
2023-07-22 14:41:05 +0300

    01. Room Painting Completed

---
+++ 01. Basic/12. Exam 2/01. Room Painting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace roomPainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int numberCansPaint = int.Parse(Console.ReadLine());
+            int numberWallpaperRolls = int.Parse(Console.ReadLine());
+            double priceOfPairGloves = double.Parse(Console.ReadLine());
+            double priceBrush = double.Parse(Console.ReadLine());
+
+            // calculations
+            double sumPaint = numberCansPaint * 21.5;
+            double sumWallpaperRolls = numberWallpaperRolls * 5.2;
+            double sumGloves = Math.Ceiling(numberWallpaperRolls * 0.35) * priceOfPairGloves;
+            double sumBursh = Math.Floor(numberCansPaint * 0.48) * priceBrush;
+            double sumDelivery = (sumPaint + sumWallpaperRolls + sumGloves + sumBursh) / 15;
+
+            // print result
+            Console.WriteLine($"This delivery will cost {sumDelivery:f2} lv.");
+        }
+    }
+}
