a077c4209a17746e71a532d3f5f7ff667abd29af
LPenev <lppenev@gmail.com>
2023-07-23 13:33:39 +0300

    05. Christmas Gifts Completed

---
+++ 01. Basic/12. Exam 2/05. Christmas Gifts/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace christmasGifts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kids = 0;
+            int adults = 0;
+            // read from console
+            string input = Console.ReadLine();
+
+            while (input != "Christmas")
+            {
+                int age = int.Parse(input);
+                if(age <= 16)
+                {
+                    kids++;
+                }
+                else
+                {
+                    adults++;
+                }
+
+                // read from console
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Number of adults: {adults}");
+            Console.WriteLine($"Number of kids: {kids}");
+            Console.WriteLine($"Money for toys: {kids*5}");
+            Console.WriteLine($"Money for sweaters: {adults*15}");
+        }
+    }
+}
