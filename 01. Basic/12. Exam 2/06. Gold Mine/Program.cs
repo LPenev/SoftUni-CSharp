a33ae310264a0e2fe6d9590e1d48b23fbf82631f
LPenev <lppenev@gmail.com>
2023-07-23 13:34:06 +0300

    06. Gold Mine Completed

---
+++ 01. Basic/12. Exam 2/06. Gold Mine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace goldMine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLocations = int.Parse(Console.ReadLine());
+
+            for (sbyte i = 0; i < numberLocations; i++)
+            {
+                double gold = 0;
+                double averagePerDayGold = double.Parse(Console.ReadLine());
+                int numberDays = int.Parse(Console.ReadLine());
+                for (int d = 0; d < numberDays; d++)
+                {
+                    gold += double.Parse(Console.ReadLine());
+                }
+
+                double averageGold = averagePerDayGold * numberDays;
+
+                if (gold >= averageGold)
+                {
+                    Console.WriteLine($"Good job! Average gold per day: {gold / numberDays:f2}.");
+                }
+                else
+                {
+                    Console.WriteLine($"You need {(averageGold - gold) / numberDays:f2} gold.");
+                }
+            }
+        }
+    }
+}
