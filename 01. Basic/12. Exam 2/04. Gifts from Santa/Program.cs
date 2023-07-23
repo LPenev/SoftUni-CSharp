79371b4d6c5ee472ce50217e23fb1b4a5b5e9897
LPenev <lppenev@gmail.com>
2023-07-23 13:33:07 +0300

    04. Gifts from Santa Completed

---
+++ 01. Basic/12. Exam 2/04. Gifts from Santa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace giftFromSanta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            double addressNumber = 0;
+
+            // calculations
+            for (int i = m; i >= n; i--)
+            {
+                if(i%2 == 0)
+                {
+                    if(i%3 == 0)
+                    {
+                        if (i == s) break;
+
+                        // print result
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
