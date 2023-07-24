f9fcbc5df8e11e13be25f56c646db038b4a1d567
LPenev <lppenev@gmail.com>
2023-07-24 11:47:31 +0300

    01. Easter Lunch Completed

---
+++ 01. Basic/10.3. Programming Basics Online Example Exam 3/01. Easter Lunch/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Easter_Lunch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // init constants 
+            const double PriceKozunak = 3.20;
+            const double PriceEggsCrustFrom12Eggs = 4.35;
+            const double PriceCookiesPerKilogram = 5.40;
+            const double PriceDyeForEggsPerEgg = 0.15;
+
+            // read user inputs from console
+            int numberOfKuzunaks = int.Parse(Console.ReadLine());
+            int numberOfEggshells = int.Parse(Console.ReadLine());
+            int kilogramsOfCookies = int.Parse(Console.ReadLine());
+
+            //
+            double sum = PriceKozunak * numberOfKuzunaks;
+            sum += PriceEggsCrustFrom12Eggs * numberOfEggshells;
+            sum += PriceDyeForEggsPerEgg * numberOfEggshells * 12;
+            sum += PriceCookiesPerKilogram * kilogramsOfCookies;
+
+            // print result
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
