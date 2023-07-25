af6a909134c95d822fd31d4c3bc6ac78d7100316
LPenev <lppenev@gmail.com>
2023-07-25 11:11:23 +0300

    02.  Easter Party Completed

---
+++ 01. Basic/10.3. Programming Basics Online Example Exam 3/02.  Easter Party/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.__Easter_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read inputs 
+            int numberOfGuests = int.Parse(Console.ReadLine());
+            double priceOfEnvelopeForPerson = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            // calcs
+
+            if (numberOfGuests >= 10 && numberOfGuests <= 15)
+            {
+                priceOfEnvelopeForPerson *= 0.85;
+            }
+            else if (numberOfGuests <= 20 && numberOfGuests > 10)
+            {
+                priceOfEnvelopeForPerson *= 0.8;
+            }
+            else if (numberOfGuests > 20)
+            {
+                priceOfEnvelopeForPerson *= 0.75;
+            }
+
+            double sum = numberOfGuests * priceOfEnvelopeForPerson + budget * 0.1;
+
+            if (sum > budget)
+            {
+                Console.WriteLine($"No party! {sum - budget:f2} leva needed.");
+            }
+            else
+            {
+                Console.WriteLine($"It is party time! {budget - sum:f2} leva left.");
+            }
+        }
+    }
+}
