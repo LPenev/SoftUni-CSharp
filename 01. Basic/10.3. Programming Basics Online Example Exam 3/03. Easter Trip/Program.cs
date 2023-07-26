b9347921bee32ccdb5c4fb81162c6b6ef1b2c3f9
LPenev <lppenev@gmail.com>
2023-07-26 12:07:01 +0300

    03. Easter Trip Completed

---
+++ 01. Basic/10.3. Programming Basics Online Example Exam 3/03. Easter Trip/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _03._Easter_Trip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read inputs from console 
+            string destination = Console.ReadLine().Trim();
+            string dateOfReservation = Console.ReadLine().Trim();
+            int numberOfNights = int.Parse(Console.ReadLine());
+
+            // calcs
+            double priceOfNight = 0;
+
+            switch (destination)
+            {
+                case "France":
+                    switch (dateOfReservation)
+                    {
+                        case "21-23":
+                            priceOfNight = 30;
+                            break;
+                        case "24-27":
+                            priceOfNight = 35;
+                            break;
+                        case "28-31":
+                            priceOfNight = 40;
+                            break;
+                    }
+                    break;
+
+                case "Italy":
+                    switch (dateOfReservation)
+                    {
+                        case "21-23":
+                            priceOfNight = 28;
+                            break;
+                        case "24-27":
+                            priceOfNight = 32;
+                            break;
+                        case "28-31":
+                            priceOfNight = 39;
+                            break;
+                    }
+                    break;
+
+                case "Germany":
+                    switch (dateOfReservation)
+                    {
+                        case "21-23":
+                            priceOfNight = 32;
+                            break;
+                        case "24-27":
+                            priceOfNight = 37;
+                            break;
+                        case "28-31":
+                            priceOfNight = 43;
+                            break;
+                    }
+                    break;
+            }
+
+            double sum = priceOfNight * numberOfNights;
+
+            // print result
+            Console.WriteLine($"Easter trip to {destination} : {sum:f2} leva.");
+        }
+    }
+}
