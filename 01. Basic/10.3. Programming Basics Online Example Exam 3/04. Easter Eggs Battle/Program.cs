bb0e0bb8e11fc77720fb455aa47ba151a2eea1df
LPenev <lppenev@gmail.com>
2023-07-27 11:18:28 +0300

    04. Easter Eggs Battle Completed

---
+++ 01. Basic/10.3. Programming Basics Online Example Exam 3/04. Easter Eggs Battle/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _04._Easter_Eggs_Battle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from Console 
+            int numberOfEggsFirstPlayer = int.Parse(Console.ReadLine());
+            int numberOfEggsSecondPlayer = int.Parse(Console.ReadLine());
+
+            string input;
+
+            while ((input = Console.ReadLine().Trim()) != "End")
+            {
+
+
+                if (input == "one")
+                {
+                    numberOfEggsSecondPlayer--;
+                }
+                else if (input == "two")
+                {
+                    numberOfEggsFirstPlayer--;
+                }
+
+                if (numberOfEggsFirstPlayer < 1)
+                {
+                    Console.WriteLine($"Player one is out of eggs. Player two has {numberOfEggsSecondPlayer} eggs left.");
+                    return;
+                }
+                else if (numberOfEggsSecondPlayer < 1)
+                {
+                    Console.WriteLine($"Player two is out of eggs. Player one has {numberOfEggsFirstPlayer} eggs left.");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Player one has {numberOfEggsFirstPlayer} eggs left.");
+            Console.WriteLine($"Player two has {numberOfEggsSecondPlayer} eggs left.");
+        }
+    }
+}
