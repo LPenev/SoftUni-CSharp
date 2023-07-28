4da18e316e25ebd96d0cbb09b638716292b3b404
LPenev <lppenev@gmail.com>
2023-07-28 10:54:33 +0300

    05. Easter Eggs Completed

---
+++ 01. Basic/10.3. Programming Basics Online Example Exam 3/05. Easter Eggs/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _05._Easter_Eggs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberfDyedEggs = int.Parse(Console.ReadLine());
+            
+            // init vars 
+            int countRedEggs = 0;
+            int countOrangeEggs = 0;
+            int countBlueEggs = 0;
+            int countGreenEggs = 0;
+
+            int maxCountEggs = int.MinValue;
+            string MaxCountColor = "";
+
+            for (int i = 0; numberfDyedEggs > i; i++)
+            {
+                string currentColor = Console.ReadLine().Trim();
+                switch (currentColor)
+                {
+                    case "red":
+                        countRedEggs++;
+                        break;
+                    case "orange":
+                        countOrangeEggs++;
+                        break;
+                    case "blue":
+                        countBlueEggs++;
+                        break;
+                    case "green":
+                        countGreenEggs++;
+                        break;
+                }
+            }
+
+            if (countRedEggs > maxCountEggs)
+            {
+                maxCountEggs = countRedEggs;
+                MaxCountColor = "red";
+            }
+            if (countOrangeEggs > maxCountEggs)
+            {
+                maxCountEggs = countOrangeEggs;
+                MaxCountColor = "orange";
+            }
+            if (countBlueEggs > maxCountEggs)
+            {
+                maxCountEggs = countBlueEggs;
+                MaxCountColor = "blue";
+            }
+            if (countGreenEggs > maxCountEggs)
+            {
+                maxCountEggs = countGreenEggs;
+                MaxCountColor = "green";
+            }
+
+            // print result
+            Console.WriteLine($"Red eggs: {countRedEggs}");
+            Console.WriteLine($"Orange eggs: {countOrangeEggs}");
+            Console.WriteLine($"Blue eggs: {countBlueEggs}");
+            Console.WriteLine($"Green eggs: {countGreenEggs}");
+            Console.WriteLine($"Max eggs: {maxCountEggs} -> {MaxCountColor}");
+        }
+    }
+}
