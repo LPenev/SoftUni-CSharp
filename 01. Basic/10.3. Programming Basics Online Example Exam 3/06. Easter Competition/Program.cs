4aba1f97e9a9b11260d6796a7f26a3568f274e1d
LPenev <lppenev@gmail.com>
2023-07-29 11:44:19 +0300

    06. Easter Competition Completed

---
+++ 01. Basic/10.3. Programming Basics Online Example Exam 3/06. Easter Competition/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _06._Easter_Competition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numberOfKozunaks = int.Parse(Console.ReadLine());
+            string bestBackerName = string.Empty;
+            int bestBackerPoints = int.MinValue;
+
+
+            for (int i = 0; i < numberOfKozunaks; i++)
+            {
+                string backerName = Console.ReadLine().Trim();
+                int backerPoints = 0;
+                string input = string.Empty;
+
+                while((input = Console.ReadLine().Trim()) != "Stop")
+                {
+                    backerPoints += int.Parse(input);
+                }
+
+                Console.WriteLine($"{backerName} has {backerPoints} points.");
+
+                if (backerPoints > bestBackerPoints)
+                {
+                    bestBackerName = backerName;
+                    bestBackerPoints = backerPoints;
+                    Console.WriteLine($"{bestBackerName} is the new number 1!");
+                }
+            }
+
+            Console.WriteLine($"{bestBackerName} won competition with {bestBackerPoints} points!");
+        }
+    }
+}
