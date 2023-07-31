1cfcdb940759342c339b3e8b11e519aae4316247
LPenev <lppenev@gmail.com>
2023-07-31 12:11:23 +0300

    01. Series Calculator Completed

---
+++ 01. Basic/10.4. Programming Basics Online Example Exam 4/01. Series Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace serieCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameSerial = Console.ReadLine();
+            int numberSeasons = int.Parse(Console.ReadLine());
+            int numberEpisodes = int.Parse(Console.ReadLine());
+            double timeOfEpisode = double.Parse(Console.ReadLine());
+            double totalDuration = 0;
+            double realTimeOfEpisodesWithAdv = timeOfEpisode * 1.2;
+
+            totalDuration = realTimeOfEpisodesWithAdv * numberEpisodes * numberSeasons + numberSeasons * 10;
+            
+            Console.WriteLine($"Total time needed to watch the {nameSerial} series is {totalDuration:f0} minutes.");
+        }
+    }
+}
