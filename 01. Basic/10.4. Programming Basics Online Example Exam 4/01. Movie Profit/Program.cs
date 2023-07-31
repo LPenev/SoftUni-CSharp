667953b4d0f4383d929c1d9a4923557b0496d50c
LPenev <lppenev@gmail.com>
2023-07-31 12:10:43 +0300

    01. Movie Profit Completed

---
+++ 01. Basic/10.4. Programming Basics Online Example Exam 4/01. Movie Profit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace movieProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfFilm = Console.ReadLine();
+            int numberDays = int.Parse(Console.ReadLine());
+            int numberTickets = int.Parse(Console.ReadLine());
+            double priceOfTicket = double.Parse(Console.ReadLine());
+            int procentForCinema = int.Parse(Console.ReadLine());
+
+            double sumIncome = numberDays * numberTickets * priceOfTicket;
+            double profit = sumIncome - sumIncome * procentForCinema / 100;
+
+            Console.WriteLine($"The profit from the movie {nameOfFilm} is {profit:f2} lv.");
+
+        }
+    }
+}
