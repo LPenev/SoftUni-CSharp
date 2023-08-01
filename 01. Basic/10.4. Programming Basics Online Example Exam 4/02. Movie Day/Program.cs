cd6a4ead501cf353d05efee25fa2b77f3d7fbed5
LPenev <lppenev@gmail.com>
2023-08-01 11:18:02 +0300

    02. Movie Day Completed

---
+++ 01. Basic/10.4. Programming Basics Online Example Exam 4/02. Movie Day/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace movieDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeForMovies = int.Parse(Console.ReadLine());
+            int numberScene = int.Parse(Console.ReadLine());
+            int timePerScene = int.Parse(Console.ReadLine());
+
+            double sumTime = timeForMovies * 0.15;
+            double sumScene = numberScene * timePerScene;
+            double total = sumTime + sumScene;
+
+            if ( total < timeForMovies ) 
+            {
+                double leftTime = timeForMovies - total;
+                Console.WriteLine($"You managed to finish the movie on time! You have {leftTime:f0} minutes left!");
+            }
+            else
+            {
+                double leftTime = total - timeForMovies;
+                Console.WriteLine($"Time is up! To complete the movie you need {leftTime:f0} minutes.");
+            }
+        }
+    }
+}
