b72503f167ecd1d3699810e7c21828d1381f142e
LPenev <lppenev@gmail.com>
2023-08-02 10:43:40 +0300

    03. Film Premiere Completed

---
+++ 01. Basic/10.4. Programming Basics Online Example Exam 4/03. Film Premiere/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace filmPremiere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            string nameOfFilm = Console.ReadLine();
+            string packet = Console.ReadLine();
+            int numberTickets = int.Parse(Console.ReadLine());
+            double priceTicket =0;
+
+            // calc
+            switch (nameOfFilm)
+            {
+                case "John Wick":
+                    if(packet == "Drink")
+                    {
+                        priceTicket = 12;
+                    }
+                    else if(packet == "Popcorn")
+                    {
+                        priceTicket = 15;
+                    }
+                    else if(packet == "Menu")
+                    {
+                        priceTicket = 19;
+                    }
+                    break;
+                case "Star Wars":
+                    if (packet == "Drink")
+                    {
+                        priceTicket = 18;
+                    }
+                    else if (packet == "Popcorn")
+                    {
+                        priceTicket = 25;
+                    }
+                    else if (packet == "Menu")
+                    {
+                        priceTicket = 30;
+                    }
+
+                    if(numberTickets >= 4) { priceTicket *= 0.7; }
+                    
+                    break;
+
+                case "Jumanji":
+                    if (packet == "Drink")
+                    {
+                        priceTicket = 9;
+                    }
+                    else if (packet == "Popcorn")
+                    {
+                        priceTicket = 11;
+                    }
+                    else if (packet == "Menu")
+                    {
+                        priceTicket = 14;
+                    }
+
+                    if ( numberTickets == 2) { priceTicket *= 0.85; }
+
+                    break;
+            }
+            double totalSum = priceTicket * numberTickets;
+            Console.WriteLine($"Your bill is {totalSum:f2} leva.");
+        }
+    }
+}
