431d68b0431a0a2b3560d4dd6ee7ff52282d032e
LPenev <lppenev@gmail.com>
2023-08-03 11:29:28 +0300

    04. Movie Stars Completed

---
+++ 01. Basic/10.4. Programming Basics Online Example Exam 4/04. Movie Stars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace movieStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budgetActiors = double.Parse(Console.ReadLine());
+            string nameActior = Console.ReadLine();
+
+            while (nameActior != "ACTION")
+            {
+                if (nameActior.Length <= 15)
+                {
+                    double priceCurrentActior = double.Parse(Console.ReadLine());
+                    budgetActiors -= priceCurrentActior;
+                }
+                else
+                {
+                    budgetActiors -= budgetActiors * 0.2;
+                }
+                if(budgetActiors <=0) { break; }
+                nameActior = Console.ReadLine();
+            }
+
+            if (budgetActiors > 0)
+            {
+                Console.WriteLine($"We are left with {budgetActiors:f2} leva.");
+            }
+            else
+            {
+                Console.WriteLine($"We need {Math.Abs(budgetActiors):f2} leva for our actors.");
+            }
+        }
+    }
+}
