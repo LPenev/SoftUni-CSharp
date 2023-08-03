32c44e5f4f558a5902c50dfca6965f2d428a293d
LPenev <lppenev@gmail.com>
2023-08-03 11:28:57 +0300

    04. Cinema Completed

---
+++ 01. Basic/10.4. Programming Basics Online Example Exam 4/04. Cinema/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacityHall = int.Parse(Console.ReadLine());
+            string people = Console.ReadLine();
+            int sumPeople = 0;
+            double sum = 0;
+
+            while ( people != "Movie time!")
+            {
+                int currentPeople = int.Parse(people);
+                sumPeople += currentPeople;
+
+                // check hall capacity    
+                if(capacityHall < sumPeople){ 
+                    Console.WriteLine("The cinema is full.");
+                    break;
+                }
+
+                // sum of Tickets
+                sum += currentPeople * 5;
+                // discount
+                if (currentPeople % 3 == 0)
+                {
+                    sum -= 5;
+                }
+                // read from console
+                people = Console.ReadLine();
+            }
+            // print result
+            if(capacityHall >= sumPeople)
+            {
+                int freeseats = capacityHall - sumPeople;
+                Console.WriteLine($"There are {freeseats} seats left in the cinema.");
+            }
+            Console.WriteLine($"Cinema income - {sum} lv.");
+        }
+    }
+}
