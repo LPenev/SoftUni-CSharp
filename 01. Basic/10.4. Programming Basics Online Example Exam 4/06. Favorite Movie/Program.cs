0e2d0546da513c4443c0c072bd7ba7d2e211fd55
LPenev <lppenev@gmail.com>
2023-08-05 11:44:44 +0300

    06. Favorite Movie Completed

---
+++ 01. Basic/10.4. Programming Basics Online Example Exam 4/06. Favorite Movie/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace favoriteMovie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfFilm = Console.ReadLine();
+            int counterFilms = 0;
+            int favoriteFilmPoints = 0;
+            string favoriteFilmName = "";
+
+            while (nameOfFilm != "STOP")
+            {
+                int sumPoints = 0;
+                counterFilms++;
+                if(counterFilms == 7) { Console.WriteLine("The limit is reached."); break; }
+                int lengthNameOfFim = nameOfFilm.Length;
+                for(int i = 0; i < lengthNameOfFim; i++)
+                {
+                    char ch = nameOfFilm[i];
+                    sumPoints += (int)ch;
+                    if((int)ch < 91 && (int)ch > 64) { sumPoints -= nameOfFilm.Length; } 
+                    else if((int)ch < 123 && (int)ch > 96) { sumPoints -= (nameOfFilm.Length * 2); }
+                }
+                if(sumPoints > favoriteFilmPoints)
+                {
+                    favoriteFilmPoints = sumPoints;
+                    favoriteFilmName = nameOfFilm;
+                }
+                nameOfFilm = Console.ReadLine() ;
+            }
+            
+            Console.WriteLine($"The best movie for you is {favoriteFilmName} with {favoriteFilmPoints} ASCII sum.");
+        }
+    }
+}
