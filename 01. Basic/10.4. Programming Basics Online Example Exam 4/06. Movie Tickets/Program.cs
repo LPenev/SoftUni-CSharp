150cd305fdad71baee8dd77e34de3c4d03971755
LPenev <lppenev@gmail.com>
2023-08-05 11:45:04 +0300

    06. Movie Tickets Completed

---
+++ 01. Basic/10.4. Programming Basics Online Example Exam 4/06. Movie Tickets/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace movieTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from console
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int n2 = n / 2;
+            // 1 symbol
+            for (int i = a1;  i < a2; i++)
+            {
+                if(i%2 != 0) {
+
+                    char ch1 = Convert.ToChar(i);
+                    int ch2 = 0; int ch3 = 0; int ch4 = 0;
+
+                    // 2 symbol
+                    for (int j = 1; j < n; j++)
+                    {
+                        ch2 = j;
+                        // 3 symbol
+                        for (int k = 1; k < n2; k++)
+                        {
+                            ch3 = k;
+
+                            // 4 symbol
+                            ch4 = (int)ch1;
+                            int sumCh2to4 = ch2 + ch3 + ch4;
+                            if (sumCh2to4 % 2 != 0)
+                            {
+                                Console.WriteLine($"{ch1}-{ch2}{ch3}{ch4}");
+                            }
+                        }
+                    }
+
+                }
+            }
+
+           // Console.WriteLine("{Символ 1}-{Символ 2}{Символ 3}{Символ  4}");
+        }
+    }
+}
