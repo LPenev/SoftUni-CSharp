c84a4cfabf9f930316032b16abbaf37fe1f64494
LPenev <lppenev@gmail.com>
2023-08-07 12:14:59 +0300

    01. Pool Day Completed

---
+++ 01. Basic/10.5, Programming Basics Online Example Exam 5/01. Pool Day/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace poolDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            
+
+            // read from console
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            double entranceFee = double.Parse(Console.ReadLine());
+            double priceOneSunLounger = double.Parse(Console.ReadLine());
+            double priceOneUmbrella = double.Parse(Console.ReadLine());
+
+            // calculation
+            double numberSunLounger = Math.Ceiling((double)numberOfPeople * 0.75);
+            double numberSunUmbrela = Math.Ceiling((double)numberOfPeople / 2);
+            double sumMoney = entranceFee * numberOfPeople;
+            sumMoney += numberSunLounger * priceOneSunLounger;
+            sumMoney += numberSunUmbrela * priceOneUmbrella;
+            // print result
+            Console.WriteLine($"{sumMoney:f2} lv.");
+        }
+    }
+}
