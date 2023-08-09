a58d72e52d673b12f2fa18c03ed73dfee66e0017
LPenev <lppenev@gmail.com>
2023-08-09 11:27:20 +0300

    04. Club Completed

---
+++ 01. Basic/10.5, Programming Basics Online Example Exam 5/04. Club/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            double sumMoneyOrders = 0;
+            // read from console
+            double clubProfit = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            // calculations
+            while ( input != "Party!")
+            {
+                int numberOfCocktailsForOrder = int.Parse(Console.ReadLine());
+                double order = input.Length * numberOfCocktailsForOrder;
+                if(order%2 != 0)
+                {
+                    sumMoneyOrders += order * 0.75;
+                }
+                else 
+                {
+                    sumMoneyOrders += order;
+                }
+                if(sumMoneyOrders >= clubProfit) { break; }
+                input = Console.ReadLine();
+            }
+
+            // print result
+            if( input == "Party!")
+            {
+                Console.WriteLine($"We need {clubProfit-sumMoneyOrders:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Target acquired.");
+            }
+            Console.WriteLine($"Club income - {sumMoneyOrders:f2} leva.");
+        }
+    }
+}
