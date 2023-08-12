f8f9f6f24e7dc95f95178dc6e148dcfb8c801ee5
LPenev <lppenev@gmail.com>
2023-08-12 11:27:21 +0300

    02. Cat Walking Completed

---
+++ 01. Basic/10.6. Programming Basics Online Example Exam 6/02. Cat Walking/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace catWalking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int sumTime = 0;
+            int brunedCalories = 0;
+            // read from console
+            int timeTrip = int.Parse(Console.ReadLine());
+            int numberTripPerDay = int.Parse(Console.ReadLine());
+            int incomenCalorie = int.Parse(Console.ReadLine());
+
+            // calc
+            sumTime = numberTripPerDay * timeTrip;
+            brunedCalories = sumTime * 5;
+            int halfCaloriesForTag = incomenCalorie / 2;
+
+            if (brunedCalories >= halfCaloriesForTag)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {brunedCalories}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {brunedCalories}.");
+            }
+        }
+    }
+}
