240a70519ff639a9f490bf527cd602c1003d0a25
LPenev <lppenev@gmail.com>
2023-08-12 11:26:53 +0300

    01. Change Bureau Completed

---
+++ 01. Basic/10.6. Programming Basics Online Example Exam 6/01. Change Bureau/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace changeBureau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int bitcoinToBGN = 1168;
+            double chinaJoanaToUSD = 0.15; // USD
+            double usdToBGN = 1.76; // BGN
+            double euroToBGN = 1.95; // BGN
+            // read from console
+            int bitcoin = int.Parse(Console.ReadLine());
+            double chinaJoana = double.Parse(Console.ReadLine());
+            double comision = double.Parse(Console.ReadLine());
+            // calc
+            double sumBGN = bitcoinToBGN * bitcoin;
+            sumBGN += (chinaJoana * chinaJoanaToUSD) * usdToBGN;
+            double sumEuro = sumBGN / euroToBGN;
+            sumEuro -= sumEuro * (comision / 100);
+
+            // print result
+            Console.WriteLine($"{sumEuro:f2}");
+        }
+    }
+}
