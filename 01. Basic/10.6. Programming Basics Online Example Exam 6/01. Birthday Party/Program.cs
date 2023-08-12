193aa9503f0df8fe7b56c8c42db4bf91dfe9b9de
LPenev <lppenev@gmail.com>
2023-08-12 11:26:25 +0300

    01. Birthday Party Completed

---
+++ 01. Basic/10.6. Programming Basics Online Example Exam 6/01. Birthday Party/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace birthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double renteHall = double.Parse(Console.ReadLine());
+            // calc
+            double torte = renteHall * 0.2;
+            double drinks = torte * 0.55;
+            double animaor = renteHall / 3;
+            double sum = renteHall + torte + drinks + animaor;
+
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
