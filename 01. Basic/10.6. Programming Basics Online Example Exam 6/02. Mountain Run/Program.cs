c7720251a065c79f6d05fd2a94efc398e065b4e0
LPenev <lppenev@gmail.com>
2023-08-12 11:27:49 +0300

    02. Mountain Run Completed

---
+++ 01. Basic/10.6. Programming Basics Online Example Exam 6/02. Mountain Run/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace mountainRum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            double sumTimeSecunde = 0;
+            // read from console
+            double recordTime = double.Parse(Console.ReadLine());
+            double PathLenght = double.Parse(Console.ReadLine());
+            double timeProMeter = double.Parse(Console.ReadLine());
+
+            // calc
+            sumTimeSecunde = PathLenght * timeProMeter;
+            sumTimeSecunde += Math.Floor((PathLenght / 50)) * 30;
+
+            if (recordTime > sumTimeSecunde)
+            {
+                Console.WriteLine($"Yes! The new record is {sumTimeSecunde:f2} seconds.");
+            }else
+            {
+                Console.WriteLine($"No! He was {sumTimeSecunde- recordTime:f2} seconds slower.");
+            }
+        }
+    }
+}
