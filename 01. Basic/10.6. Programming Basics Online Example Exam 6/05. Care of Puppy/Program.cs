3c9890144c89b8a7f87a97d437c5b9df0b00711d
LPenev <lppenev@gmail.com>
2023-08-15 11:31:10 +0300

    05. Care of Puppy Completed

---
+++ 01. Basic/10.6. Programming Basics Online Example Exam 6/05. Care of Puppy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace careOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int gramsPuppyEatAtEachMeal = 0;
+
+            // read from console
+            int amountOfPuppyFoodPurchasedKg = int.Parse(Console.ReadLine());
+            // convert kg to gr.
+            amountOfPuppyFoodPurchasedKg *= 1000;
+
+            string input = Console.ReadLine();
+
+            // calc
+            while (input != "Adopted")
+            {
+                gramsPuppyEatAtEachMeal += int.Parse(input);
+                // read from console
+                input = Console.ReadLine();
+            }
+
+            // print result
+            if(amountOfPuppyFoodPurchasedKg >= gramsPuppyEatAtEachMeal)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {amountOfPuppyFoodPurchasedKg-gramsPuppyEatAtEachMeal} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {gramsPuppyEatAtEachMeal-amountOfPuppyFoodPurchasedKg} grams more.");
+            }
+
+        }
+    }
+}
