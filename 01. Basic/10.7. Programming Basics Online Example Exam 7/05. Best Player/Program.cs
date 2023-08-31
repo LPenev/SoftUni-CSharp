d07c99a72a83765bfbc8846944f29d1df44426cf
LPenev <lppenev@gmail.com>
2023-08-31 11:13:05 +0300

    05. Best Player Completed

---
+++ 01. Basic/10.7. Programming Basics Online Example Exam 7/05. Best Player/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _05._Best_Player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // init vars
+            string bestPlayer = "";
+            int bestPlayerGoals = 0;
+
+            string player = "";
+
+            while ((player = Console.ReadLine().Trim()) != "END")
+            {
+                int goals = int.Parse(Console.ReadLine());
+
+                if(goals > bestPlayerGoals)
+                {
+                    bestPlayerGoals = goals;
+                    bestPlayer = player;
+                }
+
+                if(bestPlayerGoals >= 10) break;
+            }
+
+            Console.WriteLine($"{bestPlayer} is the best player!");
+
+            if (bestPlayerGoals >= 3)
+            {
+                Console.WriteLine($"He has scored {bestPlayerGoals} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {bestPlayerGoals} goals.");
+            }
+            
+        }
+    }
+}
