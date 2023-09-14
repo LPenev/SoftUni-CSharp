cf39c438835b4a57cca52197bfe114d6367183c9
LPenev <lppenev@gmail.com>
2023-09-14 11:28:18 +0300

    09. Sum of Odd Num Completed

---
+++ 02. Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/09. Sum of Odd Num/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace sumOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int Odd = i * 2 - 1;
+                Console.WriteLine("{0}",Odd);
+                sum += Odd;
+            }
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
