ca79fc075ab799c549aa85e0f1b90acc748528b9
LPenev <lppenev@gmail.com>
2023-09-16 11:46:00 +0300

    02. Divison Completed

---
+++ 02. Fundamentals/01.1. Basic Syntax, Conditional Statements and Loops - Exercise/02. Divison/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // given integer is divisible by 2 or 3, or 6, or 7, or 10
+            int number = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            if (number % 10 == 0)
+            {
+                result = 10;
+            }
+            else if (number % 7 == 0)
+            {
+                result = 7;
+            }
+            else if(number % 6 == 0)
+            {
+                result = 6;
+            }
+            else if(number % 3 == 0)
+            {
+                result = 3;
+            }
+            else if( number % 2 == 0)
+            {
+                result = 2;
+            }
+
+            if(result == 0 )
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine("The number is divisible by {0}", result);
+            }
+        }
+    }
+}
