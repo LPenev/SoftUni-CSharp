6548e55bc5ec5a5d2361cbd95b688b1a9958e8ba
LPenev <lppenev@gmail.com>
2023-09-18 11:57:04 +0300

    06. Strong Number Completed

---
+++ 02. Fundamentals/01.1. Basic Syntax, Conditional Statements and Loops - Exercise/06. Strong Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int workNumber = number;
+            int sum = 0;
+
+            while (workNumber > 0)
+            {
+                int factoriel = 1;
+                int digit = workNumber % 10;
+                workNumber /= 10;
+
+                for(int i = 1; i <= digit; i++)
+                {
+                    factoriel *= i;
+                }
+                sum += factoriel;
+            }
+
+            if (sum == number)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
