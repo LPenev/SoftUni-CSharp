4aa9a67710901df7c58fe9d000f3c47c628c2a7a
LPenev <lppenev@gmail.com>
2023-09-21 10:47:51 +0300

    05. Special Numbers Completed

---
+++ 02. Fundamentals/02. Data Types and Variables/05. Special Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿// A number is special when its sum of digits is 5, 7 or 11.
+
+int loops = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= loops; i++)
+{
+    int current = i;
+    int sum = default(int);
+
+    while (current >= 1)
+    {
+        sum += current%10;
+        current /= 10;
+    }
+
+    if(sum == 5 || sum == 7 || sum == 11)
+    {
+        Console.WriteLine($"{i} -> True");
+    }
+    else
+    {
+        Console.WriteLine($"{i} -> False");
+    }
+}
