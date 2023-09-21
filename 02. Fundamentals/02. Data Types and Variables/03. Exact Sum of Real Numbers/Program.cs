22e287c7dddb64b3b0a7c9dd0f21e45e24112669
LPenev <lppenev@gmail.com>
2023-09-21 10:46:50 +0300

    03. Exact Sum of Real Numbers Completed

---
+++ 02. Fundamentals/02. Data Types and Variables/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,8 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+decimal sum = default(decimal);
+for (int i = 0; i < count; i++)
+{
+    sum += decimal.Parse(Console.ReadLine()); 
+}
+Console.WriteLine(sum);
