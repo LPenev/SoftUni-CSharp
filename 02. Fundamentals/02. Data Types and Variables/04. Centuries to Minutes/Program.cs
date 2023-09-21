920dd9bb72df2636acf17aa0fc34ecc67eb2e34d
LPenev <lppenev@gmail.com>
2023-09-21 10:47:19 +0300

    04. Centuries to Minutes Completed

---
+++ 02. Fundamentals/02. Data Types and Variables/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int cencenturies = int.Parse(Console.ReadLine());
+int years = cencenturies * 100;
+double calc = years * 365.2422;
+int days = (int)calc;
+int hours = days * 24;
+int minutes = hours * 60;
+Console.WriteLine($"{cencenturies} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
