155e040d76d14310ddef464d89369bd453cdd571
LPenev <lppenev@gmail.com>
2023-09-22 11:31:23 +0300

    08. Town Info Completed

---
+++ 02. Fundamentals/02. Data Types and Variables/08. Town Info/Program.cs
@@ -0,0 +1,5 @@
+﻿string townName = Console.ReadLine();
+string population = Console.ReadLine();
+string area = Console.ReadLine();
+
+Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
