344dd6b5a794968e1564f484e373f00c5057c055
LPenev <lppenev@gmail.com>
2023-09-23 11:52:48 +0300

    03. Elevator Completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberOfPeople = int.Parse(Console.ReadLine());
+int capacityOfElevator = int.Parse(Console.ReadLine());
+
+// calculations
+int result = (int)Math.Ceiling( (double)numberOfPeople / capacityOfElevator);
+Console.WriteLine(result);
