bc31b3df77505397d200e1d793d415ded5fc3b63
LPenev <lppenev@gmail.com>
2023-09-23 11:53:15 +0300

    04. Sum of Chars Completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberOfCahrs = int.Parse(Console.ReadLine());
+
+int sumOfChars = 0;
+for (int i = 0; i < numberOfCahrs; i++)
+{
+    char input = char.Parse(Console.ReadLine());
+    sumOfChars += input;
+}
+Console.WriteLine($"The sum equals: {sumOfChars}");
