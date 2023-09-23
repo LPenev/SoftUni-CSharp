328eebc0978e3fe6a40c8e59c66d30b351de957e
LPenev <lppenev@gmail.com>
2023-09-23 11:51:54 +0300

    01. Integer Operations Completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/01. Integer Operations/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+int third = int.Parse(Console.ReadLine());
+int four = int.Parse(Console.ReadLine());
+
+// calculations
+int operation1 = first + second;
+int operation2 = operation1 / third;
+int result = operation2 * four;
+
+// print result
+Console.WriteLine(result);
