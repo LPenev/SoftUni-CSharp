2110fbd02c4c6a8a54c05f4430be9ad79af6c6aa
LPenev <lppenev@gmail.com>
2023-09-25 11:25:58 +0300

    08. Beer Kegs Completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,21 @@
+﻿byte loops = byte.Parse(Console.ReadLine());
+
+string sname = default;
+double svolume = 0;
+
+for (int i = 0; i < loops; i++)
+{
+    string name = Console.ReadLine();
+    float radius = float.Parse(Console.ReadLine());
+    int height = int.Parse(Console.ReadLine());
+    //
+    double volume = Math.PI * Math.Pow(2, radius) * height;
+    if( svolume < volume)
+    {
+        svolume = volume;
+        sname = name;
+    }
+}
+
+// print result
+Console.WriteLine(sname);
