d0b43dcfacc19c31699cb6bbaf140486ff99af31
LPenev <lppenev@gmail.com>
2023-09-25 11:26:25 +0300

    09. Spice Must Flow Completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,21 @@
+﻿uint yield = uint.Parse(Console.ReadLine());
+
+uint sumOfSpice = 0;
+uint countDays = 0;
+
+// calculations
+
+while (yield >= 100)
+{
+    sumOfSpice += yield - 26;
+    yield -= 10;
+    countDays++;
+}
+if (sumOfSpice > 0)
+{
+    sumOfSpice -= 26;
+}
+
+// print result
+Console.WriteLine(countDays);
+Console.WriteLine(sumOfSpice);
