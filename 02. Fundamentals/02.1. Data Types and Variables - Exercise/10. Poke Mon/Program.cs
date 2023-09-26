2bf8bc461ed78aa3ff4b94368ade1f3c6a896b3a
LPenev <lppenev@gmail.com>
2023-09-26 11:35:25 +0300

    10. Poke Mon Completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,25 @@
+﻿// N = powerOfPoking
+// M = distanceBetweenTargets
+// Y = depletionFactor
+//
+int powerOfPoking = int.Parse(Console.ReadLine());
+int distanceBetweenTargets  = int.Parse(Console.ReadLine());
+byte depletionFactor  = byte.Parse(Console.ReadLine());
+
+double halfPower = powerOfPoking * 0.5;
+
+int pokingCounter = 0;
+//
+while (distanceBetweenTargets <= powerOfPoking)
+{
+    powerOfPoking -= distanceBetweenTargets;
+    pokingCounter++;
+
+    if (powerOfPoking == halfPower && depletionFactor != 0)
+    {
+        powerOfPoking /= depletionFactor;
+    }
+}
+
+Console.WriteLine(powerOfPoking);
+Console.WriteLine(pokingCounter);
