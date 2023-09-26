975fd44ff6f5c74f1292908049ea66423815c5db
LPenev <lppenev@gmail.com>
2023-09-26 11:35:51 +0300

    11. Snowballs Completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/11. Snowballs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+ushort numberOfSnowballs = ushort.Parse(Console.ReadLine());
+BigInteger biggestSnowballValue = 0;
+string result = default;
+
+for (int i = 0; i < numberOfSnowballs; i++)
+{
+    ushort snowballSnow = ushort.Parse(Console.ReadLine());
+    ushort snowballTime = ushort.Parse(Console.ReadLine());
+    ushort snowballQuality = ushort.Parse(Console.ReadLine());
+
+    BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+
+    if(snowballValue > biggestSnowballValue)
+    {
+        result = $"{snowballSnow} : {snowballTime} = {snowballValue} ({snowballQuality})";
+        biggestSnowballValue = snowballValue;
+    }
+}
+
+// print result
+Console.WriteLine(result);
