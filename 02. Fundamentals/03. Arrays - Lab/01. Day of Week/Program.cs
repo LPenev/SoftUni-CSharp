f3fd7be7b50b03fd5ac384d5606b4492a9f5b1ae
LPenev <lppenev@gmail.com>
2023-09-27 11:14:11 +0300

    01. Day of Week Completed

---
+++ 02. Fundamentals/03. Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,11 @@
+﻿int input = int.Parse(Console.ReadLine());
+string[] day = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+if (input > 0 && input < 8)
+{
+    Console.WriteLine(day[input - 1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
