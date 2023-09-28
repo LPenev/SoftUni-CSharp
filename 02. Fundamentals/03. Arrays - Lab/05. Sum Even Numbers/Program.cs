55aa9e277e73ca5952db85f27d965470861a96a2
LPenev <lppenev@gmail.com>
2023-09-28 11:45:13 +0300

    05. Sum Even Numbers Completed

---
+++ 02. Fundamentals/03. Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int sum = default;
+
+foreach (var number in numbers)
+{
+    if (number%2 == 0) { sum += number; }
+}
+
+Console.WriteLine(sum);
+
