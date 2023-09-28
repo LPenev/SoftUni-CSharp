87a3e4bd584da73534819b5f7b68da4ab65393c5
LPenev <lppenev@gmail.com>
2023-09-28 11:44:46 +0300

    04. Reverse Array of Strings Completed

---
+++ 02. Fundamentals/03. Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string input = Console.ReadLine();
+
+string[] inputArray = input.Split(' ');
+
+//
+//
+// for(int i = inputArray.Length - 1 ; i > 0; i--)
+// {
+//     Console.Write(inputArray[i] + " ");
+// }
+// Console.Write(inputArray[0]);
+
+//
+//
+// for(int i = 0; i < inputArray.Length / 2; i++)
+// {
+//     string currentElement = inputArray[i];
+//    inputArray[i] = inputArray[inputArray.Length - 1 - i];
+//    inputArray[inputArray.Length - 1 - i] = currentElement;
+// }
+// for(int i = 0;i < inputArray.Length -1; i++)
+// {
+//     Console.Write(inputArray[i] + " ");
+// }
+// Console.WriteLine(inputArray[inputArray.Length -1]);
+
+//
+Console.WriteLine(string.Join(" ", inputArray.Reverse()));
