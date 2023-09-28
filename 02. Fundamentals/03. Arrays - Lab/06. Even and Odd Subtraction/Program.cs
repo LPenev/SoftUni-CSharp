7e29142f9911ad164fcb85d0b8d5d610118a3e75
LPenev <lppenev@gmail.com>
2023-09-28 11:45:51 +0300

    06. Even and Odd Subtraction Completed

---
+++ 02. Fundamentals/03. Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int sumEven = default;
+int sumOdd = default;
+
+foreach (int number in numbers)
+{
+    if(number%2 == 0)
+    {
+        sumEven += number;
+    }
+    else
+    {
+        sumOdd += number;
+    }
+}
+// print result
+Console.WriteLine(sumEven - sumOdd);
