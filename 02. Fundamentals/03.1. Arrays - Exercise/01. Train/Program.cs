6a7efd774252ad66c6289fc9b1b98ba147aa2884
LPenev <lppenev@gmail.com>
2023-09-29 11:29:03 +0300

    01. Train Completed

---
+++ 02. Fundamentals/03.1. Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberOfWagons = int.Parse(Console.ReadLine());
+
+int[] wagons = new int[numberOfWagons];
+int sumAllPassengers = 0;
+
+for (int i = 0; i < numberOfWagons; i++)
+{
+    wagons[i] = int.Parse(Console.ReadLine());
+    sumAllPassengers += wagons[i];
+}
+
+// print result
+Console.WriteLine(string.Join(" ",wagons));
+Console.WriteLine(sumAllPassengers);
