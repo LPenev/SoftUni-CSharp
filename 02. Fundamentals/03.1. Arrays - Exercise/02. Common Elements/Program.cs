9e0a1a9d6eddc9fc84d054df6e43439e81964a56
LPenev <lppenev@gmail.com>
2023-09-30 12:10:35 +0300

    02. Common Elements Completed

---
+++ 02. Fundamentals/03.1. Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] inputA = Console.ReadLine().Split();
+string[] inputB = Console.ReadLine().Split();
+
+for (int i = 0; i < inputB.Length; i++)
+{
+    for (int j = 0; j < inputA.Length; j++)
+    {
+        if (inputB[i] == inputA[j])
+        {
+            Console.Write(inputB[i]);
+            if(inputB.Length -1 != i)
+            {
+                Console.Write(" ");
+            }
+        }
+    }
+}
