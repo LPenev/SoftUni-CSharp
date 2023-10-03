90bdffb01ef38176262c0d531df92667df9de71d
LPenev <lppenev@gmail.com>
2023-10-03 11:23:16 +0300

    08. Magic Sum Completed

---
+++ 02. Fundamentals/03.1. Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int selectedNumber = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < input.Length; i++)
+{
+	for (int j = i + 1; j < input.Length; j++)
+	{
+		if (selectedNumber == input[i] + input[j])
+		{
+			Console.WriteLine($"{input[i]} {input[j]}");
+        }
+	}
+}
