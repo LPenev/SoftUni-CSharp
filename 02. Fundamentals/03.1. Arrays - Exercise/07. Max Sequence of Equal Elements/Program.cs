a6e6c5c0f2325454ad60342a0eda96b22940b56b
LPenev <lppenev@gmail.com>
2023-10-03 11:22:54 +0300

    07. Max Sequence of Equal Elements Completed

---
+++ 02. Fundamentals/03.1. Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int currentCounter = 0;
+int currentNumber = 0;
+int maxIndex = 0;
+int maxLength = 0;
+
+for (int i = 0; i < input.Length -1; i++)
+{
+    if (input[i] == input[i + 1])
+    {
+        currentNumber = input[i];
+        currentCounter++;
+        
+        if (currentCounter > maxLength)
+        {
+            maxIndex = i;
+            maxLength = currentCounter;
+        }
+    }
+    else
+    {
+        currentNumber = 0;
+        currentCounter = 0;
+    }
+}
+
+for(int i = 0;i <= maxLength; i++)
+{
+    Console.Write($"{input[maxIndex]} ");
+}
