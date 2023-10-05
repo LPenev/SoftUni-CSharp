73615ce23dd8e9104f4c9b77f7c8f94a9d80c723
LPenev <lppenev@gmail.com>
2023-10-05 11:27:59 +0300

    07. Repeat String Completed

---
+++ 02. Fundamentals/04. Metohds - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputedString = Console.ReadLine();
+            int timesRepeat = int.Parse(Console.ReadLine());
+            string textToPring = repeatString(inputedString, timesRepeat);
+            Console.WriteLine(textToPring);
+        }
+
+        static string repeatString(string text, int timeOfRepeat)
+        {
+            string result = "";
+            for(int i = 1;i <= timeOfRepeat;i++)
+            {
+                result += text;
+            }
+            return result;
+        }
+    }
+}
