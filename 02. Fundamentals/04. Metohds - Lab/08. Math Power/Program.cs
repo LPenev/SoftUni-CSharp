b8314e391a9443c875907282066eca2a5959054d
LPenev <lppenev@gmail.com>
2023-10-05 11:28:36 +0300

    08. Math Power Completed

---
+++ 02. Fundamentals/04. Metohds - Lab/08. Math Power/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseDigit = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double result = mathPower( baseDigit, power);
+            Console.WriteLine(result);
+        }
+
+        static double mathPower(double baseDigit, double power)
+        {
+            double result = Math.Pow(baseDigit, power);
+            return result;
+        }
+    }
+}
