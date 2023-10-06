537abd52ac185f137ae1dd559434fe847c369f31
LPenev <lppenev@gmail.com>
2023-10-06 11:11:03 +0300

    11. Math operations Completed

---
+++ 02. Fundamentals/04. Metohds - Lab/11. Math operations/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double operatorA = double.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            double operatorB = double.Parse(Console.ReadLine());
+            
+            double result = Calculate(operatorA, @operator, operatorB);
+            Console.WriteLine(result);
+        }
+
+        static double Calculate(double operA, string @operator, double operB)
+        {
+            double result = 0;
+            switch (@operator)
+            {
+                case "*":
+                    result = operA * operB;
+                    break;
+                case "/":
+                    result = operA / operB;
+                    break;
+                case "+":
+                    result = operA + operB;
+                    break;
+                case "-":
+                    result = operA - operB;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
