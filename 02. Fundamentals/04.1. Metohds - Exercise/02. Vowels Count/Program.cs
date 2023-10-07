fd6ee269cb72a6cbab7b8025d24ce83f1f0113d3
LPenev <lppenev@gmail.com>
2023-10-07 12:03:14 +0300

    02. Vowels Count Completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _1102._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int result = CountOfVowels(input.ToLower());
+            Console.WriteLine(result);
+        }
+
+        static int CountOfVowels(string inputedText)
+        {
+            int counter = 0;
+            for (int i = 0; i < inputedText.Length; i++)
+            {
+                if (inputedText[i] == 'a' 
+                    || inputedText[i] == 'e'
+                    || inputedText[i] == 'i'
+                    || inputedText[i] == 'o'
+                    || inputedText[i] == 'u')
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
