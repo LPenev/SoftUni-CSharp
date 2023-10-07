80dd959fdf3e2756e4466ac2c544760d6513190f
LPenev <lppenev@gmail.com>
2023-10-07 12:03:42 +0300

    03. Characters in Range Completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _1103._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char inputA = char.Parse(Console.ReadLine());
+            char inputB = char.Parse(Console.ReadLine());
+            StringInRange(inputA, inputB);
+        }
+
+        static void StringInRange(char inputA, char inputB)
+        {
+            char startChar, endChar;
+
+            if (inputA > inputB)
+            {
+                startChar = inputB;
+                endChar = inputA;
+            }
+            else
+            {
+                startChar = inputA;
+                endChar = inputB;
+            }
+
+            for (int i = (int)startChar + 1;i < (int)endChar; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
