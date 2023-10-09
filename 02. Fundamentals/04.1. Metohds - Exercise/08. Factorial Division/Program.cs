1123b4f2d022374cacc40d6ddfef2d8e411e29ff
LPenev <lppenev@gmail.com>
2023-10-09 11:16:16 +0300

    08. Factorial Division Completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1108._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputA = int.Parse(Console.ReadLine());
+            int inputB = int.Parse(Console.ReadLine());
+            double factorial = Factoriel(inputA);
+            double devision = Factoriel(inputB);
+            double result = factorial / devision;
+            Console.WriteLine($"{result:F2}");
+        }
+
+        static double Factoriel(int input)
+        {
+            long factorial = 1;
+            for (int i = input;i > 0;i--)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
