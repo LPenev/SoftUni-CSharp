c9dd6a8f43d1f1ef592645970a6ea6e10e595ee1
LPenev <lppenev@gmail.com>
2023-10-09 11:14:48 +0300

    05. Add and Subtract Completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+
+namespace _1105._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputA = int.Parse(Console.ReadLine());
+            int inputB = int.Parse(Console.ReadLine());
+            int inputC = int.Parse(Console.ReadLine());
+
+            int sumAB = SumOfTwo(inputA, inputB);
+            int result = SubtractOfTwo(sumAB, inputC);
+            Console.WriteLine(result);
+        }
+
+        static int SumOfTwo(int valueA, int valueB)
+        {
+            int result = valueA + valueB;
+            return result;
+        }
+
+        static int SubtractOfTwo(int valueA, int valueB)
+        {
+            int result = valueA - valueB;
+            return result;
+        }
+    }
+}
