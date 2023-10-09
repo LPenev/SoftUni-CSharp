b238a9d16a4b97b2469d111ea2dbf8fab9b7e746
LPenev <lppenev@gmail.com>
2023-10-09 11:15:40 +0300

    07. NxN Matrix Completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1107._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            NxN_Matrix(input);
+        }
+
+        static void NxN_Matrix(int inputedValue)
+        {
+            for (int i = 0; i < inputedValue; i++)
+            {
+                for(int j = 0; j < inputedValue; j++)
+                {
+                    Console.Write(inputedValue + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
