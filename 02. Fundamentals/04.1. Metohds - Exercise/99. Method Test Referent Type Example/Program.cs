723030af67de9684ee90854c291e878231a0c129
LPenev <lppenev@gmail.com>
2023-10-10 12:03:48 +0300

    99. Method Test Referent Type Example Completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/99. Method Test Referent Type Example/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _99._Method_Test_Referent_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4 };
+
+            UpdateArray(array);
+
+            Console.WriteLine("[ {0} ]", string.Join(", ", array));
+        }
+
+        static void UpdateArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] + 1;
+            }
+        }
+    }
+}
