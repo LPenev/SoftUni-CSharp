2180a24f0284a4b968477b00ca957183891800d0
LPenev <lppenev@gmail.com>
2023-10-10 12:03:22 +0300

    98. Method Test Referent Type 1 Example Completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/98. Method Test Referent Type 1 Example/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 2, 3, 4 };
+
+UpdateArray(ref array);
+
+Console.WriteLine("[ {0} ]",string.Join(", ",array));
+
+static void UpdateArray(ref int[] inputedArray )
+{
+    for(int i = 0; i < inputedArray.Length; i++)
+    {
+        inputedArray[i] = inputedArray[i] + 1;
+    }
+}
