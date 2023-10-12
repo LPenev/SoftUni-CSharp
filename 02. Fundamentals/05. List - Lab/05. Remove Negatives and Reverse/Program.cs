4ab14f6600972412c47ed381c7877bae259974ce
LPenev <lppenev@gmail.com>
2023-10-12 11:25:11 +0300

    05. Remove Negatives and Reverse Completed

---
+++ 02. Fundamentals/05. List - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            numbers.RemoveAll(x => x < 0);
+
+            numbers.Reverse();
+            
+            PrintList(numbers);
+        }
+
+        static void PrintList(List<int> numbers)
+        {
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
