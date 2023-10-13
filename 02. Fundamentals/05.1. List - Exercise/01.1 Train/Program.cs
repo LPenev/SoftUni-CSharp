0bc8d5d8f44ad290d6f81afb40233c8721a6df95
LPenev <lppenev@gmail.com>
2023-10-13 12:21:31 +0300

    01.1 Train Completed

---
+++ 02. Fundamentals/05.1. List - Exercise/01.1 Train/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _1011._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> trainWagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int maxCapacityOfWagon = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command.StartsWith("Add"))
+                {
+                    string[] passengers = command.Split();
+                    trainWagons.Add(int.Parse(passengers[1]));
+                }
+                else if (command.StartsWith("end"))
+                {
+                    Console.WriteLine(string.Join(" ", trainWagons));
+                    return;
+                }
+                else
+                {
+                    int passengers = int.Parse(command);
+
+                    for (int i = 0; i < trainWagons.Count; i++)
+                    {
+                        if (trainWagons[i] + passengers <= maxCapacityOfWagon)
+                        {
+                            trainWagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
