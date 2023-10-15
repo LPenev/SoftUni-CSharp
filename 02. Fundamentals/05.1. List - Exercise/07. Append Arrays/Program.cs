9f94a76dcace0ba85e1c460e6e58fa3611a98e5b
LPenev <lppenev@gmail.com>
2023-10-15 12:03:30 +0300

    07. Append Arrays Completed

---
+++ 02. Fundamentals/05.1. List - Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _107._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split("|")
+                .ToList();
+            
+            List<int> result = new List<int>();
+
+            input.Reverse();
+
+            foreach (string current in input)
+            {
+                int[] currentTail = current.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                foreach (int tail in currentTail)
+                {
+                    result.Add(tail);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
