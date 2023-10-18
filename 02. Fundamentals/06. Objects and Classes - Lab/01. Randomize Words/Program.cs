d3d80cb1106a8448e1910b83744edfbcc3353dcc
LPenev <lppenev@gmail.com>
2023-10-18 11:34:00 +0300

    01. Randomize Words Completed

---
+++ 02. Fundamentals/06. Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            RandomizeString(input);
+            Console.WriteLine(string.Join(Environment.NewLine, input));
+        }
+
+        static void RandomizeString(string[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            { 
+                int randomPosition = random.Next(0,array.Length-1);
+                string current = array[i];
+                array[i] = array[randomPosition];
+                array[randomPosition] = current;
+            }
+        }
+    }
+}
