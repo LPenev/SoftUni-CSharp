86814b3b0eced594afdb89c76f322e32223b1b46
LPenev <lppenev@gmail.com>
2023-10-18 11:35:09 +0300

    03. Songs Completed

---
+++ 02. Fundamentals/06. Objects and Classes - Lab/03. Songs/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _03._Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            List<Song> songs = new List<Song>();
+
+            for(int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split("_");
+                
+                Song song = new Song();
+
+                song.TypeList = input[0];
+                song.Name = input[1];
+                song.Time = input[2];
+
+                songs.Add(song);
+            }
+
+            string typeList = Console.ReadLine();
+            if (typeList == "all")
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, songs.Select(x => x.Name)));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, songs.Where(x => x.TypeList == typeList).Select(x => x.Name)));
+            }
+
+        }
+    }
+
+    public class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+    }
+}
