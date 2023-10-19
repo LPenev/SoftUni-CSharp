fc277bd82bdedf71ed1078db403a2c336d05e048
LPenev <lppenev@gmail.com>
2023-10-19 11:19:39 +0300

    05. Students 2.0 Completed

---
+++ 02. Fundamentals/06. Objects and Classes - Lab/05. Students 2.0/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _05._Students_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> listStudents = new List<Student>();
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] infoArray = input.Split();
+
+                Student student = new Student();
+
+                student.FirstName = infoArray[0];
+                student.LastName = infoArray[1];
+                student.Age = infoArray[2];
+                student.HomeTown = infoArray[3];
+
+                Student currentStudent = listStudents
+                    .FirstOrDefault(x => x.FirstName == infoArray[0] && x.LastName == infoArray[1]);
+
+                if (currentStudent != null)
+                {
+                    currentStudent.Age = infoArray[2];
+                    currentStudent.HomeTown = infoArray[3];
+                }
+                else
+                {
+                    listStudents.Add(student);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string inputedTown = Console.ReadLine();
+
+            Console.WriteLine(string.Join(Environment.NewLine,
+                listStudents
+                .Where(x => x.HomeTown == inputedTown)
+                .Select(x => $"{x.FirstName} {x.LastName} is {x.Age} years old.")));
+        }
+    }
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string HomeTown { get; set; }
+
+    }
+}
