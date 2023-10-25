43b5bf6c5d815258d33f13ee66139b7ce8a10858
LPenev <lppenev@gmail.com>
2023-10-25 11:27:32 +0300

    02. Odd Occurrences Completed

---
+++ 02. Fundamentals/07. Associative Arrays/02. Odd Occurrences/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> counts = new Dictionary<string,int>();
+            string[] words = Console.ReadLine().Split();
+
+            foreach (string word in words)
+            {
+                string wordToLower = word.ToLower();
+
+                if (counts.ContainsKey(wordToLower))
+                {
+                    counts[wordToLower]++;
+                }
+                else
+                {
+                    counts.Add(wordToLower, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
