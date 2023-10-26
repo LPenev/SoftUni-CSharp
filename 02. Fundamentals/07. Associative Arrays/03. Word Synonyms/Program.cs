eef981a534f42e8a870e62c4a42673a489f6f6ad
LPenev <lppenev@gmail.com>
2023-10-26 11:16:13 +0300

    03. Word Synonyms Completed

---
+++ 02. Fundamentals/07. Associative Arrays/03. Word Synonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new Dictionary<string, List<string>>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if(words.ContainsKey(word))
+                {
+                    words[word].Add(synonym);
+                }
+                else
+                {
+                    words.Add(word, new List<string>());
+                    words[word].Add(synonym);
+                }
+            }
+
+            foreach (string word in words.Keys)
+            {
+                Console.WriteLine($"{word} - " + string.Join(", ", words[word]));
+            }
+        }
+    }
+}
