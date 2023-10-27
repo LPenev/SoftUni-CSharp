e8e81c225b402547ce70154efb9037a275b109c0
LPenev <lppenev@gmail.com>
2023-10-27 11:48:24 +0300

    02. A Miner Task Completed

---
+++ 02. Fundamentals/07.1 Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace _102._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, BigInteger> resources = new Dictionary<string, BigInteger>();
+            string name = string.Empty;
+            int counter = 0;
+
+            while((name = Console.ReadLine()) != "stop")
+            {
+                BigInteger value = BigInteger.Parse(Console.ReadLine());
+                if (resources.ContainsKey(name))
+                {
+                    resources[name] += value;
+                } 
+                else
+                {
+                    resources.Add(name, value);
+                }
+            }
+
+            foreach(var resource in resources)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
