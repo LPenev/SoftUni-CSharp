b08594888e768d2a5eaeed8a3bc4d85efc56a323
LPenev <lppenev@gmail.com>
2023-10-30 11:04:38 +0200

    06. Student Academy Completed

---
+++ 02. Fundamentals/07.1 Associative Arrays - Exercise/06. Student Academy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace _106._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new Dictionary<string, List<double>>();
+
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string student = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(student))
+                {
+                    students.Add(student, new List<double>());
+                }
+                students[student].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                string name = student.Key;
+                double averageGrade = student.Value.Average();
+                if(averageGrade < 4.5)
+                {
+                    continue;
+                }
+                Console.WriteLine($"{name} -> {averageGrade:f2}");
+            }
+        }
+    }
+}
