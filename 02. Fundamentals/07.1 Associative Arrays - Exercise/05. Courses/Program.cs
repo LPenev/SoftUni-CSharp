f0778d1423cf95c4bdc0b1128b0790ab22275380
LPenev <lppenev@gmail.com>
2023-10-30 11:04:05 +0200

    05. Courses Completed

---
+++ 02. Fundamentals/07.1 Associative Arrays - Exercise/05. Courses/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _105._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string input = string.Empty;
+            while((input = Console.ReadLine()) != "end")
+            {
+                string[] inputArray = input.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+                string courseName = inputArray[0];
+                string studentName = inputArray[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>());
+                }
+
+                courses[courseName].Add(studentName);
+            }
+
+            foreach (var course in courses)
+            {
+                string courseName = course.Key;
+                int registeredStudents = course.Value.Count;
+                Console.WriteLine($"{courseName}: {registeredStudents}");
+                foreach (var student in course.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
