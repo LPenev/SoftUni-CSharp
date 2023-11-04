46870b28d1f88743263a2114e0aaaff0731d78cf
LPenev <lppenev@gmail.com>
2023-11-04 11:15:09 +0200

    04. Snowwhite Completed

---
+++ 02. Fundamentals/07.2 Associative Arrays - More Exercise/04. Snowwhite/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _204._Snowwhite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
