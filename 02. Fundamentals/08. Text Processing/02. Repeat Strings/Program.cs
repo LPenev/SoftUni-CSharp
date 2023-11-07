575fe5c4c3a065012799fa99869d15e70db8aa4f
LPenev <lppenev@gmail.com>
2023-11-07 11:09:31 +0200

    02. Repeat Strings Completed

---
+++ 02. Fundamentals/08. Text Processing/02. Repeat Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    Console.Write(input[i]);
+                }
+            }
+        }
+    }
+}
