103ffcd0d5bb2fc879f36c01780536e8239ae62e
LPenev <lppenev@gmail.com>
2023-11-07 11:09:01 +0200

    01. Reverse Strings Completed

---
+++ 02. Fundamentals/08. Text Processing/01. Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new Dictionary<string,string>();
+            string input = string.Empty;
+
+            while((input = Console.ReadLine()) != "end")
+            {
+                var charArray = input.ToCharArray();
+                Array.Reverse(charArray);
+                list.Add(input, String.Concat(charArray));
+            }
+
+            foreach (var current in list)
+            {
+                Console.WriteLine($"{current.Key} = {current.Value}");
+            }
+        }
+    }
+}
