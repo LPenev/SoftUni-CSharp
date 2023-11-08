942e4b4d6e8cd418ad06fd2c6d37b032dddc1836
LPenev <lppenev@gmail.com>
2023-11-08 13:23:38 +0200

    02.1. Repeat Strings Completed

---
+++ 02. Fundamentals/08. Text Processing/02.1. Repeat Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace _02._1._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var word in input)
+            {
+                for(int i = 0; i < word.Length; i++)
+                {
+                    sb.Append(word);
+                }
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd('\n'));
+        }
+    }
+}
