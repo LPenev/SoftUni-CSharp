086dea941ed1ca278e36628fbe0003d2418367fa
LPenev <lppenev@gmail.com>
2023-11-08 13:24:05 +0200

    03. Substring Completed

---
+++ 02. Fundamentals/08. Text Processing/03. Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string toRemove = Console.ReadLine();
+            string inputedString = Console.ReadLine();
+
+            while (inputedString.Contains(toRemove))
+            {
+                int startIndex = inputedString.IndexOf(toRemove);
+                int endIndex = toRemove.Length;
+
+                inputedString = inputedString.Remove(startIndex, endIndex);
+            }
+
+            Console.WriteLine(inputedString);
+        }
+    }
+}
