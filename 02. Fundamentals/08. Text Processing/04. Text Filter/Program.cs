1645f7ccdc57defd776e41353bfacca7c7f1588b
LPenev <lppenev@gmail.com>
2023-11-08 13:24:38 +0200

    04. Text Filter Completed

---
+++ 02. Fundamentals/08. Text Processing/04. Text Filter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var banWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            foreach (string banWord in banWords)
+            {
+                string replaceWord = "*";
+                for (int i = 1; i < banWord.Length; i++)
+                {
+                    replaceWord = String.Concat(replaceWord, "*");
+                }
+
+                while (text.Contains(banWord))
+                {
+                    text = text.Replace(banWord, replaceWord);
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
