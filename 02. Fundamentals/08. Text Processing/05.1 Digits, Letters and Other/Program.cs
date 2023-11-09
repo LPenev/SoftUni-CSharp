cfc518d75d494288c02aace1efaa41eccb7a9dc0
LPenev <lppenev@gmail.com>
2023-11-09 11:34:45 +0200

    05.1 Digits, Letters and Other Completed

---
+++ 02. Fundamentals/08. Text Processing/05.1 Digits, Letters and Other/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _05._1_Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string listOfLeters = string.Empty;
+            string listOfDigits = string.Empty;
+            string listOfOthers = string.Empty;
+
+            var input = Console.ReadLine().ToCharArray();
+
+            foreach (char currentChar in input)
+            {
+                if (char.IsDigit(currentChar))
+                {
+                    listOfDigits = string.Concat(listOfDigits, currentChar);
+                }
+                else if (char.IsLetter(currentChar))
+                {
+                    listOfLeters = string.Concat(listOfLeters, currentChar);
+                }
+                else
+                {
+                    listOfOthers = string.Concat(listOfOthers, currentChar);
+                }
+            }
+
+            Console.WriteLine(string.Join("", listOfDigits));
+            Console.WriteLine(string.Join("", listOfLeters));
+            Console.WriteLine(string.Join("", listOfOthers));
+        }
+    }
+}
