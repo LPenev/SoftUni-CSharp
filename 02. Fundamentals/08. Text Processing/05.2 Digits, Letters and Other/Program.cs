2badd0bb23ef153afacbfc048dc7d7ef30033222
LPenev <lppenev@gmail.com>
2023-11-09 11:35:20 +0200

    05.2 Digits, Letters and Other Completed

---
+++ 02. Fundamentals/08. Text Processing/05.2 Digits, Letters and Other/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace _05._2_Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var digits = new StringBuilder();
+            var leters = new StringBuilder();
+            var others = new StringBuilder();
+
+            var input = Console.ReadLine().ToCharArray();
+
+            foreach (char currentChar in input)
+            {
+                if (char.IsDigit(currentChar))
+                {
+                    digits.Append(currentChar);
+                }
+                else if (char.IsLetter(currentChar))
+                {
+                    leters.Append(currentChar);
+                }
+                else
+                {
+                    others.Append(currentChar);
+                }
+            }
+
+            var result = new StringBuilder();
+            result.AppendLine(digits.ToString());
+            result.AppendLine(leters.ToString());
+            result.AppendLine(others.ToString());
+
+            Console.WriteLine(result);
+        }
+    }
+}
