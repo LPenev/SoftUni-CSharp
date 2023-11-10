eeb292773403cdf05f51b17de1c1619827287119
LPenev <lppenev@gmail.com>
2023-11-10 11:22:54 +0200

    01. Valid Usernames Completed

---
+++ 02. Fundamentals/08.1. Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _101._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var validUsernames = new List<string>();
+            var usernames = Console.ReadLine().Split(", ");
+
+            foreach (var username in usernames)
+            {
+                if (isUsernameValid(username))
+                {
+                    validUsernames.Add(username);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, validUsernames)); 
+        }
+
+        public static bool isUsernameValid(string username)
+        {
+            if(username.Length < 3 ||  username.Length > 16)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < username.Length; i++)
+            {
+                if (!(char.IsLetterOrDigit(username[i]) || username[i].Equals('-') || username[i].Equals('_')))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
