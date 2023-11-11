19985e6099a86aaa30518c0606a45c158bd432b7
LPenev <lppenev@gmail.com>
2023-11-11 12:09:54 +0200

    04. Caesar Cipher Completed

---
+++ 02. Fundamentals/08.1. Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _104._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .ToCharArray()
+                .Select(x => Convert.ToChar(x + 3));
+
+            Console.WriteLine(string.Join("", input));
+        }
+    }
+}
