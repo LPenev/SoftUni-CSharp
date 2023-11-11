a49590dc67bb3d78385f30b0921b47c9ffba434b
LPenev <lppenev@gmail.com>
2023-11-11 12:08:57 +0200

    03. Extract File Completed

---
+++ 02. Fundamentals/08.1. Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _103._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            int startIndex = input.LastIndexOf('\\') + 1;
+            int endIndex = input.LastIndexOf('.');
+            var filename = input.Substring(startIndex, endIndex - startIndex);
+
+            startIndex = endIndex + 1;
+            endIndex = input.Length - startIndex;
+
+            var extension = input.Substring(startIndex, endIndex);
+            
+            Console.WriteLine($"File name: {filename}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
