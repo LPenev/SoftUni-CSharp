2e30e80868b4f4570183f3fa89e2a724a267c43d
LPenev <lppenev@gmail.com>
2023-11-13 11:19:01 +0200

    06. Replace Repeating Chars Completed

---
+++ 02. Fundamentals/08.1. Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _106._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = new List<char>();
+            chars = Console.ReadLine().ToCharArray().ToList();
+
+            int i = 1;
+            while (i < chars.Count)
+            {
+                while (chars[i] == chars[i - 1])
+                {
+                    chars.RemoveAt(i);
+                    if(i == chars.Count)
+                    {
+                        break;
+                    }
+                }
+                i++;
+            }
+
+            Console.WriteLine(string.Join("",chars));
+        }
+    }
+}
