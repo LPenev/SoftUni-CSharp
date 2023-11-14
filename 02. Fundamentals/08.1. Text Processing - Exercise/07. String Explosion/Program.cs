d167c795b856bc5535dd98c741e6fd1b5e3a9040
LPenev <lppenev@gmail.com>
2023-11-14 11:02:14 +0200

    07. String Explosion Completed

---
+++ 02. Fundamentals/08.1. Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _107._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var strength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+                }
+                else if (strength > 0)
+                {
+                    input = input.Remove(i, 1);
+                    strength--;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
