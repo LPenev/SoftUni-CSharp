810ce10626bd97867a6316f537f0337c438bd7f1
LPenev <lppenev@gmail.com>
2023-11-15 11:20:02 +0200

    01. Match Full Name Completed

---
+++ 02. Fundamentals/09. Regular Expressions/01. Match Full Name/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"(?<name>\b[A-Z][a-z]+) (?<fname>[A-Z][a-z]+)";
+
+            string input = Console.ReadLine();
+            MatchCollection mached = Regex.Matches(input, patern);
+
+            Console.WriteLine(string.Join(" ", mached));
+
+        }
+    }
+}
