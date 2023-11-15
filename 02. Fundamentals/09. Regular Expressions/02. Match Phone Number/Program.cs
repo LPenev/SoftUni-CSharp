f45f7e170391fdd1f8b9ff24c595dc2cfadca955
LPenev <lppenev@gmail.com>
2023-11-15 11:20:35 +0200

    02. Match Phone Number Completed

---
+++ 02. Fundamentals/09. Regular Expressions/02. Match Phone Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"\+\b359(-| )2\1\d{3}\1\d{4}\b";
+            var input = Console.ReadLine();
+
+            var regex = new Regex(pattern);
+            MatchCollection phoneNumbers = regex.Matches(input);
+            var result = phoneNumbers
+                .Select(x => x.Value)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ",result));
+
+            //var phoneMatches = Regex.Matches(input, pattern);
+
+            //var machedPhones = phoneMatches
+            //    .Cast<Match>()
+            //    .Select(a => a.Value.Trim())
+            //    .ToArray();
+
+            //Console.WriteLine(string.Join(", ",machedPhones));
+        }
+    }
+}
