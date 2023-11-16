e0f126c4085aad80de95ad02b04075042a14cda5
LPenev <lppenev@gmail.com>
2023-11-16 11:13:02 +0200

    03. Match Dates Completed

---
+++ 02. Fundamentals/09. Regular Expressions/03. Match Dates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"\b(?<day>\d{2})([-./])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            var input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection result = regex.Matches(input);
+
+            //var result = Regex.Matches(input, pattern);
+
+            foreach (Match date in result)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }   
+        }
+    }
+}
