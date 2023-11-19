f4fb1c5ffe8734e787e924ae083afd41761f4502
LPenev <lppenev@gmail.com>
2023-11-19 11:27:54 +0200

    03. SoftUni Bar Income Completed

---
+++ 02. Fundamentals/09.1. Regular Expressions - Exercise/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _13._SoftUni_Bar_Income
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"^%(?<name>[A-Z][a-z]+)%[^$%.|]*<(?<product>\w+)>[^$%.|]*\|(?<qnty>\d+)\|[^$%.|]*?(?<price>\d+(\.[0-9]+)?)\$";
+            decimal totalSum = 0;
+
+            string input = string.Empty;
+            while((input = Console.ReadLine()) != "end of shift")
+            {
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    string customerName = match.Groups["name"].Value;
+                    string product = match.Groups["product"].Value;
+                    int qnty = int.Parse(match.Groups["qnty"].Value);
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+                    decimal totalPrice = price * qnty;
+
+                    Console.WriteLine($"{customerName}: {product} - {totalPrice:f2}");
+                    totalSum += totalPrice;
+                }
+            }
+
+            // print result
+            Console.WriteLine($"Total income: {totalSum:f2}");
+        }
+    }
+}
