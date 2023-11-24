6e52c826d2c9388bb30203773af256041457d15b
LPenev <lppenev@gmail.com>
2023-11-24 11:43:43 +0200

    02. Fancy Barcodes Completed

---
+++ 02. Fundamentals/10.2. Exam Preparation 2/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
