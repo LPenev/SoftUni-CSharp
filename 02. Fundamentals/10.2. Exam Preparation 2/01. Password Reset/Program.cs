d72e25106b8afd61c7c9b5930cc0610f9b190c22
LPenev <lppenev@gmail.com>
2023-11-24 11:43:07 +0200

    01. Password Reset Completed

---
+++ 02. Fundamentals/10.2. Exam Preparation 2/01. Password Reset/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11._Password_Reset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
