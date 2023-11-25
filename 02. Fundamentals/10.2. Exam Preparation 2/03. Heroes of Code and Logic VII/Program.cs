94533676bf12fcc4f9e144287ec0ffa9edd73681
LPenev <lppenev@gmail.com>
2023-11-25 11:25:10 +0200

    03. Heroes of Code and Logic VII Completed

---
+++ 02. Fundamentals/10.2. Exam Preparation 2/03. Heroes of Code and Logic VII/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13._Heroes_of_Code_and_Logic_VII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
