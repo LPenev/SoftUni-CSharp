8b6e022124fb833e782207b0b00d71363e6f2b43
LPenev <lppenev@gmail.com>
2023-11-27 11:48:52 +0200

    2.03.1 Numbers Completed

---
+++ 02. Fundamentals/11.1. MidExam/2.03.1 Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _2._03._1_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            double averageSum = numbers.Average();
+
+            int[] result = numbers
+                .OrderByDescending(x => x)
+                .Where(x => x > averageSum)
+                .Take(5)
+                .ToArray();
+
+            if (result.Length > 0)
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
