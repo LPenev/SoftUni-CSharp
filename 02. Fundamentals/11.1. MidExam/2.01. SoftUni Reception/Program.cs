73b8205e7d9eef17776a7bc9470b380c1e8e4c3d
LPenev <lppenev@gmail.com>
2023-11-27 11:46:58 +0200

    2.01. SoftUni Reception Completed

---
+++ 02. Fundamentals/11.1. MidExam/2.01. SoftUni Reception/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace _2._01._SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int employer1StudentsPerHour = int.Parse(Console.ReadLine());
+            int employer2StudentsPerHour = int.Parse(Console.ReadLine());
+            int employer3StudentsPerHour = int.Parse(Console.ReadLine());
+
+            int studentsCount = int.Parse(Console.ReadLine());
+            int workHours = 3;
+            int pauseHours = 1;
+
+            int StudentsPerHourProcessed = employer1StudentsPerHour + employer2StudentsPerHour + employer3StudentsPerHour;
+
+            double needHoursToProcessed = (double)studentsCount / StudentsPerHourProcessed;
+            double hours = Math.Ceiling(needHoursToProcessed);
+           
+            if (hours > workHours)
+            {
+                if (hours % workHours == 0)
+                {
+                    hours += Math.Floor((hours / workHours - 1) * pauseHours);
+                }
+                else
+                {
+                    hours += Math.Floor((hours / workHours) * pauseHours);
+                }
+            }
+            Console.WriteLine("Time needed: {0}h.",hours);
+        }
+    }
+}
