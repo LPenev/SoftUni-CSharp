4213809a95f867cfde29746ab6ef539ff3b88944
LPenev <lppenev@gmail.com>
2023-11-28 11:23:29 +0200

    3.01. Counter-Strike Completed

---
+++ 02. Fundamentals/11.1. MidExam/3.01. Counter-Strike/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _3._01._Counter_Strike
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End of battle")
+                {
+                    Console.WriteLine($"Won battles: {count}. Energy left: {energy}");
+                    break;
+                }
+
+                int distance = int.Parse(input);
+                if (energy == 0 || energy < distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {count} won battles and {energy} energy");
+                    break;
+                }
+                energy -= distance;
+                count++;
+
+                if(count % 3 == 0)
+                {
+                    energy += count;
+                }
+            }
+        }
+    }
+}
