2e5faee3afffa472b8b924550d0694d877c63ff8
LPenev <lppenev@gmail.com>
2023-11-29 10:41:13 +0200

    4.03. Heart Delivery Completed

---
+++ 02. Fundamentals/11.1. MidExam/4.03. Heart Delivery/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _4._03._Heart_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
