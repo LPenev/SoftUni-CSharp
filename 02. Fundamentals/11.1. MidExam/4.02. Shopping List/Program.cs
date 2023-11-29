beb1445ecd0b4c85187dc65b364b89a8b6bd5a86
LPenev <lppenev@gmail.com>
2023-11-29 10:40:42 +0200

    4.02. Shopping List Completed

---
+++ 02. Fundamentals/11.1. MidExam/4.02. Shopping List/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _4._02._Shopping_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = Console.ReadLine()
+                .Split("!")
+                .ToList();
+            
+            string input = Console.ReadLine();
+            int index = 0;
+
+            while (input != "Go Shopping!")
+            {
+                string[] commands = input.Split().ToArray();
+                switch (commands[0])
+                {
+                    case "Urgent":
+                        index = shoppingList.IndexOf(commands[1]);
+                        if (index == -1)
+                        {
+                            shoppingList.Insert(0, commands[1]);
+                        }
+                        break;
+
+                    case "Unnecessary":
+                        index = shoppingList.IndexOf(commands[1]);
+                        if (index != -1)
+                        {
+                            shoppingList.RemoveAt(index);
+                        }
+                        break;
+
+                    case "Correct":
+                        index = shoppingList.IndexOf(commands[1]);
+                        if(index != -1)
+                        {
+                            shoppingList[index] = commands[2];
+                        }
+                        break;
+
+                    case "Rearrange":
+                        index = shoppingList.IndexOf(commands[1]);
+                        if (index != -1)
+                        {
+                            shoppingList.Add(shoppingList[index]);
+                            shoppingList.RemoveAt(index);
+                        }
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ", shoppingList));
+        }
+    }
+}
