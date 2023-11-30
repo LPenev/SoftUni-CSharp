b344802176e379f44c43764276da80e1e793fa6c
LPenev <lppenev@gmail.com>
2023-11-30 11:29:42 +0200

    5.02. MuOnline Completed

---
+++ 02. Fundamentals/11.1. MidExam/5.02. MuOnline/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5._02._MuOnline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
