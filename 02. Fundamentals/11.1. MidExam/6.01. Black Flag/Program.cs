6e40abcb93e038f002d09f29e0874ae44be53bfc
LPenev <lppenev@gmail.com>
2023-12-01 11:16:41 +0200

    6.01. Black Flag Completed

---
+++ 02. Fundamentals/11.1. MidExam/6.01. Black Flag/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _6._01._Black_Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
