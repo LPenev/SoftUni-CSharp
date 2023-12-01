ee6bd861f3df62508988e510439ad8dffb5e9caa
LPenev <lppenev@gmail.com>
2023-12-01 11:17:11 +0200

    6.02. Treasure Hunt Completed

---
+++ 02. Fundamentals/11.1. MidExam/6.02. Treasure Hunt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _6._02._Treasure_Hunt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
