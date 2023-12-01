fb06f1e9c539d01f8e7de16a4f9afa5c9555d2c8
LPenev <lppenev@gmail.com>
2023-12-01 11:17:32 +0200

    6.03. Man O War Completed

---
+++ 02. Fundamentals/11.1. MidExam/6.03. Man O War/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _6._03._Man_O_War
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
