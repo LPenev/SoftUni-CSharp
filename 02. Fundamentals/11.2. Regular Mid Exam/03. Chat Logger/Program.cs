a0db568b130db1c6782d3f792f4cb44ac5392faf
LPenev <lppenev@gmail.com>
2023-12-02 11:46:10 +0200

    03. Chat Logger Completed

---
+++ 02. Fundamentals/11.2. Regular Mid Exam/03. Chat Logger/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+
+namespace _03._Chat_Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> messages = new List<string>();
+
+            string input = Console.ReadLine();
+            
+            while (input != "end")
+            {
+                string[] command = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (command[0])
+                {
+                    case "Chat":
+                        messages.Add(command[1]);
+                        break;
+
+                    case "Delete":
+                        int index = messages.IndexOf(command[1]);
+                        if(index > -1)
+                        {
+                            messages.RemoveAt(index);
+                        }
+                        break;
+
+                    case "Edit":
+                        int index1 = messages.IndexOf(command[1]);
+                        messages[index1] = command[2];
+                        break;
+
+                    case "Pin":
+                        int index2 = messages.IndexOf(command[1]);
+                        if(index2 > -1)
+                        {
+                            messages.Add(messages[index2]);
+                            messages.RemoveAt(index2);
+                        }
+                        break;
+
+                    case "Spam":
+                        if (command.Length > 1) {
+                            for (int i = 1;i < command.Length; i++)
+                            {
+                                messages.Add((string)command[i]);
+                            }
+                        }
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, messages));
+        }
+    }
+}
