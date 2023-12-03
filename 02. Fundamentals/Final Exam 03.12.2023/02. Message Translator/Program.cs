c26fea2ce2467a6a97493b7a969bbfa9baf444db
LPenev <lppenev@gmail.com>
2023-12-03 14:49:45 +0200

    02. Message Translator Completed

---
+++ 02. Fundamentals/Final Exam 03.12.2023/02. Message Translator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Message_Translator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"!(?<Command>[A-Z][a-z]{1,})!:\[(?<String>[A-Za-z]{8,})\]";
+            Regex regex = new Regex(pattern);
+
+            var countOfInput = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < countOfInput; i++)
+            {
+                var input = Console.ReadLine();
+                Match match = regex.Match(input);
+                if(match.Success)
+                {
+                    var currentString = match.Groups["String"].Value;
+                    Console.Write($"{match.Groups["Command"]}:");
+                    foreach(char current in currentString)
+                    {
+                        int number = (int)current;
+                        Console.Write($" {number}");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+            }
+        }
+    }
+}
