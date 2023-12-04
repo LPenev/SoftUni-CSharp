cafbc128e9ef33d04d52754853c017ee3940d749
LPenev <lppenev@gmail.com>
2023-12-04 11:28:16 +0200

    01. Secret Chat Completed

---
+++ 02. Fundamentals/12.1. Final-Exam_September_2023/01. Secret Chat/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01._Secret_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string inputedCommand = string.Empty;
+            while ((inputedCommand = Console.ReadLine()) != "Reveal")
+            {
+                var commandArray = inputedCommand.Split(":|:");
+
+                switch (commandArray[0])
+                {
+                    case "ChangeAll":
+                        int index = 0;
+                        while ((index = message.IndexOf(commandArray[1])) > 0)
+                        {
+                            message = message.Replace(commandArray[1], commandArray[2]);
+                        }
+                        Console.WriteLine(message);
+                        break;
+                    case "Reverse":
+                        if (message.IndexOf(commandArray[1]) >= 0)
+                        {
+                            string reversedSubstring = new string(commandArray[1].Reverse().ToArray());
+                            message = message.Replace(commandArray[1], reversedSubstring);
+                            Console.WriteLine(message);
+                        }
+                        else
+                        {
+                            Console.WriteLine("error");
+                        }
+                        break;
+                    case "InsertSpace":
+                        int startIndex = int.Parse(commandArray[1]);
+                        message = message.Insert(startIndex, " ");
+                        Console.WriteLine(message);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
