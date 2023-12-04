48c3dae365b2e7ffd09e96f1cf18cd29f1102c59
LPenev <lppenev@gmail.com>
2023-12-04 11:28:45 +0200

    2. Fancy Barcodes Completed

---
+++ 02. Fundamentals/12.1. Final-Exam_September_2023/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex barcode = new Regex(@"@#+(?<Barcode>[A-Z]{1}[A-Za-z0-9]{4,}[A-Z])@#+");
+            Regex digits = new Regex(@"\d+");
+
+            int lineCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lineCount; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = barcode.Match(input);
+                if (!match.Success)
+                {
+                    Console.WriteLine("Invalid barcode");
+                    continue;
+                }
+
+                MatchCollection number = digits.Matches(match.Groups["Barcode"].Value);
+                if (number.Count() > 0)
+                {
+                    string result = String.Join("", number.Select(x=>x.Value));
+                    Console.WriteLine($"Product group: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Product group: 00");
+                }
+            }
+        }
+    }
+}
