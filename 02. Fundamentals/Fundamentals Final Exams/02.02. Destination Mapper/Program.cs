95d186279926392e6f179ea565928559231b7ad4
LPenev <lppenev@gmail.com>
2023-12-07 12:21:15 +0200

    02.02. Destination Mapper Completed

---
+++ 02. Fundamentals/Fundamentals Final Exams/02.02. Destination Mapper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._02._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var destinations = new List<string>();
+
+            var pattern = @"(=|\/)(?<Destination>[A-Z][A-Za-z]{2,})\1";
+            Regex regex = new Regex(pattern);
+
+            var input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+
+            var points = 0;
+            foreach (Match match in matches)
+            {
+                points += match.Groups["Destination"].Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", matches.Select(x => x.Groups["Destination"]))}");
+            Console.WriteLine($"Travel Points: {points}");
+        }
+    }
+}
