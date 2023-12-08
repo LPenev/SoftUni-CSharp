e2a6db10b60f8d0ad0c0892098d8efb766999b1e
LPenev <lppenev@gmail.com>
2023-12-08 12:07:33 +0200

    03.02. Mirror Words Completed

---
+++ 02. Fundamentals/Fundamentals Final Exams/03.02. Mirror Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03._02._Mirror_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"";
+            Regex regex = new Regex(pattern);
+
+            var input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+
+            var word = string.Empty;
+            foreach (Match match in matches)
+            {
+                word = match.Value;
+            }
+        }
+    }
+}
