3e9630d02561cd865f1a9388b06dbab83337d550
LPenev <lppenev@gmail.com>
2023-12-12 11:36:34 +0200

    02. From Left to the Right Completed

---
+++ 02. Fundamentals/02.2. Data Types and Variables More Exercise/02. From Left to the Right/Program.cs
@@ -0,0 +1,31 @@
+﻿int inputNumberLines = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < inputNumberLines; i++)
+{
+    string input = Console.ReadLine();
+    string[] output = input.Split(' ');
+
+    long left = long.Parse(output[0]);
+    long right = long.Parse(output[1]);
+    long number;
+    long sum = 0;
+
+    if( left > right )
+    {
+        number = left;
+    }
+    else
+    {
+        number= right;
+    }
+
+    if( number < 0) { number *= -1; }
+   
+    while( number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    Console.WriteLine(sum);
+}
