eba4352b0de88a2a2193912d0511673f852ff115
LPenev <lppenev@gmail.com>
2023-12-13 11:29:18 +0200

    03. Floating Equality Completed

---
+++ 02. Fundamentals/02.2. Data Types and Variables More Exercise/03. Floating Equality/Program.cs
@@ -0,0 +1,13 @@
+﻿
+double inputA = double.Parse(Console.ReadLine());
+double inputB = double.Parse(Console.ReadLine());
+double eps = 0.000001;
+
+if( Math.Abs(inputA - inputB) < eps)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
