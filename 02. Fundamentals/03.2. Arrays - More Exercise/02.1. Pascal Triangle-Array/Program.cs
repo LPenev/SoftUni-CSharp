b0e896365638d5519cd605e94bc0d4d3967a12ee
LPenev <lppenev@gmail.com>
2023-12-15 11:11:10 +0200

    02.1. Pascal Triangle-Array Completed

---
+++ 02. Fundamentals/03.2. Arrays - More Exercise/02.1. Pascal Triangle-Array/Program.cs
@@ -0,0 +1,20 @@
+﻿int triangleLines = int.Parse(Console.ReadLine());
+
+long[][] triangle = new long[triangleLines][];
+
+for (int row = 0; row < triangleLines; row++)
+{
+    triangle[row] = new long[row + 1];
+    triangle[row][0] = 1;
+    triangle[row][^1] = 1;
+
+    for (int column = 1; column < row; column++)
+    {
+        triangle[row][column] = triangle[row - 1][column - 1] + triangle[row - 1][column];
+    }
+}
+
+for (var row = 0; row < triangleLines; row++)
+{
+    Console.WriteLine(string.Join(" ", triangle[row]));
+}
