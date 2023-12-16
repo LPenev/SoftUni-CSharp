ffa57545137593387ef3b32d131975e2c516b3b0
LPenev <lppenev@gmail.com>
2023-12-16 11:24:48 +0200

    03.1 Fibonacci Completed

---
+++ 02. Fundamentals/03.2. Arrays - More Exercise/03.1 Fibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿int input = int.Parse(Console.ReadLine());
+
+int[] fibonacci = new int[50];
+
+fibonacci[0] = 1;
+fibonacci[1] = 1;
+int i = 2;
+
+for(; i < input; i++)
+{
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+}
+Console.WriteLine(fibonacci[i - 1]);
