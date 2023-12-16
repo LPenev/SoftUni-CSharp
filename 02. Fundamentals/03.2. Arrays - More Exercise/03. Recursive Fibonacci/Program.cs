6917a97e781042ea14f6b0d21fa020649cb31fdf
LPenev <lppenev@gmail.com>
2023-12-16 11:24:11 +0200

    03. Recursive Fibonacci Completed

---
+++ 02. Fundamentals/03.2. Arrays - More Exercise/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿int input = int.Parse(Console.ReadLine());
+
+int a = 1;
+int b = 1;
+int c = 1;
+
+int counter = 2;
+while (counter < input)
+{
+    // 1 1 2 3 5 8 13 21
+    c = a + b;
+    a = b;
+    b = c;
+    counter++;
+}
+Console.WriteLine(c);
