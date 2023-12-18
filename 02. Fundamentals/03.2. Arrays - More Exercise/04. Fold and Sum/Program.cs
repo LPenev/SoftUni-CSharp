f0715dd30820802f5a578cb4ddd7f51b07f6ce43
LPenev <lppenev@gmail.com>
2023-12-18 11:11:51 +0200

    04. Fold and Sum Completed

---
+++ 02. Fundamentals/03.2. Arrays - More Exercise/04. Fold and Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] input = Console.ReadLine()
+    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int foldLenght = input.Length / 4;
+int counter = foldLenght;
+
+for (int i = foldLenght -1 ; i >= 0; i--)
+{
+    Console.Write(input[i] + input[counter] + " ");
+    counter++;
+}
+
+for (int j = input.Length - 1 ; j > input.Length - foldLenght -1; j--)
+{
+    Console.Write(input[j] + input[counter] + " ");
+    counter++;
+}
