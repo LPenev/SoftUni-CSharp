e2198b316e599fcf2dfcc5e22f7d5e4e3ea048a3
LPenev <lppenev@gmail.com>
2023-12-19 10:32:47 +0200

    04.1. Fold and Sum Completed

---
+++ 02. Fundamentals/03.2. Arrays - More Exercise/04.1. Fold and Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int foldPoint = input.Length / 4;
+int[] result  = new int[foldPoint * 2];
+
+for (int i = 0; i < foldPoint; i++)
+{
+    result[i] = input[i + foldPoint] + input[foldPoint - 1 - i];
+    result[i + foldPoint] = input[i + 2 * foldPoint] + input[input.Length -1 -i];
+}
+
+Console.WriteLine(String.Join(' ', result));
