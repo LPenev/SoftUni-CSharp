05a5a6205691c8d3d1119fc779ab50cec0aa6cc5
LPenev <lppenev@gmail.com>
2023-12-21 11:14:56 +0200

    02 Oldest Family Member Completed

---
+++ 02. Fundamentals/06.2. Objects and Classes - More Exercise/02 Oldest Family Member/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20._02_Oldest_Family_Member
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
