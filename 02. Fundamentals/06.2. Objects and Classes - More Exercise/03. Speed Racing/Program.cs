3d44f7bc79ed6cd9b44d57368d1dea5e40c477ac
LPenev <lppenev@gmail.com>
2023-12-22 11:48:37 +0200

    03. Speed Racing Completed

---
+++ 02. Fundamentals/06.2. Objects and Classes - More Exercise/03. Speed Racing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20._03._Speed_Racing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
