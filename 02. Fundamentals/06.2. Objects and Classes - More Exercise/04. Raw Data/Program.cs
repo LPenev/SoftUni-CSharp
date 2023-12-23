4473e00094e9e4ba8b1e34112cd8bc1774cabada
LPenev <lppenev@gmail.com>
2023-12-23 11:39:30 +0200

    04. Raw Data Completed

---
+++ 02. Fundamentals/06.2. Objects and Classes - More Exercise/04. Raw Data/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20._04._Raw_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
