834634278aa57a827575478547065ffe50651bbf
LPenev <lppenev@gmail.com>
2023-12-28 11:33:37 +0200

    02. Ascii Sumator Completed

---
+++ 02. Fundamentals/08.2. Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _202._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
