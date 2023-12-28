a4eb0d91df2c2eea5dcd64f83b94e63982cdb937
LPenev <lppenev@gmail.com>
2023-12-28 11:33:02 +0200

    01. Extract Person Information Completed

---
+++ 02. Fundamentals/08.2. Text Processing - More Exercise/01. Extract Person Information/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+
+namespace _201._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                
+                int startIndexOfName = input.IndexOf("@") + 1;
+                int lenghtOfName = input.IndexOf("|") - startIndexOfName;
+                string name = input.Substring(startIndexOfName, lenghtOfName);
+
+                int startIndexOfAge = input.IndexOf("#") + 1;
+                int lenghtOfAge = input.IndexOf("*") - startIndexOfAge;
+                string age = input.Substring(startIndexOfAge, lenghtOfAge);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
