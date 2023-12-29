df7871328779e4bdcac086a134d9e3e3f1f0d20f
LPenev <lppenev@gmail.com>
2023-12-29 11:20:13 +0200

    03. Treasure Finder Completed

---
+++ 02. Fundamentals/08.2. Text Processing - More Exercise/03. Treasure Finder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _203._Treasure_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
