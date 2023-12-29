c645ff002534347a3b9b00473f24bf2bbc9dd7f6
LPenev <lppenev@gmail.com>
2023-12-29 11:20:56 +0200

    04. Morse Code Translator Completed

---
+++ 02. Fundamentals/08.2. Text Processing - More Exercise/04. Morse Code Translator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _204._Morse_Code_Translator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
