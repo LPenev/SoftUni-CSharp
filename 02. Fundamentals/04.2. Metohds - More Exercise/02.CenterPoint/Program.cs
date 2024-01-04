9082ee061bb63a960f611d7c2f769b8c3a66361c
LPenev <lppenev@gmail.com>
2024-01-04 11:19:34 +0200

    02.CenterPoint Completed

---
+++ 02. Fundamentals/04.2. Metohds - More Exercise/02.CenterPoint/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02.CenterPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUT
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            //OUTPUT
+            if (FirstPointCoordinates(x1, y1) < SecondPointCoordinates(x2, y2))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else if (FirstPointCoordinates(x1, y1) > SecondPointCoordinates(x2, y2))
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else if (FirstPointCoordinates(x1, y1) == SecondPointCoordinates(x2, y2))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+        }
+
+        //ACTION
+        static double FirstPointCoordinates(double firstNum, double secondNum)
+        {
+            return Math.Abs(firstNum) + Math.Abs(secondNum);
+        }
+        static double SecondPointCoordinates(double firstNum, double secondNum)
+        {
+            return Math.Abs(firstNum) + Math.Abs(secondNum);
+        }
+    }
+}
