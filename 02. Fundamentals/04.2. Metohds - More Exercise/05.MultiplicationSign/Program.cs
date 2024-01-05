68d44c8df7da7816e8c0485a285389ec87e71779
LPenev <lppenev@gmail.com>
2024-01-05 12:28:52 +0200

    05.MultiplicationSign Completed

---
+++ 02. Fundamentals/04.2. Metohds - More Exercise/05.MultiplicationSign/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _05.MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUT
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int numThree = int.Parse(Console.ReadLine());
+
+            //OUTPUT
+            Console.WriteLine(PositiveOrNegative(numOne, numTwo, numThree));
+        }
+
+        //ACTION
+        static string PositiveOrNegative(int numOne, int numTwo, int numThree)
+        {
+            int negativeNumsCnt = 0;
+
+            //Zero Checher
+            if (numThree == 0 || numTwo == 0 || numThree == 0)
+            {
+                return "zero";
+            }
+
+            //Negative Numbers Counter
+            if (numOne < 0)
+            {
+                negativeNumsCnt++;
+            }
+            if (numTwo < 0)
+            {
+                negativeNumsCnt++;
+            }
+            if (numThree < 0)
+            {
+                negativeNumsCnt++;
+            }
+
+            //Positive Or Negative Sum
+            if (negativeNumsCnt % 2 == 0)
+            {
+                return "positive";
+            }
+            else
+            {
+                return "negative";
+            }
+        }
+    }
+}
