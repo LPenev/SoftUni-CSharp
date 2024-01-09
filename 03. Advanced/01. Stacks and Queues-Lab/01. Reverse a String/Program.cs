55dbf8e0afbe96108dc5687968ea309e6d414429
LPenev <lppenev@gmail.com>
2024-01-09 13:40:20 +0200

    01. Reverse a String Completed

---
+++ 03. Advanced/01. Stacks and Queues-Lab/01. Reverse a String/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<string>();
+            
+            foreach (var current in input)
+            {
+                stack.Push(current.ToString());
+            }
+
+            Console.WriteLine(string.Join("",stack));
+        }
+    }
+}
