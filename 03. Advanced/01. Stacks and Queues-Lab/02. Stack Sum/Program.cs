b908fe02c6c7a93941cdf9621b6f0cd7365aaca2
LPenev <lppenev@gmail.com>
2024-01-09 13:40:56 +0200

    02. Stack Sum Completed

---
+++ 03. Advanced/01. Stacks and Queues-Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _02._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputArray = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            var stack = new Stack<int>(inputArray);
+
+            var input = string.Empty;
+            while ((input = Console.ReadLine().ToLower()) != "end")
+            {
+                var commandArray = input.Split(' ');
+                var command = commandArray[0];
+                var token1 = int.Parse(commandArray[1]);
+
+                switch (command)
+                {
+                    case "add":
+                        var token2 = int.Parse(commandArray[2]);
+                        stack.Push(token1);
+                        stack.Push(token2);
+                        break;
+
+                    case "remove":
+                        int n = token1;
+                        while (n > 0 && stack.Count() > n)
+                        {
+                            stack.Pop();
+                            n--;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
