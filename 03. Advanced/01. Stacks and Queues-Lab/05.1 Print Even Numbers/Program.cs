3ae0d1444475a9ef6a335962db1034578d4b4f5a
LPenev <lppenev@gmail.com>
2024-01-10 11:26:31 +0200

    05.1 Print Even Numbers Completed

---
+++ 03. Advanced/01. Stacks and Queues-Lab/05.1 Print Even Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05._1_Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var evenArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var evenNumbers = new Queue<int>(evenArray);
+
+        }
+    }
+}
