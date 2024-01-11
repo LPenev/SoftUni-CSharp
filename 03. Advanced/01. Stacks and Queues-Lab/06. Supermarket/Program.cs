adae07ff0c3783e4136fdc4ce23aa5d8d918c067
LPenev <lppenev@gmail.com>
2024-01-11 11:08:25 +0200

    06. Supermarket Completed

---
+++ 03. Advanced/01. Stacks and Queues-Lab/06. Supermarket/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _06._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+
+            var input = string.Empty;
+            while((input = Console.ReadLine()) != "End")
+            {
+                if(input == "Paid")
+                {
+                    while (queue.Any())
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{queue.Count()} people remaining.");
+        }
+    }
+}
