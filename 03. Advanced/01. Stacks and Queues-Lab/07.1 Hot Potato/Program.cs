ec1205bc417d08557369fdb49c99b988be170efb
LPenev <lppenev@gmail.com>
2024-01-11 11:09:26 +0200

    07.1 Hot Potato Completed

---
+++ 03. Advanced/01. Stacks and Queues-Lab/07.1 Hot Potato/Program.cs
@@ -0,0 +1,32 @@
+﻿//Lucas Jacob Noah Logan Ethan
+//10
+
+//Removed Ethan
+//Removed Jacob
+//Removed Noah
+//Removed Lucas
+//Last is Logan
+
+namespace _07._1_Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var children = Console.ReadLine().Split();
+            var queue = new Queue<string>(children);
+
+            var counter = int.Parse(Console.ReadLine());
+
+            while(queue.Count > 1)
+            {
+                for(int i = 1; i < counter; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
