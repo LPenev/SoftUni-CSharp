3f1781df7060996328b71296e63e65ee060e2afe
LPenev <lppenev@gmail.com>
2024-01-12 12:18:51 +0200

    03. Maximum and Minimum Element Completed

---
+++ 03. Advanced/01.1 Stacks and Queues-Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _13._Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+
+            while (counter > 0)
+            {
+                int[] commandArray = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int command = commandArray[0];
+                switch (command)
+                {
+                    // push element to stack
+                    case 1:
+                        int elementToPush = commandArray[1];
+                        stack.Push(elementToPush);
+                        break;
+
+                    // pop element from stack
+                    case 2:
+                        if (stack.Any())
+                        {
+                            stack.Pop();
+                        }
+                        break;
+
+                    // print bigest element
+                    case 3:
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+
+                    // print smallest element
+                    case 4:
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        break;
+                }
+
+                counter--;
+            }
+            // print all elements from stack
+            if (stack.Any())
+            {
+                Console.WriteLine(string.Join(", ", stack));
+            }
+        }
+    }
+}
