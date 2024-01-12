8a3b746701aee9444003f3020eb28f993e0da10f
LPenev <lppenev@gmail.com>
2024-01-12 12:19:15 +0200

    04. Fast Food Completed

---
+++ 03. Advanced/01.1 Stacks and Queues-Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _14._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read quantity of Food
+            int quantityOfFood = int.Parse(Console.ReadLine());
+            // read orders split by whitespace
+            int[] ordersArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            // print bigest order
+            int bigestOrder = ordersArray.Max();
+            Console.WriteLine(bigestOrder);
+
+            // create Stack and add all orders
+            Queue<int> orders = new(ordersArray);
+
+            while (orders.Any() && quantityOfFood - orders.Peek() >= 0)
+            {
+                quantityOfFood -= orders.Peek();
+                orders.Dequeue();
+            }
+
+            if (orders.Any())
+            {
+                // print rest of orders
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+            else
+            {
+                // print message
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
