1761a63f601309de42a2dda45efb10970e80d4e6
LPenev <lppenev@gmail.com>
2024-01-13 11:54:50 +0200

    05. Fashion Boutique Completed

---
+++ 03. Advanced/01.1 Stacks and Queues-Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _15._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int boxCapacity = int.Parse(Console.ReadLine());
+
+            Stack<int> racks = new Stack<int>();
+
+            int sum = 0;
+            for(int i = 0; i < inputArray.Length; i++)
+            {
+                if(sum + inputArray[i] > boxCapacity)
+                {
+                    racks.Push(sum);
+                    sum = inputArray[i];
+                }
+                else
+                {
+                    sum += inputArray[i];
+                }
+            }
+
+            if(sum > 0)
+            {
+                racks.Push(sum);
+            }
+
+            Console.WriteLine(racks.Count);
+        }
+    }
+}
