740ba015b978309850510f0200d2ce6bb3c3e81c
LPenev <lppenev@gmail.com>
2024-01-17 11:30:21 +0200

    02. Average Student Grades Completed

---
+++ 03. Advanced/03. Sets-and-Dictionaries-Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,27 @@
+﻿int countOfStudets = int.Parse(Console.ReadLine());
+Dictionary<string,List<decimal>> students = new Dictionary<string,List<decimal>>();
+
+// add students to dictionary
+for (int i = 0; i < countOfStudets; i++)
+{
+    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string studentName = input[0];
+    decimal grade = decimal.Parse(input[1]);
+
+    if (!students.ContainsKey(studentName))
+    {
+        students.Add(studentName, new List<decimal>());
+    }
+    students[studentName].Add(grade);
+}
+
+// print result
+foreach(var student in students)
+{
+    Console.Write($"{student.Key} -> ");
+    foreach(var grade in student.Value)
+    {
+        Console.Write($"{grade:f2} ");
+    }
+    Console.WriteLine($"(avg: {student.Value.Average():f2})");
+}
