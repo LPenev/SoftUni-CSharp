32e2b6632c1685a3925dc46320c0b0cc81c82ca5
LPenev <lppenev@gmail.com>
2024-01-17 11:30:54 +0200

    03. Largest 3 Numbers Completed

---
+++ 03. Advanced/03. Sets-and-Dictionaries-Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int[] numbersArray = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .OrderByDescending(x=>x)
+    .Take(3)
+    .ToArray();
+
+Console.WriteLine(string.Join(" ", numbersArray));
