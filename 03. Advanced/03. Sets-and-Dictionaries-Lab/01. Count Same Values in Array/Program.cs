93551da16dc26b444f9f15b09b7cc1319c207aeb
LPenev <lppenev@gmail.com>
2024-01-17 11:29:32 +0200

    01. Count Same Values in Array Completed

---
+++ 03. Advanced/03. Sets-and-Dictionaries-Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,21 @@
+﻿double[] inputArray = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToArray();
+
+Dictionary<double, int> dict = new Dictionary<double, int>();
+
+foreach (var key in inputArray)
+{
+    if (!dict.ContainsKey(key))
+    {
+        dict.Add(key, 0);
+    }
+
+    dict[key]++;
+}
+
+foreach (var current in dict)
+{
+    Console.WriteLine($"{current.Key} - {current.Value} times");
+}
