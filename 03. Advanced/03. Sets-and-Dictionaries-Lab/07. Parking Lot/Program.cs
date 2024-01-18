5139a5b07bcd43fafd543742aeabfb343270a6a9
LPenev <lppenev@gmail.com>
2024-01-18 11:45:20 +0200

    07. Parking Lot Completed

---
+++ 03. Advanced/03. Sets-and-Dictionaries-Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,30 @@
+﻿string input = string.Empty;
+HashSet<string> bases = new HashSet<string>();
+
+// read from console and fillup HashSet
+while((input = Console.ReadLine()) != "END")
+{
+    string[] inputArray = input.Split(", ", StringSplitOptions.RemoveEmptyEntries); 
+    string command = inputArray[0];
+    string regNumber = inputArray[1];
+
+    if(command == "IN")
+    {
+        bases.Add(regNumber);
+    }
+    else if(command == "OUT")
+    {
+        bases.Remove(regNumber);
+    }
+}
+
+// print result
+if (!bases.Any()) {
+    Console.WriteLine("Parking Lot is Empty");
+    return;
+}
+
+foreach(var current in bases)
+{
+    Console.WriteLine(current);
+}
