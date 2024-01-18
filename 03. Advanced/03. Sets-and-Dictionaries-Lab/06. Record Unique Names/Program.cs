5c6df1a455168eb64d9b0bcf2ece671ede4118e7
LPenev <lppenev@gmail.com>
2024-01-18 11:44:39 +0200

    06. Record Unique Names Completed

---
+++ 03. Advanced/03. Sets-and-Dictionaries-Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,21 @@
+﻿int countOfNames = int.Parse(Console.ReadLine());
+
+HashSet<string> names = new HashSet<string>();
+
+// fill HashSet with unique names
+for(int i = 0; i < countOfNames; i++)
+{
+    string name = Console.ReadLine();
+
+    // check for name is in names exist.
+    if (!names.Contains(name))
+    {
+        names.Add(name);
+    }
+}
+
+// print names from HashSet
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
