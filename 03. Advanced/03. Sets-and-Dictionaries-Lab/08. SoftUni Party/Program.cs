856f1a63e54cc6556efd5094bd03b4f5749e0bf3
LPenev <lppenev@gmail.com>
2024-01-18 11:45:38 +0200

    08. SoftUni Party Completed

---
+++ 03. Advanced/03. Sets-and-Dictionaries-Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,39 @@
+﻿HashSet<string> vipGuests = new HashSet<string>();
+HashSet<string> regularGuests = new HashSet<string>();
+
+string input = string.Empty;
+while ((input = Console.ReadLine()) != "PARTY")
+{
+    if (char.IsDigit(input[0]))
+    {
+        vipGuests.Add(input);
+    }
+    else
+    {
+        regularGuests.Add(input);
+    }
+}
+
+while ((input = Console.ReadLine()) != "END")
+{
+    if (char.IsDigit(input[0]))
+    {
+        vipGuests.Remove(input);
+    }
+    else
+    {
+        regularGuests.Remove(input);
+    }
+}
+
+// print result
+Console.WriteLine(vipGuests.Count + regularGuests.Count);
+foreach (string number in vipGuests)
+{
+    Console.WriteLine(number);
+}
+
+foreach (string number in regularGuests)
+{
+    Console.WriteLine(number);
+}
