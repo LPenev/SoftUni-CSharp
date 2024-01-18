511b80bdc8acb261fcd6a936fa7545f9fe4a8800
LPenev <lppenev@gmail.com>
2024-01-18 11:47:54 +0200

    03. Periodic Table Completed

---
+++ 03. Advanced/03.1 CSharp-Advanced-Sets-and-Dictionaries-Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,11 @@
+﻿int countOfElements = int.Parse(Console.ReadLine());
+SortedSet<string> sortedSetOfInt = new();
+
+for (int i = 0; i < countOfElements; i++)
+{
+    string[] inputArray = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    
+    sortedSetOfInt.UnionWith(inputArray);
+}
+
+Console.WriteLine(string.Join(" ", sortedSetOfInt));
