3c0b2a94fadad23816e95657d4839550f5615130
LPenev <lppenev@gmail.com>
2024-01-18 11:46:45 +0200

    01. Unique Usernames Completed

---
+++ 03. Advanced/03.1 CSharp-Advanced-Sets-and-Dictionaries-Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,13 @@
+﻿int countOfUSers = int.Parse(Console.ReadLine());
+
+HashSet<string> users = new HashSet<string>();
+
+for(int i = 0; i < countOfUSers; i++)
+{
+    users.Add(Console.ReadLine());
+}
+
+foreach(string user in users)
+{
+    Console.WriteLine(user);
+}
