f1fbc2e46843d4f45f9ff1f025c374b191eeca99
LPenev <lppenev@gmail.com>
2024-01-19 12:26:32 +0200

    04. Even Times Completed

---
+++ 03. Advanced/03.1 CSharp-Advanced-Sets-and-Dictionaries-Exercise/04. Even Times/Program.cs
@@ -0,0 +1,14 @@
+﻿int countOfInputs = int.Parse(Console.ReadLine());
+Dictionary<int, int> digits = new();
+
+for(int i = 0; i < countOfInputs; i++)
+{
+    int current = int.Parse(Console.ReadLine());
+    if (!digits.ContainsKey(current))
+    {
+        digits.Add(current, 0);
+    }
+    digits[current]++;
+}
+
+Console.WriteLine(digits.Single(x => x.Value%2 == 0).Key);
