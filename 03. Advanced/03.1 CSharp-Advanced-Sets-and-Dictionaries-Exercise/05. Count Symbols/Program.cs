189d02fdc5a8e5e34a2597a67ba8199fa18033f7
LPenev <lppenev@gmail.com>
2024-01-19 12:27:11 +0200

    05. Count Symbols Completed

---
+++ 03. Advanced/03.1 CSharp-Advanced-Sets-and-Dictionaries-Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,16 @@
+﻿SortedDictionary<char, int> charCounts = new();
+
+char[] inputArray = Console.ReadLine().ToCharArray();
+foreach (char current in inputArray)
+{
+    if (!charCounts.ContainsKey(current))
+    {
+        charCounts.Add(current, 0);
+    }
+    charCounts[current]++;
+}
+
+foreach (var current in charCounts)
+{
+    Console.WriteLine($"{current.Key}: {current.Value} time/s");
+}
