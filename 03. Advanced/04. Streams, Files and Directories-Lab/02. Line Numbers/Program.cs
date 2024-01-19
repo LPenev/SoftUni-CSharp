4a0fc177e8e99e31ac0fe23225e8a736c84d91f3
LPenev <lppenev@gmail.com>
2024-01-19 12:30:57 +0200

    02. Line Numbers Completed

---
+++ 03. Advanced/04. Streams, Files and Directories-Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    string currentLine = string.Empty;
+                    int counter = 1;
+
+                    while((currentLine = reader.ReadLine()) != null)
+                    {
+                        currentLine = $"{counter}. " + currentLine;
+                        writer.WriteLine(currentLine);
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
