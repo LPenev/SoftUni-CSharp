79fcdc3f7ed2b1cd9aa5f826f5c8579a802aa7a3
LPenev <lppenev@gmail.com>
2024-01-19 12:30:22 +0200

    01. Odd Lines Completed

---
+++ 03. Advanced/04. Streams, Files and Directories-Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            string currentLine = string.Empty;
+            int count = 0;
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+
+                        if (++count % 2 == 0)
+                        {
+                            writer.WriteLine(currentLine);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
