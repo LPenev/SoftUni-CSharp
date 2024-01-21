107831b6eb88f5ba12ee0e38d4114ce5484de1e0
LPenev <lppenev@gmail.com>
2024-01-21 11:38:05 +0200

    02. Line Numbers Completed

---
+++ 03. Advanced/04.1 Streams, Files and Directories-Exercise/02. Line Numbers/LineNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int letersCount = lines[i].Count(char.IsLetter);
+                int puntuationCount = lines[i].Count(char.IsPunctuation);
+
+                sb.AppendLine($"Line {i+1} {lines[i]} ({letersCount})({puntuationCount})");
+            }
+
+            File.WriteAllText(outputFilePath, sb.ToString());
+        }
+    }
+}
