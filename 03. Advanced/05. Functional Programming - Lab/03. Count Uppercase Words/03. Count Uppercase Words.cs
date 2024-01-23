a4d6c8f33d7d4f96add107e7dc468ff748c14907
LPenev <lppenev@gmail.com>
2024-01-23 10:37:56 +0200

    03. Count Uppercase Words Completed

---
+++ 03. Advanced/05. Functional Programming - Lab/03. Count Uppercase Words/03. Count Uppercase Words.cs
@@ -0,0 +1,8 @@
+﻿Func<string, bool> isStartWithUpper = x => Char.IsUpper(x[0]);
+
+string[] words = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Where(isStartWithUpper)
+    .ToArray();
+
+Console.WriteLine(String.Join("\n", words));
