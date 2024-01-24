94fff6b35f3bba049f0c8d1c4266442a74f88f75
LPenev <lppenev@gmail.com>
2024-01-24 11:30:15 +0200

    01. Action Print Completed

---
+++ 03. Advanced/05.1. Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,6 @@
+﻿Func<string, string[]> stringToArray = words => words.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+string[] words = stringToArray(Console.ReadLine());
+
+// print result
+Action <string[]> print = words => Console.WriteLine(String.Join("\n", words));
+print(words);
