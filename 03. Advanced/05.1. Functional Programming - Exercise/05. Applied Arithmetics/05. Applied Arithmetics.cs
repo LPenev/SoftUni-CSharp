b8e9fd19a894114719bb68e1bf44b4d43fb48ffe
LPenev <lppenev@gmail.com>
2024-01-24 11:32:50 +0200

    05. Applied Arithmetics Completed

---
+++ 03. Advanced/05.1. Functional Programming - Exercise/05. Applied Arithmetics/05. Applied Arithmetics.cs
@@ -0,0 +1,40 @@
+﻿Func<string, int[]> readIntegers = input => input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+.Select(int.Parse)
+.ToArray();
+
+int[] inputArray = readIntegers(Console.ReadLine());
+
+Controler(Console.ReadLine(), inputArray);
+
+void Controler(string command, int[] numbers)
+{
+    Func<int[], int[]> calculator = null;
+    Action<int[]> printer = numbers => Console.WriteLine(String.Join(" ", numbers));
+
+    switch (command)
+    {
+        case "add":
+            calculator = number => number.Select(x => ++x).ToArray();
+            numbers = calculator(numbers);
+            break;
+
+        case "multiply":
+            calculator = number => number.Select(x => x * 2).ToArray();
+            numbers = calculator(numbers);
+            break;
+
+        case "subtract":
+            calculator = number => number.Select(x => --x).ToArray();
+            numbers = calculator(numbers);
+            break;
+
+        case "print":
+            printer(numbers);
+            break;
+
+        case "end":
+            return;
+    }
+
+    Controler(Console.ReadLine(), numbers);
+}
