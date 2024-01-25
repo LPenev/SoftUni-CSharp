4b0db7e711c1610f84fd09e9b4fbb9d30d1d34c1
LPenev <lppenev@gmail.com>
2024-01-25 11:11:45 +0200

    06. Reverse And Exclude Completed

---
+++ 03. Advanced/05.1. Functional Programming - Exercise/06. Reverse And Exclude/06. Reverse And Exclude.cs
@@ -0,0 +1,16 @@
+﻿Func<string, int[]> readIntegers = input => input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+.Select(int.Parse)
+.ToArray();
+
+Func<int[], int, int[]> checkForDevisibleBy = (input, givenInt) => input.Where(x=> x % givenInt != 0).ToArray();
+
+Func<int[], int[]> reverseArray = digits => digits.Reverse().ToArray();
+
+int[] inputArray = readIntegers(Console.ReadLine());
+int givenInteger = int.Parse(Console.ReadLine());
+
+
+int[] filtredDigits = checkForDevisibleBy(inputArray, givenInteger);
+int[] revesedDigits = reverseArray(filtredDigits);
+
+Console.WriteLine(String.Join(" ", revesedDigits));
