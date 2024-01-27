9041b519de3a95fdb2c8f4c179931d660fe1e9b4
LPenev <lppenev@gmail.com>
2024-01-27 11:35:25 +0200

    01. Define a Class Person Completed

---
+++ 03. Advanced/06.1. Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person peter = new("Peter", 20);
+            Person george = new("George", 18);
+            Person jose = new("Peter", 43);
+        }
+    }
+}
