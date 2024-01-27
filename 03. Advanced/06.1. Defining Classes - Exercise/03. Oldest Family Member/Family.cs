e26d6935b26b33090a92fe0135770f48e8379844
LPenev <lppenev@gmail.com>
2024-01-27 11:36:36 +0200

    03. Oldest Family Member Completed

---
+++ 03. Advanced/06.1. Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,24 @@
+﻿namespace DefiningClasses
+{
+    internal class Family
+    {
+        private List<Person> people = new List<Person>();
+
+        public List<Person> People { get { return people; } set { people = value; } }
+
+        public Family()
+        {
+            List<Person> list = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            people.Add(member);
+        }
+
+        public Person GetOldestPerson()
+        {
+            return People.MaxBy(x => x.Age);
+        }
+    }
+}
---
+++ 03. Advanced/06.1. Defining Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family family = new();
+
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                family.AddMember(new Person(name, age));
+            }
+
+            var oldestPerson = family.GetOldestPerson();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
