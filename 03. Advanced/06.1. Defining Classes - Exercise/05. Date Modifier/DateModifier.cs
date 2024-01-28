f508b4728c561148fc9324b4cc00154c3fcfd3f9
LPenev <lppenev@gmail.com>
2024-01-28 11:06:51 +0200

    05. Date Modifier Completed

---
+++ 03. Advanced/06.1. Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,14 @@
+﻿namespace DefiningClasses
+{
+    public static class DateModifier
+    {
+		public static int GetDifference(string start, string end)
+		{
+			var startDate = DateTime.Parse(start);
+			var endDate = DateTime.Parse(end);
+			var difference = endDate.Subtract(startDate);
+			var result = Math.Abs(difference.Days);
+			return result;
+		}
+    }
+}
---
+++ 03. Advanced/06.1. Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            var result = DateModifier.GetDifference(startDate, endDate);
+            Console.WriteLine(result);
+        }
+    }
+}
