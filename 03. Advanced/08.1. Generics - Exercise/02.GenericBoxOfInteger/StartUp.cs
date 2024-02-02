43503c696bacdadda4fd8adc9fbebe25033409ed
LPenev <lppenev@gmail.com>
2024-02-02 11:24:23 +0200

    02.GenericBoxOfInteger Completed

---
+++ 03. Advanced/08.1. Generics - Exercise/02.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace BoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Box<int>> boxes = new();
+
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new(input);
+                boxes.Add(box);
+            }
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
