e7ed4d22d4660f5d4f9f3453b466afbf4c02f708
LPenev <lppenev@gmail.com>
2024-02-02 11:24:56 +0200

    03.GenericSwapMethodString Completed

---
+++ 03. Advanced/08.1. Generics - Exercise/03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = new List<string>();
+
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                var inputItem = Console.ReadLine();
+                items.Add(inputItem);
+            }
+
+            var index = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Box<string> box = new Box<string>(items, index[0], index[1]);
+        }
+    }
+}
