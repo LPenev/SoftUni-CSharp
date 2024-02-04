234e7e49163c2574adb6c91c272253f3b1db0cce
LPenev <lppenev@gmail.com>
2024-02-04 11:31:41 +0200

    06.GenericCountMethodDouble Completed

---
+++ 03. Advanced/08.1. Generics - Exercise/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,32 @@
+﻿namespace GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public int CompareToItem(T itemToCompare)
+        {
+            int count = 0;
+
+            foreach (T item in list)
+            {
+
+                if (item.CompareTo(itemToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
