c7dc2c3d59fd02a4e60a33f60f0a1478bfe36847
LPenev <lppenev@gmail.com>
2024-02-04 11:32:07 +0200

    07.Tuple Completed

---
+++ 03. Advanced/08.1. Generics - Exercise/07.Tuple/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAndAddress = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            TupleClass<string, string> nameWithAdress 
+                = new TupleClass<string, string>($"{nameAndAddress[0]} {nameAndAddress[1]}", nameAndAddress[2]);
+
+            string[] nameAndBeer = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            
+            TupleClass<string, int> nameWithBeer 
+                = new TupleClass<string, int>(nameAndBeer[0], int.Parse(nameAndBeer[1]));
+
+            string[] numArray = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            TupleClass<int, double> numbers 
+                = new TupleClass<int, double>(int.Parse(numArray[0]), double.Parse(numArray[1]));
+            
+            Console.WriteLine(nameWithAdress);
+            Console.WriteLine(nameWithBeer);
+            Console.WriteLine(numbers);
+        }
+    }
+}
