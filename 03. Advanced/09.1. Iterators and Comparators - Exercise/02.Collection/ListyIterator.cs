4dc3fb1b50d0b2a2ce900e7ab96a071c9c9ff11a
LPenev <lppenev@gmail.com>
2024-02-07 11:45:55 +0200

    02.Collection Completed

---
+++ 03. Advanced/09.1. Iterators and Comparators - Exercise/02.Collection/ListyIterator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace _02.Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> list;
+        private int index;
+
+        public T Current => throw new NotImplementedException();
+
+        public ListyIterator(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < list.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(list[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in list)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ 03. Advanced/09.1. Iterators and Comparators - Exercise/02.Collection/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _02.Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();
+            ListyIterator<string> list = new(words);
+
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+
+                    case "PrintAll":
+                        foreach (var current in list)
+                        {
+                            Console.Write(current + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
