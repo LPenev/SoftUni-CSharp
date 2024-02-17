c6edaa39e4a30848fbb0180909230d48d7317117
LPenev <lppenev@gmail.com>
2024-02-17 15:13:05 +0200

    01.Chicken Snack Completed

---
+++ 03. Advanced/C# Advanced Regular Exam - 17 February 2024/01.Chicken Snack/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _01.Chicken_Snack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> amountOfMoney = new(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            Queue<int> prices = new(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int eatCounter = 0;
+            int restOfMoney = 0;
+
+            while(amountOfMoney.Count > 0 && prices.Count > 0)
+            {
+                int currentMoney = amountOfMoney.Pop();
+                int price = prices.Dequeue();
+
+                if(currentMoney + restOfMoney == price)
+                {
+                    eatCounter++;
+                }
+                else if(currentMoney + restOfMoney > price)
+                {
+                    eatCounter++;
+                    restOfMoney = currentMoney + restOfMoney - price;
+
+                }
+            }
+
+            if(eatCounter >= 4)
+            {
+                Console.WriteLine($"Gluttony of the day! Henry ate {eatCounter} foods.");
+            }
+            else if(eatCounter > 1)
+            {
+                Console.WriteLine($"Henry ate: {eatCounter} foods.");
+            }
+            else if (eatCounter == 1)
+            {
+                Console.WriteLine($"Henry ate: {eatCounter} food.");
+            }
+            else
+            {
+                Console.WriteLine("Henry remained hungry. He will try next weekend again.");
+            }
+        }
+    }
+}
