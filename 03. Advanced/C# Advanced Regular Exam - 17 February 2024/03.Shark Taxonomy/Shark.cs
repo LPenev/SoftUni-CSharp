7454cbbfea825d689db11dc4b66962d2f9cc67a8
LPenev <lppenev@gmail.com>
2024-02-17 15:14:18 +0200

    03.Shark Taxonomy Completed

---
+++ 03. Advanced/C# Advanced Regular Exam - 17 February 2024/03.Shark Taxonomy/Shark.cs
@@ -0,0 +1,28 @@
+﻿namespace SharkTaxonomy
+{
+    public class Shark
+    {
+        private string kind;
+        private int length;
+        private string food;
+        private string habitat;
+
+        public Shark(string kind, int length, string food, string habitat)
+        {
+            this.Kind = kind;
+            this.Length = length;
+            this.Food = food;
+            this.Habitat = habitat;
+        }
+
+        public string Kind { get; set; }
+        public int Length { get; set; }
+        public string Food { get; set; }
+        public string Habitat { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Kind} shark: {Length}m long.{Environment.NewLine}Could be spotted in the {Habitat}, typical menu: {Food}";
+        }
+    }
+}
