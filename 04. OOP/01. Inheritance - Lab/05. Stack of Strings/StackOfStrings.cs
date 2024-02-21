58053ddbeee1d46799aaf32a9d21b4a507b572eb
LPenev <lppenev@gmail.com>
2024-02-21 11:37:01 +0200

    05. Stack of Strings Completed

---
+++ 04. OOP/01. Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> inputedRange)
+        {
+            foreach (var item in inputedRange)
+            {
+                this.Push(item);
+            }
+        }
+
+    }
+}
