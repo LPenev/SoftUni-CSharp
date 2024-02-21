832ab33a10cc0ede9d9cfddb58aa06bae1180e9e
LPenev <lppenev@gmail.com>
2024-02-21 11:36:29 +0200

    04. Random List Completed

---
+++ 04. OOP/01. Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("11");
+            list.Add("22");
+            list.Add("33");
+            list.Add("44");
+            list.Add("55");
+
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+
+            list.RemoveRandomElement();
+            list.RemoveRandomElement();
+
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+
+        }
+    }
+}
