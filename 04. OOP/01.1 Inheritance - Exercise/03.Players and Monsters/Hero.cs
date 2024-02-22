b544b8d1e0d43b3b482fd75b68a76975078b7a80
LPenev <lppenev@gmail.com>
2024-02-22 11:14:37 +0200

    03.Players and Monsters Completed

---
+++ 04. OOP/01.1 Inheritance - Exercise/03.Players and Monsters/Hero.cs
@@ -0,0 +1,19 @@
+﻿namespace PlayersAndMonsters
+{
+    public abstract class Hero
+    {
+        public Hero(string username, int level)
+        {
+            this.Level = level;
+            this.Username = username;
+        }
+
+        public string Username { get; set; }
+        public int Level { get; set; }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
