d34411a47dc443ce2dc59cccae43de5ad4db7614
LPenev <lppenev@gmail.com>
2024-02-24 12:20:19 +0200

    01.Persons Completed

---
+++ 04. OOP/02. Encapsulation - Lab/01.Persons/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            int personCount = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < personCount; i++)
+            {
+                string[] inputArray = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                
+                string firstName = inputArray[0];
+                string lastName = inputArray[1];
+                int age = int.Parse(inputArray[2]);
+                
+                Person person = new(firstName, lastName, age);
+                persons.Add(person);
+            }
+
+            foreach (Person person in persons.OrderBy(x=>x.FirstName).ThenBy(x=>x.Age))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
