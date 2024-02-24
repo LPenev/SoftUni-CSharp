21af39f7175f7b6d0caf964b59f84299113d9a2f
LPenev <lppenev@gmail.com>
2024-02-24 12:21:57 +0200

    04.Team Completed

---
+++ 04. OOP/02. Encapsulation - Lab/04.Team/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team("SoftUni");
+
+            int personsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < personsCount; i++)
+            {
+                string[] inputArray = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string firstName = inputArray[0];
+                string lastName = inputArray[1];
+                int age = int.Parse(inputArray[2]);
+                decimal salary = decimal.Parse(inputArray[3]);
+
+                try
+                {
+                    Person person = new(firstName, lastName, age, salary);
+                    team.AddPlayer(person);
+
+                } catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ 04. OOP/02. Encapsulation - Lab/04.Team/Team.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        List<Person> firstTeam;
+        List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name 
+        { 
+            get { return this.name; }  
+        }
+        public IReadOnlyCollection<Person> FirstTeam => this.firstTeam;
+        public IReadOnlyCollection<Person> ReserveTeam => this.reserveTeam;
+
+        public void AddPlayer (Person person)
+        {
+            if(person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
