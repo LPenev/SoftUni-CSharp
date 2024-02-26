4fddb39bb524831a9f8983a317f37a97351ceadf
LPenev <lppenev@gmail.com>
2024-02-26 14:48:31 +0200

    05.Football Team Generator Completed

---
+++ 04. OOP/02.1. Encapsulation - Exercise/05.Football Team Generator/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace _05.Football_Team_Generator
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
