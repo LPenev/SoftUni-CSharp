2c4c2c3d9a1c18e4db904f4004463d6995ba5617
LPenev <lppenev@gmail.com>
2024-02-29 15:40:00 +0200

    03. Telephony Completed

---
+++ 04. OOP/03.1. Interfaces and Abstraction - Exercise/03. Telephony/IO/ConsoleWriter.cs
@@ -0,0 +1,9 @@
+﻿using Telephony.IO.Interfaces;
+
+namespace Telephony.IO
+{
+    internal class ConsoleWriter : IWriter
+    {
+        public void WriteLine(string line) => Console.WriteLine(line);
+    }
+}
---
+++ 04. OOP/03.1. Interfaces and Abstraction - Exercise/03. Telephony/Models/Smartphone.cs
@@ -0,0 +1,28 @@
+﻿using Telephony.Models.Interfaces;
+
+namespace Telephony.Models
+{
+    public class Smartphone : ICallable, IBrowsable
+    {
+
+        public string Call(string number)
+        {
+            if (!number.All(x=> char.IsDigit(x)))
+            {
+                throw new ArgumentException("Invalid number!");
+            }
+
+            return $"Calling... {number}";
+        }
+        
+        public string Browse(string url)
+        {
+            if (url.Any(x => char.IsDigit(x)))
+            {
+                throw new ArgumentException("Invalid URL!");
+            }
+
+            return $"Browsing: {url}!";
+        }
+    }
+}
