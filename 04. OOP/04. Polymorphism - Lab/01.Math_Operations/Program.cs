7e0aabffd7b6430f56c829f0ada6314b0968520a
LPenev <lppenev@gmail.com>
2024-03-01 15:09:58 +0200

    01.Math_Operations Completed

---
+++ 04. OOP/04. Polymorphism - Lab/01.Math_Operations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.Math_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
