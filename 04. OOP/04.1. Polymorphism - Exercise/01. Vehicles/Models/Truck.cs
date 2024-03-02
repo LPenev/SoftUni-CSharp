9d9f09a7eab2ce175ddccfddeb2175b143c01471
LPenev <lppenev@gmail.com>
2024-03-02 14:25:37 +0200

    01. Vehicles Completed

---
+++ 04. OOP/04.1. Polymorphism - Exercise/01. Vehicles/Models/Truck.cs
@@ -0,0 +1,16 @@
+﻿namespace Vehicles.Models
+{
+    public class Truck : Vehicle
+    {
+        private const double IncreasedConsumption = 1.6;
+        public Truck(double fuelQuantity, double fuelConsumtion) 
+            : base(fuelQuantity, fuelConsumtion, IncreasedConsumption)
+        {
+        }
+
+        public override void Refuel(double amount)
+        {
+            base.Refuel(amount * 0.95);
+        }
+    }
+}
