8fb5b7bcca23f74775ded67097f6a2cdc2abb4db
LPenev <lppenev@gmail.com>
2024-03-07 14:35:26 +0200

    02. Enter Numbers Completed

---
+++ 04. OOP/05. Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICollection<int> numbers = new List<int>();
+            int start = 1;
+            int end = 100;
+            
+            while (numbers.Count < 10 && start < 99)
+            {
+                start = ReadNumber(start, end);
+            }
+
+            Console.WriteLine(String.Join(", ", numbers));
+
+            int ReadNumber(int start, int end)
+            {
+                int number = 0;
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+
+                    if (number <= start || number >= end)
+                    {
+                        throw new ArgumentException($"Your number is not in range {start} - {end}!");
+                    }
+                    numbers.Add(number);
+                    start = number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return start;
+            }
+        }
+    }
+}
