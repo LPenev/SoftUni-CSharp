15609dbdeb8701fd6c236ce033db0004bf6b24a7
LPenev <lppenev@gmail.com>
2024-03-07 14:34:48 +0200

    01. Square Root Completed

---
+++ 04. OOP/05. Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+
+                if(number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
