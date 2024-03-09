ff6b895f2757a3cf5ac2debe0cac945bd4045930
LPenev <lppenev@gmail.com>
2024-03-09 14:34:18 +0200

    04. Sum of Integers Completed

---
+++ 04. OOP/05. Exceptions and Error Handling - Lab/04. Sum of Integers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine()
+                .Split(' ');
+            int sum = 0;
+
+            foreach (string currentElement in inputArray)
+            {
+                try
+                {
+                    int number = int.Parse(currentElement);
+                    sum += number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{currentElement}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{currentElement}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{currentElement}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
