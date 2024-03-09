b8c09118186d51acdd372336c6a4f8062498a2e6
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-09 18:33:15 +0100

    Create a new projects from Polymorphism Exercise.

---
+++ 04. OOP/04.1. Polymorphism - Exercise/04. Wild Farm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04._Wild_Farm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
