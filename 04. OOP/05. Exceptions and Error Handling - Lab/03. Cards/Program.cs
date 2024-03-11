1f7f31a17b085d1b81861bd926277da31444c381
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-11 19:56:02 +0100

    Unit 05 ist created

---
+++ 04. OOP/05. Exceptions and Error Handling - Lab/03. Cards/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03._Cards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
