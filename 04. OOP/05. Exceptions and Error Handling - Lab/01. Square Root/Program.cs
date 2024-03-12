75413141611bb3275bfa356e7666ab0d85b93ede
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-12 21:14:04 +0100

    The task is sloved

--- 04. OOP/05. Exceptions and Error Handling - Lab/01. Square Root/Program.cs
+++ 04. OOP/05. Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace _01._Square_Root
+﻿namespace SquareRoot
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+
+                if(number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
         }
     }
 }
