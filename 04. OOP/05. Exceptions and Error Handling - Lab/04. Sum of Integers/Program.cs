c6d2250fa10e96f494a9477a71b2e6c8da16fc5c
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-13 00:41:55 +0100

    namespace updated

--- 04. OOP/05. Exceptions and Error Handling - Lab/04. Sum of Integers/Program.cs
+++ 04. OOP/05. Exceptions and Error Handling - Lab/04. Sum of Integers/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace _04._Sum_of_Integers
+﻿namespace SumOfIntegers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
