619a2c4227f78ede7c00c6b84dbac8a3e68bb810
LPenev <lppenev@gmail.com>
2024-03-13 16:28:19 +0200

    01. Stream_Progress Completed

---
+++ 04. OOP/06. CSharp-OOP-SOLID-Lab/01. Stream_Progress/Program.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Music musicFile = new Music("Iron Maiden", "Fear of the dark", 3, 20);
+
+            StreamProgressInfo streamProgressInfo = new StreamProgressInfo(musicFile);
+            Console.WriteLine(streamProgressInfo.CalculateCurrentPercent());
+        }
+    }
+}
