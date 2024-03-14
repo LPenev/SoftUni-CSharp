de4d0eb8de7a24bc9f4f5ba8cae505b57ccaebdd
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-14 23:49:09 +0100

    program changed new employee and robot created

--- 04. OOP/07. CSharp-OOP-SOLID-Lab/04. Recharge/Program.cs
+++ 04. OOP/07. CSharp-OOP-SOLID-Lab/04. Recharge/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace P04.Recharge
+﻿using Models;
+
+namespace P04.Recharge
 {
     class Program
     {
         static void Main()
         {
+            Employee employee = new Employee("222");
+            Robot robot = new Robot("223", 200);
         }
     }
 }
