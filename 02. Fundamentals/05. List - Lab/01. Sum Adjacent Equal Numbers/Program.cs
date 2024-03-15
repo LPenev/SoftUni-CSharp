24f8a426d0061fd3589260dee654589b17b04c79
LPenev <lppenev@gmail.com>
2024-03-15 16:30:29 +0100

    Task 01 completed

---
+++ 02. Fundamentals/05. List - Lab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            
+            bool isAdjacent = true;
+
+            while (isAdjacent)
+            {
+                isAdjacent = SumAdjacentEqual(numbers);
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static bool SumAdjacentEqual (List<double> numbers)
+        {
+            for(int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] += numbers[i + 1];
+                    numbers.RemoveAt(i+1);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
