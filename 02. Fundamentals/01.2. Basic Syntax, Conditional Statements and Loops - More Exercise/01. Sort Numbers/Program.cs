e32bc1fc4ded1752de7d277b8d6bf08eccddefbe
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-16 23:41:48 +0100

    Tasks Completed

---
+++ 02. Fundamentals/01.2. Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            double n3 = double.Parse(Console.ReadLine());
+
+            double swap = 0;
+
+            while ( n1 < n2 || n3 > n2 || n2 > n1)
+            {
+                if (n3 > n1)
+                {
+                    swap = n1;
+                    n1 = n3;
+                    n3 = swap;
+                }
+
+                if (n2 > n1)
+                {
+                    swap = n1;
+                    n1 = n2;
+                    n2 = swap;
+                }
+
+                if (n3 > n2)
+                {
+                    swap = n2;
+                    n2 = n3;
+                    n3 = swap;
+                }
+            }
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+        }
+    }
+}
---
+++ 02. Fundamentals/01.2. Basic Syntax, Conditional Statements and Loops - More Exercise/04. Reverse String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace reverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string newString = "";
+
+            for (int i = input.Length-1; i >= 0 ; i--)
+            {
+                newString += input[i];
+            }
+            Console.WriteLine(newString);
+        }
+    }
+}
