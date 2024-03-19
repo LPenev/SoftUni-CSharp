51dbd841b12a8e480009e8483f8082b1b88ef091
LPenev <lppenev@gmail.com>
2024-03-19 14:38:18 +0200

    01. Stealer Completed

---
+++ 04. OOP/07. Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string nameOfTheClass, params string[] fields)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type typeOfClass = Type.GetType(nameOfTheClass);
+            FieldInfo[] fieldsInfo = typeOfClass.GetFields((BindingFlags)60);
+            Object instancedClass = Activator.CreateInstance(typeOfClass);
+
+            sb.AppendLine($"Class under investigation: {instancedClass.ToString()}");
+
+            foreach (FieldInfo field in fieldsInfo.Where(x => fields.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instancedClass)}");
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
