0df5781f1cd093106f5d66828a186ea0497316e7
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-19 23:18:36 +0100

    classes CommandInterpreter and Engine created folder Commands created

---
+++ 04. OOP/08.1. Reflection and Attributes - Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,13 @@
+﻿
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ 04. OOP/08.1. Reflection and Attributes - Exercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,20 @@
+﻿
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private ICommandInterpreter command;
+
+        public Engine(ICommandInterpreter command)
+        {
+            this.command = command;
+        }
+
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
