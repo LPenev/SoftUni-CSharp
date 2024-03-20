26bda439749c1e92a43d53c6a7754c65bd0f88c2
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-20 22:25:48 +0100

    Task 05 and 06 completed

---
+++ 02. Fundamentals/02. Data Types and Variables/06. Reversed Chars/Program.cs
@@ -0,0 +1,5 @@
+﻿char input1 = char.Parse(Console.ReadLine());
+char input2 = char.Parse(Console.ReadLine());
+char input3 = char.Parse(Console.ReadLine());
+
+Console.WriteLine(input3 + " " + input2 + " " + input1);
