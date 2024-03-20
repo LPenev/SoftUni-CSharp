b144013170a694b4dc4f2d939cd4ec18ef7b0b99
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-20 23:34:49 +0100

    CommandInterpreter completed

--- 04. OOP/08.1. Reflection and Attributes - Exercise/CommandPattern/Core/CommandInterpreter.cs
+++ 04. OOP/08.1. Reflection and Attributes - Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -1,13 +1,33 @@
-﻿
-using CommandPattern.Core.Contracts;
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace CommandPattern.Core
 {
     public class CommandInterpreter : ICommandInterpreter
     {
+        private string CommndNotFound = "Command not found.";
+
         public string Read(string args)
         {
-            throw new System.NotImplementedException();
+
+            string[] inputArray = args
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string commandName = inputArray[0];
+            string[] commandArgs = inputArray.Skip(1).ToArray();
+
+            Type commandType = Assembly.GetEntryAssembly().GetType($"{commandName}Command");
+
+            if (commandType == null)
+            {
+                throw new InvalidOperationException(CommndNotFound);
+            }
+
+            ICommand commandInstance = Activator.CreateInstance(commandType) as ICommand;
+            string result = commandInstance.Execute(commandArgs);
+
+            return result.ToString();
         }
     }
 }
