a56513b2beb7bb9ef24877545384d31a21acf331
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-20 22:48:41 +0100

    Task 09 completed

---
+++ 02. Fundamentals/02. Data Types and Variables/09. Chars to String/Program.cs
@@ -0,0 +1,8 @@
+﻿char input1 = char.Parse(Console.ReadLine());
+char input2 = char.Parse(Console.ReadLine());
+char input3 = char.Parse(Console.ReadLine());
+
+// string text = $"{input1}{input2}{input3}";
+string text2 = string.Concat(input1, input2, input3);
+
+Console.WriteLine(text2);
