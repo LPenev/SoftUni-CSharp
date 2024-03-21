310124964eb6ecba56750514ee7b8426267a66e3
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-21 19:07:27 +0100

    class Person created

---
+++ 04. OOP/08.1. Reflection and Attributes - Exercise/ValidationAttributes/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace ValidationAttributes.Models
+{
+    public class Person
+    {
+        private const int MinAge = 12;
+        private const int MaxAge = 90;
+
+        public Person(string fullName, int age)
+        {
+            FullName = fullName;
+            Age = age;
+        }
+
+        public string FullName { get; private set; }
+        public int Age { get; private set; }
+
+    }
+}
---
+++ 04. OOP/08.1. Reflection and Attributes - Exercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,6 @@
+﻿namespace ValidationAttributes
+{
+    internal class Validator
+    {
+    }
+}
