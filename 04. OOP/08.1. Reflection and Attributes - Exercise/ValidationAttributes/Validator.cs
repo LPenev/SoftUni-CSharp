b8e17fd2127f567b9fc6adb940b94a629aaf6d38
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-21 22:44:42 +0100

    Validator created

--- 04. OOP/08.1. Reflection and Attributes - Exercise/ValidationAttributes/Validator.cs
+++ 04. OOP/08.1. Reflection and Attributes - Exercise/ValidationAttributes/Validator.cs
@@ -1,6 +1,13 @@
-﻿namespace ValidationAttributes
+﻿using System;
+using ValidationAttributes.Models;
+
+namespace ValidationAttributes
 {
     internal class Validator
     {
+        internal static bool IsValid(Person person)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
