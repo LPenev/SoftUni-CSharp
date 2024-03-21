5e8c11805fbb392923b9b7884ae2c05ff6e83cb3
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-21 22:11:52 +0100

    Attributes created

---
+++ 04. OOP/08.1. Reflection and Attributes - Exercise/ValidationAttributes/Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRequiredAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj)
+        {
+            bool result = obj is not null;
+           return result;
+        }
+    }
+}
