aa328f6079f27218105ad21f3f569e6e200cfa16
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-22 23:17:43 +0100

    Axe Test cmleted

--- 04. OOP/09. Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
+++ 04. OOP/09. Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
@@ -5,10 +5,20 @@ namespace Skeleton.Tests
     [TestFixture]
     public class AxeTests
     {
+        private const int Attack = 10;
+        private const int DurabilityPoints = 10;
+        private const int Health = 10;
+        private const int Experience = 10;
+
         [Test]
         public void Test1()
         {
+            Axe axe = new Axe(Attack, DurabilityPoints);
+            Dummy dummy = new Dummy(Health, Experience);
 
+            axe.Attack(dummy);
+
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe drability doesn't change after attack.");
         }
     }
 }
