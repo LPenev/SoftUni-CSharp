d068363d1821eade9342793cfee69a622e3713ce
LPenev <lppenev@gmail.com>
2024-03-23 16:18:54 +0200

    05. Create Attribute Completed

---
+++ 04. OOP/07. Reflection and Attributes - Lab/05. Create Attribute/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace AuthorProblem
+{
+    [Author("Victor")]
+    public class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+
+        }
+
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+        public class AuthorAttribute : Attribute
+        {
+            public AuthorAttribute(string name)
+            {
+                this.Name = name;
+            }
+
+            public string Name { get; set; }
+        }
+    }
+
+}
