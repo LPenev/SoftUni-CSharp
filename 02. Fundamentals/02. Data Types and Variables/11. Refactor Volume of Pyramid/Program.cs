da1fb199331b4844602468a4a6c1212bcd28758b
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-25 21:49:29 +0100

    Task 11 completed

---
+++ 02. Fundamentals/02. Data Types and Variables/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,15 @@
+﻿
+double length, width, height = 0;
+// read length
+Console.Write("Length: ");
+length = double.Parse(Console.ReadLine());
+// read width
+Console.Write("Width: ");
+width = double.Parse(Console.ReadLine());
+// read height
+Console.Write("Height: ");
+height = double.Parse(Console.ReadLine());
+// calculation
+double volume = (length * width * height) / 3;
+// print result
+Console.WriteLine($"Pyramid Volume: {volume:f2}");
