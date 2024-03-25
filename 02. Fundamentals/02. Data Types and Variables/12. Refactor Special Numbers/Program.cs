5aa33a84cf9268f137913e0b3672304200cf38c2
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-25 21:52:45 +0100

    Tasks are completed

---
+++ 02. Fundamentals/02. Data Types and Variables/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿int inputDigit = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= inputDigit; i++)
+{
+    bool isSpecialNum = false;
+    int sumOfDigits = 0;
+    int current = i;
+    while (current > 0)
+    {
+        sumOfDigits += current % 10;
+        current = current / 10;
+    }
+    isSpecialNum = sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11;
+    Console.WriteLine("{0} -> {1}", i, isSpecialNum);
+}
