19a46a37e144a1eeae93469627fd086c1f89af25
LPenev <lppenev@gmail.com>
2024-03-25 14:45:50 +0200

    05. Create Attribute2 Completed

---
+++ 04. OOP/07. Reflection and Attributes - Lab/05. Create Attribute2/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace AuthorProblem
+{
+    [Author("Victor")]
+    internal class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
