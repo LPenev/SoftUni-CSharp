675f652767fda7fb120ab1b57b73d96747eacf15
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-27 18:52:19 +0100

    Task 6 completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = int.Parse(Console.ReadLine());
+int begin = 97;
+number += begin;
+
+for (int i = begin; i < number; i++)
+{
+    for (int j = begin; j < number; j++)
+    {
+        for (int k = begin; k < number; k++)
+        {
+            Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+        }
+    }
+}
