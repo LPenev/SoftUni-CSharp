93da224900efaf5a8d1f0125d4dfce54308fb6bb
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-27 21:32:43 +0100

    Task 7 completed

---
+++ 02. Fundamentals/02.1. Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,18 @@
+﻿byte tankCapacity = 255;
+byte numberOfLines = byte.Parse(Console.ReadLine());
+
+ushort tankWater = 0;
+for (int i = 0; i < numberOfLines; i++)
+{
+    ushort waterPourIn = ushort.Parse(Console.ReadLine());
+    if(tankWater + waterPourIn <= tankCapacity)
+    {
+        tankWater += waterPourIn;
+    }
+    else
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }    
+}
+// print result
+Console.WriteLine(tankWater);
