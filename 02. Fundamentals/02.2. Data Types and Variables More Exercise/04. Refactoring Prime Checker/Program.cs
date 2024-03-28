76ae558f9d787d783e7f3d4e9cddc0c68e21e38e
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-28 19:09:42 +0100

    Task 04 completed

---
+++ 02. Fundamentals/02.2. Data Types and Variables More Exercise/04. Refactoring Prime Checker/Program.cs
@@ -0,0 +1,16 @@
+﻿int input = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= input; i++)
+{
+    bool isPrime = true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    // print result
+    Console.WriteLine("{0} -> {1}", i, isPrime.ToString().ToLower());
+}
