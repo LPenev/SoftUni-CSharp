a447d729f03f6752128abb3fffb81527f91c83eb
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-28 19:11:01 +0100

    Task 05 completed

---
+++ 02. Fundamentals/02.2. Data Types and Variables More Exercise/05. Decrypting Messages/Program.cs
@@ -0,0 +1,8 @@
+﻿int key = int.Parse(Console.ReadLine());
+int interval = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < interval; i++)
+{
+    char input = char.Parse(Console.ReadLine());
+    Console.Write((char)(input+key));
+}
