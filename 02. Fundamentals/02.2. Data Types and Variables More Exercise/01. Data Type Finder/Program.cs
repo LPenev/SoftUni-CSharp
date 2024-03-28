62f8cf27386715cabe8965b807c3abda26282b42
LPenev <150725482+LPenev@users.noreply.github.com>
2024-03-28 19:05:30 +0100

    Taks 01 completed

---
+++ 02. Fundamentals/02.2. Data Types and Variables More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+string input = Console.ReadLine();
+string result = default;
+
+while (input != "END")
+{
+    if (int.TryParse(input, out int result1))
+    {
+        result = $"{input} is integer type";
+    }
+    else if (float.TryParse(input, out float result2))
+    {
+        result = $"{input} is floating point type";
+    }
+    else if (char.TryParse(input, out char result3))
+    {
+        result = $"{input} is character type";
+    }
+    else if (bool.TryParse(input, out bool result4))
+    {
+        result = $"{input} is boolean type";
+    }
+    else
+    {
+        result = $"{input} is string type";
+    }
+    // print result
+    Console.WriteLine(result);
+    // read from console
+    input = Console.ReadLine();
+}
