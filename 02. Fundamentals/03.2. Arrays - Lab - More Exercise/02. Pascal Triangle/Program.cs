31450a473b3be5228e9d38e9431a902326086a30
LPenev <lppenev@gmail.com>
2024-03-29 16:41:00 +0100

    Task 02

---
+++ 02. Fundamentals/03.2. Arrays - Lab - More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿int triangleLines = int.Parse(Console.ReadLine());
+int current = 0;
+
+for (int row = 0; row < triangleLines; row++)
+{
+	for (int column = 0; column <= row; column++)
+	{
+        if(column == 0)
+        {
+            current = 1;
+        }
+        else
+        {
+            current = current * (row - column + 1) / column;
+            Console.WriteLine($"{current} = {current} * ({row} - {column} + 1) / {column}");
+        }
+        Console.Write(current + " ");
+    }
+    Console.WriteLine("new");
+}
