1ea75f1473f677fd19f08c4fc696c5b56d3d34fc
LPenev <lppenev@gmail.com>
2024-03-30 16:31:45 +0100

    Task 03 completed

---
+++ 02. Fundamentals/03.1. Arrays - Lab - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberLines = int.Parse(Console.ReadLine());
+
+string[] resultA = new string[numberLines];
+string[] resultB = new string[numberLines];
+
+for (int i = 0; i < numberLines; i++)
+{
+    var input = Console.ReadLine().Split();
+    if(i%2 == 0)
+    {
+        resultA[i]= input[0];
+        resultB[i] = input[1];
+    }
+    else
+    {
+        resultA[i] = input[1];
+        resultB[i] = input[0];
+    }
+}
+
+Console.WriteLine(string.Join(" ", resultA));
+Console.WriteLine(string.Join(" ", resultB));
