3f93073d136ee5c02084935d9c149aa08b725cf3
LPenev <lppenev@gmail.com>
2024-03-30 16:33:32 +0100

    Task 05 completed

---
+++ 02. Fundamentals/03.1. Arrays - Lab - Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // read from console
+        int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        for (int i = 0; i < inputNumbers.Length; i++)
+        {
+           bool isTop = true;
+
+            for (int j = i +1 ;inputNumbers.Length > j; j++)
+            {
+                if (inputNumbers[i] <= inputNumbers[j])
+                {
+                    isTop = false;
+                    break;
+                }
+            }
+            if (isTop) 
+            {
+                Console.Write(inputNumbers[i]);
+                
+                if(i < inputNumbers.Length - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+        }
+    }
+}
