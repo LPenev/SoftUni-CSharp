1c5e4d57c828425787287ee95dd25773abd7fb30
LPenev <lppenev@gmail.com>
2024-03-30 16:32:28 +0100

    Task 04 completed

---
+++ 02. Fundamentals/03.1. Arrays - Lab - Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,17 @@
+﻿// read from console
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int countsOfRotations = int.Parse(Console.ReadLine());
+
+// calculations
+for (int i = 0; i < countsOfRotations; i++)
+{
+    int firstElement = numbers[0];
+	for (int j = 0; j < numbers.Length -1; j++)
+	{
+		numbers[j] = numbers[j + 1];
+	}
+	numbers[numbers.Length-1] = firstElement;
+}
+
+// print result
+Console.WriteLine(string.Join(" ", numbers));
