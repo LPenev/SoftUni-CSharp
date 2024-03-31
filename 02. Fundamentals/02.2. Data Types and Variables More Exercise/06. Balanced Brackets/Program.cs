95eeb475e618a2d1fbfb11e66cb3cca019f2467a
LPenev <lppenev@gmail.com>
2024-03-31 23:24:57 +0200

    Tasks completed

---
+++ 02. Fundamentals/02.2. Data Types and Variables More Exercise/06. Balanced Brackets/Program.cs
@@ -0,0 +1,44 @@
+﻿
+int numberOfLines = int.Parse(Console.ReadLine());
+bool isBalanced = true;
+bool isOpen = false;
+
+for (int i = 0; i < numberOfLines; i++)
+{
+    string input = Console.ReadLine();
+    if(input.Length > 1)
+    {
+        continue;
+    }
+    else if (input[0] == '(' && isOpen)
+    {
+        isBalanced = false;
+        break;
+    }
+    else if (input[0] == '(')
+    {
+        isOpen = true;
+        isBalanced = false;
+    }
+    else if (input[0] == ')' && isOpen)
+    {
+        isOpen = false;
+        isBalanced = true;
+    }
+    else if (input[0] == ')')
+    {
+        isBalanced=false;
+        break;
+    }
+}
+
+// print result
+if(isBalanced)
+{
+    Console.WriteLine("BALANCED");
+}
+else
+{
+    Console.WriteLine("UNBALANCED");
+}
+
