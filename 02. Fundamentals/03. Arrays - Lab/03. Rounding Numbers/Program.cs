88e8a25fde9b4d5aff2e62c6ed5336f2c1000603
LPenev <lppenev@gmail.com>
2024-03-31 23:31:52 +0200

    Taks 03 completed

---
+++ 02. Fundamentals/03. Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿string input = Console.ReadLine();
+
+double[] numbers = input.Split().Select(double.Parse).ToArray();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+
+    double printNumber = Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+    
+    if(printNumber == 0) { printNumber = 0; }
+    
+    Console.WriteLine($"{numbers[i]} => {printNumber}");
+}
