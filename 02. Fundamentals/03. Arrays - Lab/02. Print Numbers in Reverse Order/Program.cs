e388e1fe0d0c242dfc0a5716a22c6730f3404c61
LPenev <lppenev@gmail.com>
2024-03-31 23:30:41 +0200

    Taks 02 completed

---
+++ 02. Fundamentals/03. Arrays - Lab/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,15 @@
+﻿int lines = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[lines];
+
+for(int i = 0; i < lines; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+for(int j = numbers.Length - 1; j >0; j--)
+{
+    Console.Write(numbers[j] + " ");
+}
+
+Console.Write(numbers[0]);
