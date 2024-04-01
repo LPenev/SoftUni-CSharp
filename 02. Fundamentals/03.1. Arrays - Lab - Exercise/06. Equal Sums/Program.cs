9d75865b40ee0c5f2282ff21f59692dc3c98202a
LPenev <lppenev@gmail.com>
2024-04-01 17:49:57 +0200

    Task 06 completed

---
+++ 02. Fundamentals/03.1. Arrays - Lab - Exercise/06. Equal Sums/Program.cs
@@ -0,0 +1,47 @@
+﻿string input = Console.ReadLine();
+if (input == "")
+{
+    Console.WriteLine("No such index exists");
+}
+else
+{
+    int[] numbers = input.Split().Select(int.Parse).ToArray();
+
+    int sumLeft = 0;
+    int sumRight = 0;
+    int i = 0;
+    bool isEqual = false;
+
+    for (i = 0; i < numbers.Length; i++)
+    {
+        sumLeft = 0;
+        sumRight = 0;
+
+        for (int j = i - 1; j >= 0; j--)
+        {
+            sumLeft += numbers[j];
+        }
+
+        for (int k = i + 1; k < numbers.Length; k++)
+        {
+            sumRight += numbers[k];
+        }
+
+        if(sumLeft == sumRight)
+        {
+            isEqual = true;
+            break;
+        }
+
+    }
+
+    // print result
+    if (isEqual)
+    {
+        Console.WriteLine(i);
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
