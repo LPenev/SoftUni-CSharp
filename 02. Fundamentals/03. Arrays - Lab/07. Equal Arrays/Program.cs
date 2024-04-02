582da07a0f39f1fe3abe0874c07dab1fa3b3a0f8
LPenev <lppenev@gmail.com>
2024-04-02 21:45:01 +0200

    Task 07 completed

---
+++ 02. Fundamentals/03. Arrays - Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] numbersA = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] numbersB = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int sumNumbers = default;
+bool isEqual = true;
+
+for(int i = 0; i < numbersA.Length; i++)
+{
+    if (numbersA[i] == numbersB[i])
+    {
+        sumNumbers += numbersA[i];
+    }
+    else
+    {
+        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+        isEqual = false;
+        break;
+    }
+
+}
+
+if (isEqual)
+{
+    Console.WriteLine($"Arrays are identical. Sum: {sumNumbers}");
+}
