0fae002eddeaa07884e80666a94b94a269f7392f
LPenev <lppenev@gmail.com>
2024-04-03 22:04:38 +0200

    Task 04 comleted

---
+++ 02. Fundamentals/04. Metohds - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            printingTriangle(input);
+        }
+
+        static void printingTriangle(int maxNumber)
+        {
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = maxNumber; i > 0; i--)
+            {
+                for(int j = 1; j < i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
