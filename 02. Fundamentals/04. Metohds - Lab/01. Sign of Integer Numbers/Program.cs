627ba24e9a726c4d081f5d78b1557c3e49c90711
LPenev <lppenev@gmail.com>
2024-04-03 22:01:22 +0200

    Task 01

---
+++ 02. Fundamentals/04. Metohds - Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            checkNumbers(input);
+        }
+
+        static void checkNumbers(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
