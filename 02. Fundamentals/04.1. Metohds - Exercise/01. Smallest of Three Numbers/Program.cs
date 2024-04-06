df7b3cb9cfeed849b3a36fc538ea504d2514ff96
LPenev <lppenev@gmail.com>
2024-04-06 10:50:00 +0200

    Task 01 completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _101._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputedNummerA = int.Parse(Console.ReadLine());
+            int inputedNummerB = int.Parse(Console.ReadLine());
+            int inputedNummerC = int.Parse(Console.ReadLine());
+            int result = GetSmalltestNumber(inputedNummerA, inputedNummerB, inputedNummerC);
+            Console.WriteLine(result);
+        }
+
+        static int GetSmalltestNumber(int inputedNumberA, int inputedNumberB, int inputedNumberC)
+        {
+            int result = 0;
+            if (inputedNumberA <= inputedNumberB && inputedNumberA <= inputedNumberC) 
+            { 
+                result = inputedNumberA;
+            }
+            else if (inputedNumberB <= inputedNumberA && inputedNumberB <= inputedNumberC)
+            {
+                result = inputedNumberB;
+            }
+            else
+            {
+                result = inputedNumberC;
+            }
+            return result;
+        }
+    }
+}
