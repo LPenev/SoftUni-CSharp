91f8a4b29fb6a58bbfd3a11f7a45f8598d05b7f5
LPenev <lppenev@gmail.com>
2024-04-06 14:38:26 +0300

    Part 1 Completed

---
+++ 04. OOP/10. Exams/C# OOP Exam Regular - 06 April 2024/01. InfluencerManagerApp-Skeleton/InfluencerManagerApp/Models/BloggerInfluencer.cs
@@ -0,0 +1,15 @@
+﻿namespace InfluencerManagerApp.Models
+{
+    public class BloggerInfluencer : Influencer
+    {
+        private const double EngagementRate = 2.0;
+        private const double Factor = 0.2;
+
+        public BloggerInfluencer(string username, int followers) : base(username, followers, EngagementRate)
+        {
+        }
+
+        public override int CalculateCampaignPrice()
+            => Convert.ToInt32(Math.Floor(Followers * EngagementRate * Factor));
+    }
+}
---
+++ 04. OOP/10. Exams/C# OOP Exam Regular - 06 April 2024/01. InfluencerManagerApp-Skeleton/InfluencerManagerApp/Models/Campaign.cs
@@ -0,0 +1,51 @@
+﻿using InfluencerManagerApp.Models.Contracts;
+using InfluencerManagerApp.Utilities.Messages;
+
+namespace InfluencerManagerApp.Models
+{
+    public abstract class Campaign : ICampaign
+    {
+        private string brand;
+        List<string> contributors;
+
+        protected Campaign(string brand, double budget)
+        {
+            this.brand = brand;
+            Budget = budget;
+            contributors = new List<string>();
+        }
+
+        public string Brand
+        {
+            get => brand;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.BrandIsrequired);
+                }
+
+                brand = value;
+            }
+        }
+
+        public double Budget { get; private set; }
+
+        public IReadOnlyCollection<string> Contributors => contributors.AsReadOnly();
+
+        public void Engage(IInfluencer influencer)
+        {
+            contributors.Add(influencer.Username);
+        }
+
+        public void Gain(double amount)
+        {
+            Budget += amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType()} - Brand: {Brand}, Budget: {Budget}, Contributors: {contributors.Count}";
+        }
+    }
+}
