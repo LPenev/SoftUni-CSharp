8b43e45608a077ba668230254f263ccf4edb00cf
LPenev <lppenev@gmail.com>
2024-04-07 23:16:32 +0200

    Task 06  completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1106._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintsCharacterAtMiddle(input);
+        }
+
+        static void PrintsCharacterAtMiddle(string inputedString)
+        {
+            if (inputedString.Length%2 == 0)
+            {
+                int startStr = inputedString.Length / 2 - 1;
+                Console.WriteLine($"{inputedString[startStr]}{inputedString[startStr + 1]}");
+            } else
+            {
+                int startStr = inputedString.Length / 2;
+                Console.WriteLine(inputedString[startStr]);
+            }
+        }
+    }
+}
