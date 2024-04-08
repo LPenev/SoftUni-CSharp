cecf9190cdceabe4cc8b39617025530bcc5d5b8c
LPenev <lppenev@gmail.com>
2024-04-08 21:57:12 +0200

    Task 09 completed

---
+++ 02. Fundamentals/04.1. Metohds - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+
+namespace _1109._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while(input != "END")
+            {
+                Console.WriteLine(checkPalindrome(input));
+
+                input = Console.ReadLine();
+            }
+        }
+
+        static bool checkPalindrome(string input)
+        {
+            bool result = false;
+            if (input[0] == input[input.Length-1])
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
