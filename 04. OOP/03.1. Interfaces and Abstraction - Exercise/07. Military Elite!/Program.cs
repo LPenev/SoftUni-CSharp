ccb745f4b25df814c427f8910dcfc23b045bdaad
LPenev <lppenev@gmail.com>
2024-04-09 13:47:48 +0300

    07. Military Elite! Completed

---
+++ 04. OOP/03.1. Interfaces and Abstraction - Exercise/07. Military Elite!/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07._Military_Elite_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
