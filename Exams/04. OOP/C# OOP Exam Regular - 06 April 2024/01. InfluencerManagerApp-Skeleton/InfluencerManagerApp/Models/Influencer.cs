e3070a444fad968fdb30d2f48238051f118bec0b
LPenev <lppenev@gmail.com>
2024-04-15 20:47:45 +0200

    Task completed

---
+++ Exams/04. OOP/C# OOP Exam Regular - 06 April 2024/01. InfluencerManagerApp-Skeleton/InfluencerManagerApp/Models/Influencer.cs
@@ -0,0 +1,69 @@
+﻿using InfluencerManagerApp.Models.Contracts;
+using InfluencerManagerApp.Utilities.Messages;
+
+namespace InfluencerManagerApp.Models
+{
+    public abstract class Influencer : IInfluencer
+    {
+        private string username;
+        private int followers;
+        private double engagementRate;
+        private double income;
+        private List<string> participations;
+
+        public Influencer(string username, int followers, double engagementRate)
+        {
+            Username = username;
+            Followers = followers;
+            EngagementRate = engagementRate;
+            income = 0;
+            participations = new List<string>();
+        }
+
+        public string Username 
+        {
+            get => this.username;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.UsernameIsRequired);
+                }
+
+                this.username = value;
+            }
+        }
+
+        public int Followers 
+        {
+            get => followers;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.FollowersCountNegative);
+                }
+
+                followers = value;
+            }
+        }
+
+        public double EngagementRate 
+        {
+            get => engagementRate;
+            private set => engagementRate = value;
+        }
+
+        public double Income => income;
+
+        public IReadOnlyCollection<string> Participations => participations.AsReadOnly();
+
+        public abstract int CalculateCampaignPrice();
+
+        public void EarnFee(double amount)=> income += amount;
+        
+        public void EndParticipation(string brand) => participations.Remove(brand);
+
+        public void EnrollCampaign(string brand) => participations.Add(brand);
+    }
+}
---
+++ Exams/04. OOP/C# OOP Exam Regular - 06 April 2024/01. InfluencerManagerApp-Skeleton/InfluencerManagerApp/Repositories/InfluencerRepository.cs
@@ -0,0 +1,23 @@
+﻿using InfluencerManagerApp.Models.Contracts;
+using InfluencerManagerApp.Repositories.Contracts;
+
+namespace InfluencerManagerApp.Repositories
+{
+    public class InfluencerRepository : IRepository<IInfluencer>
+    {
+        private List<IInfluencer> models;
+
+        public InfluencerRepository()
+        {
+            models = new List<IInfluencer>();
+        }
+
+        public IReadOnlyCollection<IInfluencer> Models => models.AsReadOnly();
+
+        public void AddModel(IInfluencer model) => models.Add(model);
+        
+        public IInfluencer FindByName(string name) => models.FirstOrDefault(x => x.Username == name);
+
+        public bool RemoveModel(IInfluencer model) => models.Remove(model);
+    }
+}
