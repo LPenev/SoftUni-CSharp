76641067d7ade9b9f713ef001aafcaeef3c4ae0b
LPenev <lppenev@gmail.com>
2024-04-16 10:37:17 +0200

    Task completed

---
+++ Exams/04. OOP/C# OOP Exam Regular - 06 April 2024/02. InfluencerManagerApp-BusinessLogic/InfluencerManagerApp/Repositories/CampaignRepository.cs
@@ -0,0 +1,23 @@
+﻿using InfluencerManagerApp.Models.Contracts;
+using InfluencerManagerApp.Repositories.Contracts;
+
+namespace InfluencerManagerApp.Repositories
+{
+    public class CampaignRepository : IRepository<ICampaign>
+    {
+        private List<ICampaign> models;
+
+        public CampaignRepository()
+        {
+            models = new List<ICampaign>();
+        }
+
+        public IReadOnlyCollection<ICampaign> Models => models.AsReadOnly();
+
+        public void AddModel(ICampaign model) => models.Add(model);
+
+        public ICampaign FindByName(string name) => models.FirstOrDefault(x => x.Brand == name);
+
+        public bool RemoveModel(ICampaign model) => models.Remove(model);
+    }
+}
