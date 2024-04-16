c17dbe3276ef63ee543b655448073dea652a3d02
LPenev <lppenev@gmail.com>
2024-04-16 10:39:55 +0200

    Task completed

---
+++ Exams/04. OOP/C# OOP Exam Regular - 06 April 2024/C# OOP Retake Exam – 16 April 2024/01. TheContentDepartment-Skeleton/TheContentDepartment/Models/Workshop.cs
@@ -0,0 +1,10 @@
+﻿namespace TheContentDepartment.Models
+{
+    public class Workshop : Resource
+    {
+        private const int Priority = 2;
+        public Workshop(string name, string creator) : base(name, creator, Priority)
+        {
+        }
+    }
+}
---
+++ Exams/04. OOP/C# OOP Exam Regular - 06 April 2024/C# OOP Retake Exam – 16 April 2024/01. TheContentDepartment-Skeleton/TheContentDepartment/Repositories/ResourceRepository.cs
@@ -0,0 +1,27 @@
+﻿using TheContentDepartment.Models.Contracts;
+using TheContentDepartment.Repositories.Contracts;
+
+namespace TheContentDepartment.Repositories
+{
+    public class ResourceRepository : IRepository<IResource>
+    {
+        private List<IResource> models;
+
+        public ResourceRepository()
+        {
+            models = new List<IResource>();
+        }
+
+        public IReadOnlyCollection<IResource> Models => models.AsReadOnly();
+
+        public void Add(IResource model)
+        {
+            models.Add(model);
+        }
+
+        public IResource TakeOne(string modelName)
+        {
+            return models.FirstOrDefault(x=>x.Name == modelName);
+        }
+    }
+}
