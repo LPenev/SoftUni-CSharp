ded1f1c06fa8a4b87f08687dfc872ee8bdd928c7
LPenev <lppenev@gmail.com>
2024-04-17 22:26:20 +0200

    task 02 created

---
+++ Exams/04. OOP/C# OOP Retake Exam - 16 April 2024/02. TheContentDepartment-Skeleton-BusinessLogic/TheContentDepartment/Models/Workshop.cs
@@ -0,0 +1,11 @@
+﻿namespace TheContentDepartment.Models
+{
+    public class Workshop : Resource
+    {
+        private const int Priority = 2;
+
+        public Workshop(string name, string creator) : base(name, creator, Priority)
+        {
+        }
+    }
+}
