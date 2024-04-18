a53af3b15e03b190f0ed8c68e09d6bdf35403057
LPenev <lppenev@gmail.com>
2024-04-18 16:41:32 +0200

    task 04 completed

---
+++ 02. Fundamentals/05. List - Lab/04. List of Products/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfLines = int.Parse(Console.ReadLine());
+
+            List<string> listOfProducts = new List<string>();
+
+            for(int i = 0; i < numbersOfLines; i++)
+            {
+                listOfProducts.Add(Console.ReadLine());
+            }
+
+            OrderedProductsByName(listOfProducts);
+
+            PrintProductsWithNumbers(listOfProducts);
+        }
+
+        static void PrintProductsWithNumbers(List<string> listOfProducts)
+        {
+            for(int i = 0;i < listOfProducts.Count;i++)
+            {
+                Console.WriteLine("{0}.{1}", i + 1, listOfProducts[i]);
+            }
+        }
+
+        static void OrderedProductsByName(List<string> listOfProducts)
+        {
+            listOfProducts.Sort();
+        }
+    }
+}
