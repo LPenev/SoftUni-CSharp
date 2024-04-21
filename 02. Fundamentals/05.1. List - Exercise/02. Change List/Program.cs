5c789b7c936e9e233b5e7e69721af557c1936548
LPenev <lppenev@gmail.com>
2024-04-21 11:56:00 +0200

    Task 02 Change List completed

---
+++ 02. Fundamentals/05.1. List - Exercise/02. Change List/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _102._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfIntegers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string[] command = Console.ReadLine().Split().ToArray();
+            while (command[0] != "end")
+            {
+                switch (command[0])
+                {
+                    case "Delete":
+                        int toDelete = int.Parse(command[1]);
+                        listOfIntegers.RemoveAll(x => x == toDelete);
+                        break;
+
+                    case "Insert":
+                        listOfIntegers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                        break;
+                }
+
+                command = Console.ReadLine().Split().ToArray();
+            }
+            Console.WriteLine(string.Join(" ", listOfIntegers));
+        }
+    }
+}
