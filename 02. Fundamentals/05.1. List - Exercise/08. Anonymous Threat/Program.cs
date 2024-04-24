83d054ee8167866a0e8bf16ae1a995fb0977f126
LPenev <lppenev@gmail.com>
2024-04-24 14:37:43 +0200

    Task 08 Anonymous Threat completed

---
+++ 02. Fundamentals/05.1. List - Exercise/08. Anonymous Threat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _108._Anonymous_Threat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
