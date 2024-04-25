a510ac853485cb25eb894b6892ed2aaa8bd7b049
LPenev <lppenev@gmail.com>
2024-04-25 15:38:36 +0200

    Task 09 Pokemon completed

---
+++ 02. Fundamentals/05.1. List - Exercise/09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _109._Pokemon_Don_t_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
