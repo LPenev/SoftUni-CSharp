b1d4298e2ddc018d6f9cdc01523bc9a26a39e5f5
LPenev <lppenev@gmail.com>
2024-04-26 14:42:05 +0300

    03. Clothing Magazine Completed

---
+++ 03. Advanced/Exams/CSharp Advanced Retake Exam - 12 April 2023/03. Clothing Magazine/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClothesMagazine
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Initialize the repository (Magazine)
+            Magazine magazine = new Magazine("Zara", 20);
+            //Initialize entity (Cloth)
+            Cloth cloth1 = new Cloth("red", 36, "dress");
+            //Print Cloth
+            Console.WriteLine(cloth1);
+            //Add Cloth
+            magazine.AddCloth(cloth1);
+            //Remove Cloth
+            Console.WriteLine(magazine.RemoveCloth("black"));
+            Cloth cloth2 = new Cloth("brown", 34, "t-shirt");
+            Cloth cloth3 = new Cloth("blue", 32, "jeans");
+            //Add Cloth
+            magazine.AddCloth(cloth2);
+            magazine.AddCloth(cloth3);
+            //Get smallest cloth
+            Cloth smallestCloth = magazine.GetSmallestCloth();
+            Console.WriteLine(smallestCloth); 
+            
+            Cloth getCloth = magazine.GetCloth("brown");
+            Console.WriteLine(getCloth);
+            Console.WriteLine(magazine.Report());
+
+        }
+    }
+}
