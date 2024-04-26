c61b5e0c2c3d31707e472fa27aca3ec4afa11334
LPenev <lppenev@gmail.com>
2024-04-26 17:43:48 +0200

    Task 02 completed

---
+++ 02. Fundamentals/06. Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            FactorialPrint(input);
+        }
+
+        static void FactorialPrint(int count)
+        {
+            BigInteger factirial = 1;
+            for (int i = 2; i <= count; i++)
+            {
+                factirial *= i;
+            }
+            Console.WriteLine(factirial);
+        }
+    }
+}
