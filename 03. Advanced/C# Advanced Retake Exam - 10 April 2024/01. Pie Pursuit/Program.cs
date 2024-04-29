4e267b069819e6ebd376833f314fd8bb285cb8a8
LPenev <lppenev@gmail.com>
2024-04-29 14:08:01 +0300

    01. Pie Pursuit Completed

---
+++ 03. Advanced/C# Advanced Retake Exam - 10 April 2024/01. Pie Pursuit/Program.cs
@@ -0,0 +1,57 @@
+﻿
+int[] contestantsArray = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[] piesOfPiesArray = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> contestants = new Queue<int>(contestantsArray);
+Stack<int> piesOfPies = new Stack<int>(piesOfPiesArray);
+
+while (contestants.Count > 0 && piesOfPies.Count > 0)
+{
+    int currentContestant = contestants.Dequeue();
+    int currentPie = 0;
+    if (piesOfPies.Count > 0)
+    {
+        currentPie = piesOfPies.Pop();
+    }
+
+    if (currentContestant > currentPie)
+    {
+        currentContestant -= currentPie;
+
+        if (currentContestant > 0)
+        {
+            contestants.Enqueue(currentContestant);
+        }
+    }
+    else
+    {
+        currentPie -= currentContestant;
+        if (currentPie > 0)
+        {
+            piesOfPies.Push(currentPie);
+        }
+    }
+
+}
+
+if (contestants.Count > 0)
+{
+    Console.WriteLine("We will have to wait for more pies to be baked!");
+    Console.WriteLine($"Contestants left: {String.Join(", ", contestants)}");
+}
+else if (piesOfPies.Count > 0)
+{
+    Console.WriteLine("Our contestants need to rest!");
+    Console.WriteLine($"Pies left: {String.Join(", ", piesOfPies)}");
+}
+else
+{
+    Console.WriteLine("We have a champion!");
+}
