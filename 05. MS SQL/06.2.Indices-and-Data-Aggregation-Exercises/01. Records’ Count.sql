6d4a2d6204130671bf62a3e0c20689c1018eeec2
LPenev <lppenev@gmail.com>
2024-05-31 15:38:01 +0300

    01. Records’ Count Completed

---
+++ 05. MS SQL/06.2.Indices-and-Data-Aggregation-Exercises/01. Records’ Count.sql
@@ -0,0 +1,6 @@
+USE Gringotts
+GO
+-- 01. Records’ Count
+-- send the total count of records from the one and only table to Mr. Bodrog. Make sure nothing gets lost.
+
+SELECT COUNT(*) FROM WizzardDeposits
