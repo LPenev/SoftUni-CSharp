f3b2be6ef97839ee9ab6e63310e6e44fd8d3e141
LPenev <lppenev@gmail.com>
2024-05-31 15:38:17 +0300

    02. Longest Magic Wand Completed

---
+++ 05. MS SQL/06.2.Indices-and-Data-Aggregation-Exercises/02. Longest Magic Wand.sql
@@ -0,0 +1,9 @@
+USE Gringotts
+GO
+
+-- 02. Longest Magic Wand
+-- Select the size of the longest magic wand. Rename the new column appropriately.
+
+SELECT 
+	MAX(MagicWandSize) AS LongestMagicWand 
+FROM WizzardDeposits
