0eef7e5701f965c4b93757d8ba92f3a63bdb3fa1
LPenev <lppenev@gmail.com>
2024-06-05 16:22:50 +0300

    13. Departments Total Salaries Completed

---
+++ 05. MS SQL/06.2.Indices-and-Data-Aggregation-Exercises/13. Departments Total Salaries.sql
@@ -0,0 +1,14 @@
+USE SoftUni
+GO
+
+-- 13. Departments Total Salaries
+-- Create a query that shows the total sum of salaries for each department. Order them by DepartmentID.
+-- Your query should return:	
+--	DepartmentID
+
+SELECT
+      DepartmentID
+	 ,SUM(Salary) AS TotalSalary
+FROM Employees
+GROUP BY DepartmentID
+ORDER BY DepartmentID
