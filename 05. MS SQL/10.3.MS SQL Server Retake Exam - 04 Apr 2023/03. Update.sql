5bf4c248d61e4506da4ea8f7f57a2bf87683f13f
LPenev <lppenev@gmail.com>
2024-08-02 15:34:01 +0300

    03. Update Completed

---
+++ 05. MS SQL/10.3.MS SQL Server Retake Exam - 04 Apr 2023/03. Update.sql
@@ -0,0 +1,13 @@
+USE Accounting
+GO
+
+-- 3. Update
+
+UPDATE Invoices 
+SET DueDate = '2023-04-01'
+WHERE YEAR(IssueDate) = 2022 AND MONTH(IssueDate) = 11
+
+UPDATE Clients
+SET AddressId = 3
+WHERE [Name] LIKE '%CO%'
+
