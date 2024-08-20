0afd9dfe004e478fad2a2d5e1e5f2ca3fe8d548d
LPenev <lppenev@gmail.com>
2024-08-20 15:39:51 +0300

    06. Sites with Their Location and Category Completed

---
+++ 05. MS SQL/10.5. MS SQL Server Retake Exam - 10 Aug 2022/06. Sites with Their Location and Category.sql
@@ -0,0 +1,8 @@
+USE NationalTouristSitesOgBulgaria
+GO
+
+SELECT s.Name AS 'Site', l.Name AS 'Location', s.Establishment, c.Name AS 'Category'
+FROM Sites AS s
+JOIN Locations AS l ON s.LocationId = l.Id
+JOIN Categories AS C ON c.Id = s.CategoryId
+ORDER BY c.Name DESC, l.Name, s.Name
