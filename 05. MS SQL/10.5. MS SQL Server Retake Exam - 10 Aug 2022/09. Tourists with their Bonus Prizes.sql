10f3e0049fe415428a4c8517c53e9e6de4fd3f69
LPenev <lppenev@gmail.com>
2024-08-22 15:49:21 +0300

    09. Tourists with their Bonus Prizes Completed

---
+++ 05. MS SQL/10.5. MS SQL Server Retake Exam - 10 Aug 2022/09. Tourists with their Bonus Prizes.sql
@@ -0,0 +1,9 @@
+USE NationalTouristSitesOgBulgaria
+GO 
+
+SELECT t.Name, t.Age, t.PhoneNumber, t.Nationality,
+	ISNULL(bp.Name, '(no bonus prize)') AS 'BonusPrize'
+FROM Tourists AS t
+LEFT JOIN TouristsBonusPrizes AS tbp ON tbp.TouristId = t.Id
+LEFT JOIN BonusPrizes AS bp ON tbp.BonusPrizeId = bp.Id
+ORDER BY t.Name
