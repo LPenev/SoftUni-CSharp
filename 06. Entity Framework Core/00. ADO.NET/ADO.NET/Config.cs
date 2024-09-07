6ee8400f0ffd206bb28ba51c4124c171a62e9437
LPenev <lppenev@gmail.com>
2024-09-07 15:35:26 +0200

    ADO.NET Demo Completed

---
+++ 06. Entity Framework Core/00. ADO.NET/ADO.NET/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.Security;
+
+namespace ADO.NET
+{
+    public class Config
+    {
+        public static string ConnectionString =
+        //    @"server=127.0.0.1;database=SoftUni;Initial Catalog=SoftUni;Integrated Security= false;Trusted_Connection=True";
+            @"server=127.0.0.1;database=SoftUni;uid=sa;password=Asdf22Asdf;Initial Catalog=SoftUni;Integrated Security= false";
+    }
+}
---
+++ 06. Entity Framework Core/00. ADO.NET/ADO.NET/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+
+namespace ADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+            sqlConnection.Open();
+
+            // first query
+            string countOfProjectsQuery = "SELECT COUNT(*) FROM Projects";
+            
+            SqlCommand countOfProjectsCmd = new SqlCommand(countOfProjectsQuery, sqlConnection );
+            int countOfProjects = (int)countOfProjectsCmd.ExecuteScalar();
+            Console.WriteLine($"Number of projects is: {countOfProjects}\n");
+
+            // second query
+            string infoProjectsQuery = @"SELECT ProjectID,[Name] FROM Projects";
+            SqlCommand infoProjectsCmd = new SqlCommand(infoProjectsQuery, sqlConnection);
+            using SqlDataReader infoProjectsReader = infoProjectsCmd.ExecuteReader();
+
+            Console.WriteLine("ID - Project Name\n");
+
+            while (infoProjectsReader.Read())
+            {
+                int projectId = (int)infoProjectsReader["ProjectID"];
+                string projectName = (string)infoProjectsReader["Name"];
+
+                Console.WriteLine($"{projectId} - {projectName}");
+            }
+
+            sqlConnection.Close();
+
+        }
+    }
+}
