a330c3110d7fa6ba295b6c4f9800a0eb43228dd2
LPenev <lppenev@gmail.com>
2024-09-09 14:37:32 +0200

    01. Initial Setup Completed

---
+++ 06. Entity Framework Core/00. ADO.NET-Exercise/01. Initial Setup/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace _01._Initial_Setup
+{
+    internal static class Config
+    {
+        public const string Database = "MinionsDB";
+        public const string Directory = "../../../data/";
+
+        public const string ConnectionString =
+            //    @"server=.;Integrated Security= True;Trusted_Connection=True";
+            @"server=127.0.0.1;uid=sa;password=Password;Integrated Security= false";
+    }
+}
---
+++ 06. Entity Framework Core/00. ADO.NET-Exercise/01. Initial Setup/Program.cs
@@ -0,0 +1,38 @@
+﻿using _01._Initial_Setup;
+using System.Data.SqlClient;
+
+using SqlConnection connection = new SqlConnection(Config.ConnectionString);
+connection.Open();
+
+CreateDb(connection);
+CreateDbTables(connection);
+InsertDataInDb(connection);
+
+connection.Close();
+
+
+static void InsertDataInDb(SqlConnection connection)
+{
+    string fullPath = Path.Combine(Config.Directory, "InsertDataMinionsDb.sql");
+    string query = File.ReadAllText(fullPath);
+    SqlCommand command = new SqlCommand(query, connection);
+    Console.WriteLine("Insert data into Db. Rows affected {0}.", command.ExecuteNonQuery());
+}
+
+static void CreateDbTables(SqlConnection connection)
+{
+    string fullPath = Path.Combine(Config.Directory, "CreateMinionsDbTables.sql");
+    string query = File.ReadAllText(fullPath);
+    SqlCommand command = new SqlCommand(query, connection);
+    Console.WriteLine("Created tables.");
+    command.ExecuteNonQuery();
+}
+
+static void CreateDb(SqlConnection connection)
+{
+    string query = @"CREATE DATABASE " + Config.Database;
+    SqlCommand command = new SqlCommand(query, connection);
+    command.ExecuteNonQuery();
+    Console.WriteLine($"Created database {Config.Database}.");
+}
+
