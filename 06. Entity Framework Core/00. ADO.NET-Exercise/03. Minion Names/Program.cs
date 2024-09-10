9600667a93ecbdeebe71b04e7a4c2d305df469c7
LPenev <lppenev@gmail.com>
2024-09-10 17:54:03 +0200

    03. Minion Names Completed

---
+++ 06. Entity Framework Core/00. ADO.NET-Exercise/03. Minion Names/Program.cs
@@ -0,0 +1,57 @@
+﻿using _03._Minion_Names;
+using System.Data.SqlClient;
+
+using SqlConnection connection = new SqlConnection(Config.ConnectionString);
+connection.Open();
+
+int VillianId = ReadVillianId();
+string villianName = GetVillianNameById(connection, VillianId);
+
+if (villianName != null)
+{
+    PrintVillianName(villianName);
+    PrintMinionsByVillianID(connection, VillianId);
+}
+
+connection.Close();
+
+static void PrintMinionsByVillianID(SqlConnection connection, int Id)
+{
+    string FindMinionsByVillianID =
+    @"SELECT M.Name, M.Age FROM MinionsVillains AS MV JOIN Minions AS M ON M.Id = MV.MinionId WHERE MV.VillainId = @Id ORDER BY M.Name";
+    SqlCommand cmd = new SqlCommand(FindMinionsByVillianID, connection);
+    cmd.Parameters.AddWithValue("Id", Id);
+    using SqlDataReader reader = cmd.ExecuteReader();
+
+    int Num = 0;
+    while (reader.Read())
+    {
+        Num++;
+        Console.WriteLine($"{Num}. {reader.GetString(0)} {reader.GetValue(1)}");
+    }
+}
+static void PrintVillianName(string name)
+{
+    Console.WriteLine($"Villain: {name}");
+}
+static string GetVillianNameById(SqlConnection connection, int Id)
+{
+    string findVillianByIdQuery = @"SELECT Name FROM Villains WHERE ID = @VillianId";
+    SqlCommand cmd = new SqlCommand(findVillianByIdQuery, connection);
+    cmd.Parameters.AddWithValue("VillianId", Id);
+    using SqlDataReader reader = cmd.ExecuteReader();
+
+    if (!reader.Read())
+    {
+        Console.WriteLine($"No villain with ID {Id} exists in the database.");
+        return null;
+    }
+
+    return reader.GetString(0).TrimEnd();
+}
+static int ReadVillianId() 
+{
+    Console.Write("Enter Villians Id -> ");
+    int VilliansId = int.Parse(Console.ReadLine());
+    return VilliansId;
+}
