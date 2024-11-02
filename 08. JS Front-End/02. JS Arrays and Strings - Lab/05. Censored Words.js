1330a9183f0bee9986a48f28510d5954258ac5dc
LPenev <150725482+LPenev@users.noreply.github.com>
2024-11-02 18:38:46 +0200

    05. Censored Words Completed

---
+++ 08. JS Front-End/02. JS Arrays and Strings - Lab/05. Censored Words.js
@@ -0,0 +1,8 @@
+function censoredWords(text, word){
+    while(text.includes(word)){
+        text = text.replace(word, '*'.repeat(word.length));
+    }
+    console.log(text);
+}
+
+censoredWords('Find the hidden word', 'hidden');
