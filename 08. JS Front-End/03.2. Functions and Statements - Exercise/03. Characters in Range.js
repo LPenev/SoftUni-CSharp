d6ddfdf09cacf6b977c2308ef6e7e59fb8b1e160
LPenev <150725482+LPenev@users.noreply.github.com>
2024-11-07 18:27:11 +0100

    03. Characters in Range Completed

---
+++ 08. JS Front-End/03.2. Functions and Statements - Exercise/03. Characters in Range.js
@@ -0,0 +1,27 @@
+function charactersInRange(startChar, endChar){
+    
+    let chars = printCharactersRange(startChar, endChar);
+    console.log(chars);
+
+    function printCharactersRange(a,b){
+        let startOfRange = getAsciiValue(a);
+        let endOfRange = getAsciiValue(b);
+        let result = '';
+        
+        for(let i = ++startOfRange; startOfRange < endOfRange; startOfRange++){
+            result = result.concat(' ' + asciiToChar(startOfRange));
+        }
+
+        return result;
+    }
+
+    function asciiToChar(value){
+        return String.fromCharCode(value);
+    }
+
+    function getAsciiValue(char){
+        return char.charCodeAt();
+    }
+}
+
+charactersInRange('a','d');
