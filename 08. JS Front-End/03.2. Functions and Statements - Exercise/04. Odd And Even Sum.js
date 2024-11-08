e22e7db16ad42815fb588b0c962215347070f72c
LPenev <150725482+LPenev@users.noreply.github.com>
2024-11-08 16:40:03 +0100

    04. Odd And Even Sum Updated

--- 08. JS Front-End/03.2. Functions and Statements - Exercise/04. Odd And Even Sum.js
+++ 08. JS Front-End/03.2. Functions and Statements - Exercise/04. Odd And Even Sum.js
@@ -1,15 +1,16 @@
 function oddAndEvenSum(number){
     let sumEven = 0;
     let sumOdd = 0;
+    let numbers = number.toString().split('').map(Number);
 
-    for(let current of number.toString()){
+    for(let current of numbers){
         isEven(current)?sumEven+=Number(current):sumOdd+=Number(current);
     }
 
     console.log(`Odd sum = ${sumOdd}, Even sum = ${sumEven}`);
 
     function isEven(a){
-        return (a%2 != 0)?false:true;
+        return (a%2 !== 0)?false:true;
     }
 }
 
