bada501f9a60b038021f96ef9f6b41e2121603b1
LPenev <150725482+LPenev@users.noreply.github.com>
2024-11-16 17:16:19 +0100

    04. Show More Complete

---
+++ 08. JS Front-End/05.1. DOM Introduction - Lab/04. Show More/show-more.js
@@ -0,0 +1,6 @@
+function showText() {
+    const buttonMore = document.getElementById("more");
+    const text = document.getElementById("text");
+    buttonMore.style.display = "none";
+    text.style.display = "inline";
+}
