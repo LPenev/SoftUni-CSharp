fbde1723dd989a9f02c185bd8cca8a2a9b524c7a
LPenev <150725482+LPenev@users.noreply.github.com>
2024-12-23 20:31:39 +0100

    Code first demo

---
+++ 06. Entity Framework Core/00. ORM Fundamentals/Database First model/Models/ProjectsContext.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_First_model.Models;
+
+public partial class ProjectsContext : DbContext
+{
+    public ProjectsContext()
+    {
+    }
+
+    public ProjectsContext(DbContextOptions<ProjectsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Project> Projects { get; set; }
+
+    public virtual DbSet<RmployeesProject> RmployeesProjects { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;User Id=demo;Password=Demo1234;Initial Catalog=Projects;Persist Security Info=True;Encrypt=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departme__3214EC07453734DF");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC07A8064A91");
+
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.MiddleName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("FK__Employees__Depar__4316F928");
+        });
+
+        modelBuilder.Entity<Project>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Projects__3214EC07910C2638");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<RmployeesProject>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.HasOne(d => d.Employee).WithMany()
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK__Rmployees__Emplo__45F365D3");
+
+            entity.HasOne(d => d.Project).WithMany()
+                .HasForeignKey(d => d.ProjectId)
+                .HasConstraintName("FK__Rmployees__Proje__44FF419A");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ 06. Entity Framework Core/00. ORM Fundamentals/Database First model/Models/RmployeesProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database_First_model.Models;
+
+public partial class RmployeesProject
+{
+    public int? ProjectId { get; set; }
+
+    public int? EmployeeId { get; set; }
+
+    public virtual Employee? Employee { get; set; }
+
+    public virtual Project? Project { get; set; }
+}
