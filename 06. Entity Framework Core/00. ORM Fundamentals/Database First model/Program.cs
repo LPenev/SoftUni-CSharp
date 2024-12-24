49e757a087e5197484eb74e1ffd8cdeb4692d80a
LPenev <150725482+LPenev@users.noreply.github.com>
2024-12-24 20:51:24 +0100

    Delete 06. Entity Framework Core/00. ORM Fundamentals directory

--- 06. Entity Framework Core/00. ORM Fundamentals/Database First model/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// dotnet ef dbcontext scaffold "Server=localhost;User Id=demo;Password=Demo1234;Initial Catalog=Projects;Persist Security Info=True;Encrypt=False;" Microsoft.EntityFrameworkCore.SqlServer -o Models
-using Database_First_model.Models;
-
-var db = new ProjectsContext();
-var numberOfEmployees = db.Employees.Count();
-Console.WriteLine("Number of current employees is: " + numberOfEmployees.ToString());
-
