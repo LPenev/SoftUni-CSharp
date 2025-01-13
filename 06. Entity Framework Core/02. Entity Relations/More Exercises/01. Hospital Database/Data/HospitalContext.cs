c58a89da0a3423b8a746e5d43c41181321c036aa
LPenev <150725482+LPenev@users.noreply.github.com>
2025-01-13 16:17:49 +0100

    01. Hospital Database Created

---
+++ 06. Entity Framework Core/02. Entity Relations/More Exercises/01. Hospital Database/Data/HospitalContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext(DbContextOptions options) : base(options) { }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost;Database=FootballBetting;User=demo;Password=Demo1234");
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
