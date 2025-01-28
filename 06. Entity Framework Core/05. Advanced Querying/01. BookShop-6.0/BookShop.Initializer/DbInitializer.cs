d35e1a25451a5d28efa40a123d45e8066b3d11b7
LPenev <150725482+LPenev@users.noreply.github.com>
2025-01-28 20:06:27 +0100

    BookShop Task 02 Completed

--- 06. Entity Framework Core/05. Advanced Querying/01. BookShop-6.0/BookShop.Initializer/DbInitializer.cs
+++ 06. Entity Framework Core/05. Advanced Querying/01. BookShop-6.0/BookShop.Initializer/DbInitializer.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-
-namespace BookShop.Initializer
+﻿namespace BookShop.Initializer
 {
     using System;
 
--- 06. Entity Framework Core/05. Advanced Querying/01. BookShop-6.0/BookShop/StartUp.cs
+++ 06. Entity Framework Core/05. Advanced Querying/01. BookShop-6.0/BookShop/StartUp.cs
@@ -1,14 +1,43 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using System.Text;
 
     public class StartUp
     {
         public static void Main()
         {
             using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            //DbInitializer.ResetDatabase(db);
+
+            // Tasks
+
+            // 02. Age Restriction
+            string command = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+
+            // 03. Golden Books
+
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var sb = new StringBuilder();
+            var ageRestrinction = Enum.Parse<AgeRestriction>(command, true);
+
+            var resultBooksTitel = context.Books
+                .Where(x => x.AgeRestriction == ageRestrinction)
+                .Select(x => new { x.Title})
+                .OrderBy(x => x.Title);
+
+            foreach (var book in resultBooksTitel) 
+            {
+                sb.AppendLine(book.Title);
+            }
+            
+            return sb.ToString().TrimEnd();
         }
     }
 }
