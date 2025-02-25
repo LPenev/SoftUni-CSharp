5f4751fab9f8e14b2e072af15cb8d9fab7ab6055
LPenev <150725482+LPenev@users.noreply.github.com>
2025-02-25 19:59:24 +0100

    02. ProductShop importUserDTO created

---
+++ 06. Entity Framework Core/06. JavaScript Object Notation - JSON/02. ProductShop/DTOs/Import/ImortUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Import;
+
+class ImortUserDTO
+{
+    [JsonProperty("firstName")]
+    public string? FirstName { get; set; }
+    [JsonProperty("lastName")]
+    public string LastName { get; set; }
+    [JsonProperty("age")]
+    public int? Age { get; set; }
+}
