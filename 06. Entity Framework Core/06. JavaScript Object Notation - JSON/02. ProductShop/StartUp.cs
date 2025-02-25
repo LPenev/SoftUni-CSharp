6fcea89782b21bf6084d8354c99a61a18abbbca6
LPenev <150725482+LPenev@users.noreply.github.com>
2025-02-25 19:52:13 +0100

    02. ProductShop Migration Added

--- 06. Entity Framework Core/06. JavaScript Object Notation - JSON/02. ProductShop/StartUp.cs
+++ 06. Entity Framework Core/06. JavaScript Object Notation - JSON/02. ProductShop/StartUp.cs
@@ -1,9 +1,25 @@
-﻿namespace ProductShop
+﻿using Microsoft.EntityFrameworkCore;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main()
         {
+            var db = new ProductShopContext();
+
+            var result = String.Empty;
+
+            //result = ImportUsers(db, );
+
         }
+
+        //public static string ImportUsers(ProductShopContext context, string inputJson)
+        //{
+
+        //    return $"Successfully imported {users.Count}";
+        //}
     }
 }
