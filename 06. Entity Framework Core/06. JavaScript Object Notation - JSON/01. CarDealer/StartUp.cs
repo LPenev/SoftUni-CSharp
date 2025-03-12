37e74fa78bc0e94d723377567ef452a31c3b13a5
LPenev <150725482+LPenev@users.noreply.github.com>
2025-03-12 22:46:23 +0100

    09. Import Suppliers Migration added, 09. Import Suppliers Completed

--- 06. Entity Framework Core/06. JavaScript Object Notation - JSON/01. CarDealer/StartUp.cs
+++ 06. Entity Framework Core/06. JavaScript Object Notation - JSON/01. CarDealer/StartUp.cs
@@ -1,10 +1,37 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer
 {
+    //Microsoft.EntityFrameworkCore.Tools
+    //Add-Migration Initial
+    //Update-Database
     public class StartUp
     {
         public static void Main()
         {
+            var db = new CarDealerContext();
+
+            // 09. Import Suppliers
+            var suppliers = File.ReadAllText("../../../Datasets/suppliers.json");
+            Console.WriteLine(ImportSuppliers(db, suppliers));
+
 
         }
+
+        // 09. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+
+            context.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+
+
     }
 }
