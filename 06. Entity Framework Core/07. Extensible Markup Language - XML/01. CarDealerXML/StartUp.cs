1849781d28327573253dd729a4e4eaaf60ca42cb
LPenev <150725482+LPenev@users.noreply.github.com>
2025-03-24 22:07:20 +0100

    01. CarDealerXml class ImportSuppliers Added Add Migration

--- 06. Entity Framework Core/07. Extensible Markup Language - XML/01. CarDealerXML/StartUp.cs
+++ 06. Entity Framework Core/07. Extensible Markup Language - XML/01. CarDealerXML/StartUp.cs
@@ -1,10 +1,55 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            var db = new CarDealerContext();
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
 
+            var inputXml = String.Empty;
+            var output = String.Empty;
+
+            // 09. Import Suppliers
+            inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            output = ImportSuppliers(db, inputXml);
+
+
+
+            // Print output
+            Console.WriteLine(output);
+        }
+
+        // 09. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(SupplierImportDto[]), new XmlRootAttribute("Suppliers"));
+
+            SupplierImportDto[] suppliersDto;
+
+            using (var reader = new StringReader(inputXml)) 
+            {
+                suppliersDto = (SupplierImportDto[])xmlSerializer.Deserialize(reader);
+            }
+            ;
+
+            Supplier[] suppliers = suppliersDto.Select(dto => new Supplier() 
+            {
+                Name = dto.Name,
+                IsImporter = dto.IsImporter
+            }).ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            
+            return $"Successfully imported {suppliers.Count()}";
         }
     }
 }
