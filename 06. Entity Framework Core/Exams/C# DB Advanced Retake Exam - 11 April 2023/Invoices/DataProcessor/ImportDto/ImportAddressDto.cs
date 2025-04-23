bd9bb0951118cf14c51007500bb16b351d7dee5e
LPenev <150725482+LPenev@users.noreply.github.com>
2025-04-23 15:19:21 +0200

    Invoices ImportDto Client and Address created

---
+++ 06. Entity Framework Core/Exams/C# DB Advanced Retake Exam - 11 April 2023/Invoices/DataProcessor/ImportDto/ImportAddressDto.cs
@@ -0,0 +1,35 @@
+﻿using Invoices.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using static Invoices.Common.ValidationConstants;
+
+namespace Invoices.DataProcessor.ImportDto;
+
+[XmlType(nameof(Address))]
+public class ImportAddressDto
+{
+    [Required]
+    [MinLength(AddressStreetNameMinLenght)]
+    [MaxLength(AddressStreetNameMaxLenght)]
+    [XmlElement(nameof(StreetName))]
+    public string StreetName { get; set; } = null!;
+
+    [XmlElement(nameof(StreetNumber))]
+    public int StreetNumber { get; set;}
+
+    [Required]
+    [XmlElement(nameof(PostCode))]
+    public string PostCode { get; set; } = null!;
+
+    [Required]
+    [MinLength(AddressCityMinLenght)]
+    [MaxLength(AddressCityMaxLenght)]
+    [XmlElement(nameof(City))]
+    public string City { get; set; } = null!;
+
+    [Required]
+    [MinLength(AddressCountryMinLenght)]
+    [MaxLength(AddressCountryMaxLenght)]
+    [XmlElement(nameof(Country))]
+    public string Country { get; set; } = null!;
+}
---
+++ 06. Entity Framework Core/Exams/C# DB Advanced Retake Exam - 11 April 2023/Invoices/DataProcessor/ImportDto/ImportClientDto.cs
@@ -0,0 +1,23 @@
+﻿using Invoices.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using static Invoices.Common.ValidationConstants;
+
+namespace Invoices.DataProcessor.ImportDto;
+
+[XmlType(nameof(Client))]
+public class ImportClientDto
+{
+    [Required]
+    [MinLength(ClientNameMinLenght)]
+    [MaxLength(ClientNameMaxLenght)]
+    [XmlElement(nameof(Name))]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [XmlElement(nameof(NumberVat))]
+    public string NumberVat { get; set; } = null!;
+
+    [XmlArray(nameof(Addresses))]
+    public ImportAddressDto[] Addresses { get; set;} = null!;
+}
