07d3789eaf46d66e66e40a106c4ab22b2e41bbd5
LPenev <150725482+LPenev@users.noreply.github.com>
2025-05-12 13:05:46 +0200

    Boardgames ExportDto Boardgame and CreatorWithBoardgames created

---
+++ 06. Entity Framework Core/Exams/C# DB Advanced Exam - 01 April 2023/Boardgames/DataProcessor/ExportDto/ExportBoardgameDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto;
+
+[XmlType(nameof(Boardgames))]
+public class ExportBoardgameDto
+{
+    [Required]
+    public string BoardgameName { get; set; } = null!;
+
+    public int BoardgameYearPublished { get; set; }
+
+}
---
+++ 06. Entity Framework Core/Exams/C# DB Advanced Exam - 01 April 2023/Boardgames/DataProcessor/ExportDto/ExportCreatorWithBroardgamesDto.cs
@@ -0,0 +1,17 @@
+﻿using Boardgames.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto;
+
+[XmlType(nameof(Creator))]
+public class ExportCreatorWithBroardgamesDto
+{
+    [Required]
+    public string CreatorName { get; set; } = null!;
+
+    [Required]
+    [XmlArray(nameof(Boardgames))]
+    public Boardgame[] Boardgames { get; set; } = null!;
+
+}
