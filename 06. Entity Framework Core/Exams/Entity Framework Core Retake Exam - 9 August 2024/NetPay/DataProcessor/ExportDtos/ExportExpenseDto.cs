b09b2de35a8c2fe99bc49bf4bf0165db175a49b8
LPenev <150725482+LPenev@users.noreply.github.com>
2025-05-19 16:09:28 +0200

    NetPay ExportDto ExportHouseholdXmlDto and ExportExpenseXmlDto created

---
+++ 06. Entity Framework Core/Exams/Entity Framework Core Retake Exam - 9 August 2024/NetPay/DataProcessor/ExportDtos/ExportExpenseDto.cs
@@ -0,0 +1,23 @@
+﻿using NetPay.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using static NetPay.Common.ValidationConstants;
+
+namespace NetPay.DataProcessor.ExportDtos;
+
+[XmlType(nameof(Expense))]
+public class ExportExpenseDto
+{
+    
+    [XmlElement(nameof(ExpenseName))]
+    public string ExpenseName { get; set; } = null!;
+
+    [XmlElement(nameof(Amount))]
+    public decimal Amount { get; set; }
+
+    [XmlElement(nameof(PaymentDate))]
+    public string PaymentDate { get; set; }
+
+    [XmlElement(nameof(ServiceName))]
+    public string ServiceName { get; set; }
+}
---
+++ 06. Entity Framework Core/Exams/Entity Framework Core Retake Exam - 9 August 2024/NetPay/DataProcessor/ExportDtos/ExportHouseholdXmlDto.cs
@@ -0,0 +1,20 @@
+﻿using NetPay.Data.Models;
+using System.Xml.Serialization;
+
+namespace NetPay.DataProcessor.ExportDtos;
+
+[XmlType(nameof(Household))]
+public class ExportHouseholdXmlDto
+{
+    [XmlElement(nameof(ContactPerson))]
+    public string ContactPerson { get; set; }
+
+    [XmlElement(nameof(Email))]
+    public string Email { get; set; }
+
+    [XmlElement(nameof(PhoneNumber))]
+    public string PhoneNumber { get; set; } = null!;
+
+    [XmlArray(nameof(Expenses))]
+    public string Expenses { get; set; }
+}
