0ae4327e9009bec729a3e034f1a8d688ce3387db
LPenev <150725482+LPenev@users.noreply.github.com>
2025-05-26 21:26:41 +0200

    TravelAgency ImportCustomerDto created

---
+++ 06. Entity Framework Core/Exams/C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/DataProcessor/ImportDtos/ImportCustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using TravelAgency.Data.Models;
+using static TravelAgency.Common.ValidationConstants;
+
+namespace TravelAgency.DataProcessor.ImportDtos;
+
+[XmlType(nameof(Customer))]
+public class ImportCustomerDto
+{
+    [XmlAttribute(nameof(PhoneNumber))]
+    [Required]
+    [MinLength(CustomerPhoneNumberLength)]
+    [MaxLength(CustomerPhoneNumberLength)]
+    public string PhoneNumber { get; set; } = null!;
+
+    [XmlElement(nameof(FullName))]
+    [Required]
+    [MinLength(CustomerFullNameMinLength)]
+    [MaxLength(CustomerFullNameMaxLength)]
+    public string FullName { get; set; } = null!;
+
+    [XmlElement(nameof(Email))]
+    [Required]
+    [MinLength(CustomerEmailMinLength)]
+    [MaxLength(CustomerEmailMaxLength)]
+    public string Email { get; set; } = null!;
+}
