2de2c6361ce6fb74aa49c3da191325939419c3f4
LPenev <150725482+LPenev@users.noreply.github.com>
2025-06-26 16:18:50 +0200

    CinemaApp Entity Movie added und migration

---
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp.Data.Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static CinemaApp.GCommon.EntityConstants.Movie;
+
+namespace CinemaApp.Data.Models;
+
+public class Movie
+{
+    [Comment("Movie Identifier")]
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Comment("Movie Title")]
+    [Required(ErrorMessage = TitleRequiredErrorMessage)]
+    [StringLength(TitleMaxLength)]
+    public string Title { get; set; } = null!;
+    
+    [Comment("Movie Genre")]
+    [Required(ErrorMessage = GenreRequiredErrorMessage)]
+    [StringLength(GenreMaxLength, ErrorMessage = GenreMaxLengthErrorMessage)]
+    public string Genre { get; set; } = null!;
+    
+    [Comment("Movie Release Date")]
+    [Required(ErrorMessage = ReleaseDateRequiredErrorMessage)]
+    public DateTime ReleaseDate { get; set; }
+    
+    [Comment("Movie Director")]
+    [Required(ErrorMessage = DirectorRequiredErrorMessage)]
+    [StringLength(DirectorMaxLength, ErrorMessage = DirectorMaxLengthErrorMessage)]
+    public string Director { get; set; } = null!;
+    
+    [Required(ErrorMessage = DurationRequiredErrorMessage)]
+    public int Duration { get; set; }
+    
+    [Required(ErrorMessage = DescriptionRequiredErrorMessage)]
+    [StringLength(DescriptionMaxLength, ErrorMessage = DescriptionMaxLengthErrorMessage)]
+    public string Description { get; set; } = null!;
+    public string? ImageUrl { get; set; }
+    public bool IsDeleted { get; set; }
+}
--- 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp.Data/ApplicationDbContext.cs
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp.Data/ApplicationDbContext.cs
@@ -1,12 +1,23 @@
 ﻿namespace CinemaApp.Data
 {
+    using CinemaApp.Data.Models;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using System.Reflection;
+
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Movie> Movies { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //builder.ApplyConfigurationsFromAssembly(Assembly.GetEntryAssembly());
         }
     }
 }
