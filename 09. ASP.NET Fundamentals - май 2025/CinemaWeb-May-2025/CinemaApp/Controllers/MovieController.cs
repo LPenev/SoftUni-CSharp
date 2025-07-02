2ccb875bba4ed785b5af5aad5870c97d9768b70a
LPenev <150725482+LPenev@users.noreply.github.com>
2025-07-02 14:46:33 +0200

    CinemaApp MovieController created, AllMoviesIndexView moved to Movie Folder

---
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using CinemaApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers;
+
+public class MovieController : Controller
+{
+    private readonly CinemaAppDbContext context;
+
+    public MovieController(CinemaAppDbContext context)
+    {
+        this.context = context;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
