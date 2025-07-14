5cd632df82f3563c0dfeb11f1dfde9e95f3b6dd2
LPenev <150725482+LPenev@users.noreply.github.com>
2025-07-14 21:40:26 +0200

    CinemaApp Migration MovieConfigurationFilterAdded

---
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp.Data/Migrations/20250714193928_MovieConfigurationAddedFilterIsDeleted.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovieConfigurationAddedFilterIsDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Movie",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Movie",
+                type: "nvarchar(2048)",
+                maxLength: 2048,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Movie",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2048)",
+                oldMaxLength: 2048,
+                oldNullable: true);
+        }
+    }
+}
