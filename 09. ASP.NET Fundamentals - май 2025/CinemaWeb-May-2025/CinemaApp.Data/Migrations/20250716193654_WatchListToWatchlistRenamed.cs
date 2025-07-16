458f54d65309f50cc71ff76d459724403d1e2200
LPenev <150725482+LPenev@users.noreply.github.com>
2025-07-16 21:38:32 +0200

    CinemaApp Mitration WatchList to Watchlist renamed

---
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp.Data/Migrations/20250716193654_WatchListToWatchlistRenamed.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class WatchListToWatchlistRenamed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WatchLists_AspNetUsers_UserId",
+                table: "WatchLists");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WatchLists_Movies_MovieId",
+                table: "WatchLists");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WatchLists",
+                table: "WatchLists");
+
+            migrationBuilder.RenameTable(
+                name: "WatchLists",
+                newName: "Watchlists");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_WatchLists_MovieId",
+                table: "Watchlists",
+                newName: "IX_Watchlists_MovieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Watchlists",
+                table: "Watchlists",
+                columns: new[] { "UserId", "MovieId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Watchlists_AspNetUsers_UserId",
+                table: "Watchlists",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Watchlists_Movies_MovieId",
+                table: "Watchlists",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Watchlists_AspNetUsers_UserId",
+                table: "Watchlists");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Watchlists_Movies_MovieId",
+                table: "Watchlists");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Watchlists",
+                table: "Watchlists");
+
+            migrationBuilder.RenameTable(
+                name: "Watchlists",
+                newName: "WatchLists");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Watchlists_MovieId",
+                table: "WatchLists",
+                newName: "IX_WatchLists_MovieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WatchLists",
+                table: "WatchLists",
+                columns: new[] { "UserId", "MovieId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WatchLists_AspNetUsers_UserId",
+                table: "WatchLists",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WatchLists_Movies_MovieId",
+                table: "WatchLists",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
