7f6e35abdbb0064ea1dbf5b8c36e010fe64d4f25
LPenev <150725482+LPenev@users.noreply.github.com>
2025-07-30 18:21:10 +0200

    CinemaApp Manager ConfigureCinemas, ShowtimeSetup and Ticket Controllers created

---
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp/Areas/Manager/Controllers/ConfigureCinemasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    [Authorize(Roles = "Manager")]
+    public class ConfigureCinemasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // -> Areas/Manager/Views/ConfigureCinemas/Index.cshtml
+        }
+    }
+}
