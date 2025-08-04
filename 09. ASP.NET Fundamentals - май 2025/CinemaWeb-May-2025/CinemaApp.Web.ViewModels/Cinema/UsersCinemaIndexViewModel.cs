4b557423e864ebf8a9628827251e295a3587286d
LPenev <150725482+LPenev@users.noreply.github.com>
2025-08-04 12:01:26 +0200

    CinemaApp Created Cinema and Program

---
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp.Web.ViewModels/Cinema/UsersCinemaIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaApp.Web.ViewModels.Cinema;
+
+public class UsersCinemaIndexViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Location { get; set; } = null!;
+}
+
---
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp.Web.ViewModels/Movie/MovieUserProgramViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CinemaApp.Web.ViewModels.Movie;
+
+public class MovieUserProgramViewModel
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Director { get; set; } = null!;
+
+    public string ImageUrl { get; set; } = null!;
+}
+
