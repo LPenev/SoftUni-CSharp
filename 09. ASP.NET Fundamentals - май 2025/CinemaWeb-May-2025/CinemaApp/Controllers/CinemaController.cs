c3ee5ffce2ba25d4134258baf89604dbcfce5c7d
LPenev <150725482+LPenev@users.noreply.github.com>
2025-08-06 11:32:22 +0200

    CinemaApp added CinemaExplore into Home/Index CinemaControler created

---
+++ 09. ASP.NET Fundamentals - май 2025/CinemaWeb-May-2025/CinemaApp/Controllers/CinemaController.cs
@@ -0,0 +1,23 @@
+﻿using CinemaApp.Web.ViewModels.Cinema;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers;
+
+public class CinemaController : BaseController
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public CinemaController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [AllowAnonymous]
+    public IActionResult Index()
+    {
+        var model = Enumerable.Empty<UsersCinemaIndexViewModel>();
+        return View(model);
+    }
+}
+
